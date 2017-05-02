61a0a75558c19971ae8f858c479183d56d3e527f
Amanpreet Singh <amanpreet.singh@flipkart.com>
2017-05-02 14:07:17 +0530

    Added Entity Relation.

--- orm/orm-fun.go
+++ orm/orm-fun.go
@@ -1,12 +1,28 @@
 package main
 
 import "github.com/jinzhu/gorm"
-import _ "github.com/jinzhu/gorm/dialects/mysql"
+import (
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"fmt"
+)
 
 type Product struct {
 	gorm.Model
-	Code  string
-	Price uint
+	Code       string `gorm:"size 5"`
+	Price      uint
+	IgnoreMe   string `gorm:"-"` // Ignore this field
+	Vertical   Vertical
+	VerticalID int
+}
+
+type Vertical struct {
+	gorm.Model
+	Name string `gorm:"unique"`
+}
+
+//Default Name would be products
+func (p *Product) TableName() string {
+	return "MeraProduct"
 }
 
 func main() {
@@ -18,6 +34,8 @@ func main() {
 
 	playProduct(db)
 
+	db.Create(&Product{Code: "LongCode", Price: 4})
+
 	//schemaAlterPlay(db)
 }
 
@@ -28,17 +46,27 @@ func schemaAlterPlay(db *gorm.DB) {
 
 func playProduct(db *gorm.DB) {
 	// Migrate the schema
-	db.AutoMigrate(&Product{})
+	db.AutoMigrate(&Product{}, &Vertical{})
+	createVertical(db)
 	// Create
-	db.Create(&Product{Code: "L1212", Price: 1000})
+	db.Create(&Product{Code: "L1212", Price: 1000, VerticalID: 1})
 	// Read
 	var product Product
 	db.First(&product, 1)
 	// find product with id 1
-	db.First(&product, "code = ?", "L1212")
+	db.First(&product, "code = ?", "L1212").Related(&Vertical{})
+	fmt.Println("Vertical ID:", product.VerticalID, "Vertical Name:", product.Vertical.Name)
 	// find product with code l1212
 	// Update - update product's price to 2000
 	db.Model(&product).Update("Price", 2000)
 	// Delete - delete product
 	db.Delete(&product)
+}
+func createVertical(db *gorm.DB) {
+	vertical := &Vertical{}
+	db.First(vertical, "name=?", "Shirts")
+	if vertical == nil {
+		fmt.Println("Missing Vertical Creating One")
+		db.Create(&Vertical{Name: "Shirts"})
+	}
 }
