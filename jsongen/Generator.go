87caac46257809889f8091194c531884a00e608a
Amanpreet Singh <amanpreet.singh@flipkart.com>
2017-05-04 16:09:29 +0530

    Moved Template to Go File.

--- jsongen/Generator.go
+++ jsongen/Generator.go
@@ -33,6 +33,26 @@ func (g *Generator) template() (*template.Template, error) {
 		return nil, errors.New("Unsupported format")
 	}
 
-	tmpl, e := template.ParseFiles("/Users/amanpreet.singh/IdeaProjects/GoArena/src/github.com/amanhigh/go-fun/jsongen/tmpl/write_to_json.tmpl")
+	//tmpl, e := template.ParseFiles("/Users/amanpreet.singh/IdeaProjects/GoArena/src/github.com/amanhigh/go-fun/jsongen/tmpl/write_to_json.tmpl")
+	tmpl := template.New("jsonTemplate")
+	tmpl, e := tmpl.Parse(templateString())
 	return tmpl, e
+}
+
+func templateString() string {
+	return `package {{ .PackageName }}
+
+import (
+	"encoding/json"
+	"io"
+)
+
+func (obj {{ .Type }}) WriteTo(writer io.Writer) (int64, error) {
+	data, err := json.Marshal(&obj)
+	if err != nil {
+		return 0, err
+	}
+	length, err := writer.Write(data)
+	return int64(length), err
+}`
 }
