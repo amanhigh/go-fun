e943651dbec966340aac9dd28217a6c81d6ffab4
Amanpreet Singh <amanpreet.singh@flipkart.com>
2017-08-28 16:23:26 +0530

    Added Command Runner.

---
+++ command/commander.go
@@ -0,0 +1,14 @@
+package commander
+
+import (
+	"os/exec"
+	"fmt"
+)
+
+func RunCommand(cmd string) (string, error) {
+	output, err := exec.Command("sh", "-c", cmd).Output()
+	if err != nil {
+		fmt.Printf("Error Executing Command: CMD:%v ERROR:%+v\n", cmd, err)
+	}
+	return string(output), err
+}
