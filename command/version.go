e4b36413cc151408c31f5d7c1144bc0599460bea
Amanpreet Singh <amanpreet.singh@flipkart.com>
2017-08-29 12:43:20 +0530

    Added Get Latest Version.

--- command/version.go
+++ command/version.go
@@ -6,11 +6,29 @@ import (
 	"time"
 )
 
+func GetVersion(pkgName string, host string, versionType string) {
+	switch versionType {
+	case "dpkg":
+		GetDpkgVersion(pkgName, host)
+	case "latest":
+		GetLatestVersion(pkgName, host)
+	}
+}
+
 func GetDpkgVersion(pkgName string, host string) {
 	PrintBlue(fmt.Sprintf("Fetching Config for %v from %v", pkgName, host))
 	cmd := fmt.Sprintf(`ssh %v dpkg -l | grep "%v" | tail -1 | awk '{print $3}'`, host, pkgName)
 	dpkgVersion := RunCommandPrintError(cmd)
 	versionString := fmt.Sprintf("%v - %v - HostVersion: %v", pkgName, dpkgVersion, time.Now().Format(LAYOUT))
+	PrintYellow(versionString)
+	ioutil.WriteFile(RELEASE_FILE, []byte(versionString), DEFAULT_PERM)
+}
+
+func GetLatestVersion(pkgName string, host string) {
+	PrintBlue(fmt.Sprintf("Fetching LatestVersion for %v from %v", pkgName, host))
+	cmd := fmt.Sprintf(`ssh %v "sudo apt-get update > /dev/null; apt-cache madison %v | head -1" | awk '{print $3}'`, host, pkgName)
+	latestVersion := RunCommandPrintError(cmd)
+	versionString := fmt.Sprintf("%v - %v - LatestVersion: %v", pkgName, latestVersion, time.Now().Format(LAYOUT))
 	PrintYellow(versionString)
 	ioutil.WriteFile(RELEASE_FILE, []byte(versionString), DEFAULT_PERM)
 }
