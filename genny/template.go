32b1880909e6ccaf68df25d1ff120dd6ef5c73d2
Amanpreet Singh <amanpreet.singh@flipkart.com>
2017-09-04 16:03:06 +0530

    Added Genny Example.

---
+++ genny/template.go
@@ -0,0 +1,21 @@
+//go:generate genny -in=$GOFILE -out=gen-$GOFILE gen "KeyType=string,int ValueType=string,int"
+package genny
+
+import (
+	"fmt"
+	"github.com/cheekybits/genny/generic"
+)
+
+/* Use generic.Type to Compile */
+type KeyType generic.Type
+type ValueType generic.Type
+
+type KeyTypeValueTypeMap struct {
+	typedMap map[KeyType]ValueType
+}
+
+func (self *KeyTypeValueTypeMap) PrintType() {
+	for key, value := range self.typedMap {
+		fmt.Printf("Key:+v Value: %v\n", key, value)
+	}
+}
