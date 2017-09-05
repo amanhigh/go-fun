900880bc6477301ac96447397b7a4e4b931ed5ab
Amanpreet Singh <amanpreet.singh@flipkart.com>
2017-09-05 19:13:48 +0530

    Added Git Push

--- command/git.go
+++ command/git.go
@@ -11,6 +11,10 @@ func GitCommit(msg string, filePath string) {
 	PrintCommand(fmt.Sprintf("git commit -m '%v' %v", msg, filePath))
 }
 
+func GitPush(){
+	PrintCommand("git push origin")
+}
+
 func GitDiff() {
 	PrintCommand("git diff -U0")
 }
