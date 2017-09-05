325b57fe7feb27134ca76e3920ae82b1fd223527
Amanpreet Singh <amanpreet.singh@flipkart.com>
2017-09-05 17:19:09 +0530

    Added Content Piper.

--- command/commands.go
+++ command/commands.go
@@ -6,3 +6,8 @@ func Jcurl(url string, pipe string) (output string) {
 	output = RunCommandPrintError(fmt.Sprintf("curl -s %v | jq . %v", url, pipe))
 	return
 }
+
+func ContentPiper(content string,pipe string) (output string)  {
+	output = RunCommandPrintError(fmt.Sprintf("echo '%v' | %v",content,pipe ))
+	return
+}
