0ab879109d92367e404bc11f9a2e458512ecd225
Amanpreet Singh <amanpreet.singh@flipkart.com>
2017-09-05 17:49:30 +0530

    Splited By New Line.

--- command/commands.go
+++ command/commands.go
@@ -1,13 +1,16 @@
 package commander
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func Jcurl(url string, pipe string) (output string) {
 	output = RunCommandPrintError(fmt.Sprintf("curl -s %v | jq . %v", url, pipe))
 	return
 }
 
-func ContentPiper(content string,pipe string) (output string)  {
-	output = RunCommandPrintError(fmt.Sprintf("echo '%v' | %v",content,pipe ))
-	return
+func ContentPiper(content string,pipe string) ([]string)  {
+	output := RunCommandPrintError(fmt.Sprintf("echo '%v' | %v",content,pipe ))
+	return strings.Split(output,"\n")
 }
