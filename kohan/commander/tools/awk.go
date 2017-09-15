139e92a06fd96544479bf05c6278d53e8c77afe0
Amanpreet Singh <amanpreet.singh@flipkart.com>
2017-09-15 22:34:14 +0530

    Extracted Variable.

--- kohan/commander/tools/awk.go
+++ kohan/commander/tools/awk.go
@@ -6,10 +6,12 @@ import (
 	"github.com/amanhigh/go-fun/util"
 )
 
+var TIME_OUT_FILE = "/tmp/time.out"
+
 func ExtractLogForDuration(remoteIp string, logFile string, grepPattern string, startTime time.Time, endTime time.Time, timeLayout string) {
 	startString := util.FormatTime(startTime, timeLayout)
 	endString := util.FormatTime(endTime, timeLayout)
 	util.PrintYellow(fmt.Sprintf("Extracting Log between %v - %v from %v", startString, endString, logFile))
-	PrintCommand(fmt.Sprintf(`ssh %v "awk '\$2>=\"%v\" && \$2<=\"%v\"' %v | grep '%v' > /tmp/time.out"`,
-		remoteIp, startString, endString, logFile, grepPattern))
+	PrintCommand(fmt.Sprintf(`ssh %v "awk '\$2>=\"%v\" && \$2<=\"%v\"' %v | grep '%v' > %v"`,
+		remoteIp, startString, endString, logFile, grepPattern, TIME_OUT_FILE))
 }
