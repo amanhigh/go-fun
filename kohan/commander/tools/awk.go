3eff9070e565fba2e20a79d8d2df10a9627a7196
Amanpreet Singh <amanpreet.singh@flipkart.com>
2017-09-16 00:08:56 +0530

    Added More Info.

--- kohan/commander/tools/awk.go
+++ kohan/commander/tools/awk.go
@@ -11,7 +11,7 @@ var TIME_OUT_FILE = "/tmp/time.out"
 func ExtractLogForDuration(remoteIp string, logFile string, grepPattern string, startTime time.Time, endTime time.Time, timeLayout string) {
 	startString := util.FormatTime(startTime, timeLayout)
 	endString := util.FormatTime(endTime, timeLayout)
-	util.PrintYellow(fmt.Sprintf("Extracting Log between %v - %v File:%v Server:%v Output:%v", startString, endString, logFile, remoteIp, TIME_OUT_FILE))
+	util.PrintYellow(fmt.Sprintf("Extracting Log between %v - %v FilterPattern:%v File:%v Server:%v Output:%v", startString, endString, grepPattern, logFile, remoteIp, TIME_OUT_FILE))
 	PrintCommand(fmt.Sprintf(`ssh %v "awk '\$2>=\"%v\" && \$2<=\"%v\"' %v | grep '%v' > %v"`,
 		remoteIp, startString, endString, logFile, grepPattern, TIME_OUT_FILE))
 }
