14544f02fb3bf2b312af2026e6f8134453aec421
Amanpreet Singh <amanpreet.singh@flipkart.com>
2017-09-15 13:32:30 +0530

    Added New Layout.

--- util/time.go
+++ util/time.go
@@ -2,8 +2,9 @@ package util
 
 import "time"
 
-/* Date/Time */
+/* Layouts */
 const PRINT_LAYOUT = "Jan 2, 2006 at 3:04pm (MST)"
+const SLASH_MILLISECOND_LAYOUT = "[02/Jan/2006:15:04:05.000]"
 
 func FormatTime(time time.Time, layout string) string {
 	return time.Format(layout)
