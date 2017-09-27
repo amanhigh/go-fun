8ea818b984738f1b0cf03d854f114006b8fd0bdf
Amanpreet Singh <amanpreet.singh@flipkart.com>
2017-09-27 12:49:04 +0530

    Commited Empty Experiment

--- kohan/main/experiment.go
+++ kohan/main/experiment.go
@@ -1,20 +1,4 @@
 package main
 
-import (
-	"time"
-	"fmt"
-)
-
 func main() {
-	intChannel := make(chan int)
-	go func() {
-		for{
-			time.Sleep(4 * time.Second)
-			intChannel<-1
-		}
-	}()
-
-	for value := range intChannel {
-		fmt.Printf("Value:%+v\n", value)
-	}
 }
