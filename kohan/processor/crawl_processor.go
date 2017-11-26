a6618249c6ab1c3fa7b736102623192ba6311e4b
Amanpreet Singh <coool.aman@gmail.com>
2017-11-26 11:26:19 +0530

    Linked Manager.

--- kohan/processor/crawl_processor.go
+++ kohan/processor/crawl_processor.go
@@ -2,7 +2,7 @@ package processor
 
 import (
 	"flag"
-	"github.com/amanhigh/go-fun/kohan/commander/components/crawler"
+	. "github.com/amanhigh/go-fun/kohan/commander/components/crawler"
 )
 
 type CrawlProcessor struct {
@@ -23,10 +23,11 @@ func (self *CrawlProcessor) handleImdb(flagSet *flag.FlagSet, args []string) err
 	cutoff := flagSet.Int("c", 5, "Movie Cutoff")
 	langCode := flagSet.String("l", "en", "Language Code [pa,en,hi]")
 	e := flagSet.Parse(args)
-	crawl(crawler.NewImdbCrawler(*year, *langCode, *cutoff))
+	crawl(NewImdbCrawler(*year, *langCode, *cutoff))
 	return e
 }
 
-func crawl(crawler crawler.Crawler) {
-	crawler.CrawlerManager{Crawler: crawler}.Crawl()
+func crawl(crawler Crawler) {
+	manager := CrawlerManager{Crawler: crawler}
+	manager.Crawl()
 }
