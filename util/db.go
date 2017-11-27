5c705b1e62a40e4acf36ca973cfe12894c09d358
Amanpreet Singh <amanpreet.singh@flipkart.com>
2017-11-27 16:38:24 +0530

    Added Db to Utils

---
+++ util/db.go
@@ -0,0 +1,28 @@
+package util
+
+import (
+	"bitbucket.org/liamstask/goose/lib/goose"
+	log "github.com/Sirupsen/logrus"
+	"github.com/amanhigh/go-fun/kohan/commander"
+	"github.com/jinzhu/gorm"
+)
+
+func NewDb(path string) *gorm.DB {
+	dbConf, _ := goose.NewDBConf(path, "development", "mysql")
+
+	if db, err := gorm.Open(dbConf.PgSchema, dbConf.Driver.OpenStr); err != nil {
+		log.WithFields(log.Fields{"Path": path, "User": "root", "Type": "mysql"}).Panic("failed to connect database")
+		return nil
+	} else {
+		/** Print SQL */
+		if commander.IsDebugMode() {
+			db.LogMode(true)
+		}
+		return db
+	}
+}
+
+func Migrate(db *gorm.DB, values ...interface{}) {
+	/** AutoMigrate Schema */
+	db.AutoMigrate(values)
+}
