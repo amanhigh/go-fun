2d9997a68a2eee1f2f31e3400cc15b7e9b94d312
Amanpreet Singh <coool.aman@gmail.com>
2017-12-01 02:09:43 +0530

    Handled Collected Count

--- kohan/commander/components/crawler/crawler.go
+++ kohan/commander/components/crawler/crawler.go
@@ -8,6 +8,7 @@ import (
 	. "github.com/amanhigh/go-fun/models/crawler"
 	"io/ioutil"
 	"strings"
+	"sync/atomic"
 )
 
 const (
@@ -29,8 +30,8 @@ type CrawlerManager struct {
 	cancelFunc context.CancelFunc
 
 	/* Counts to track collected & required */
-	//collectCount  int32
-	//RequiredCount int32
+	collected int32
+	required  int32
 
 	infoChannel chan CrawlInfo
 	goodInfo    []CrawlInfo
@@ -39,8 +40,8 @@ type CrawlerManager struct {
 
 func NewCrawlerManager(crawler Crawler, requiredCount int) *CrawlerManager {
 	return &CrawlerManager{
-		Crawler: crawler,
-		//RequiredCount: int32(requiredCount),
+		Crawler:     crawler,
+		required:    int32(requiredCount),
 		infoChannel: make(chan CrawlInfo, BUFFER_SIZE),
 	}
 }
@@ -69,6 +70,7 @@ func (self *CrawlerManager) BuildSet() {
 	for info := range self.infoChannel {
 		if info.GoodBad() {
 			self.goodInfo = append(self.goodInfo, info)
+			atomic.AddInt32(&self.collected, 1)
 		} else {
 			self.badInfo = append(self.badInfo, info)
 		}
@@ -91,9 +93,9 @@ func (self *CrawlerManager) PrintSet(good []CrawlInfo, bad []CrawlInfo) {
 	Print Info using interface and write extracted links to
 	GOOD/BAD Files for Chrome Processing
  */
-func printWriteCrawledInfo(good []CrawlInfo, filePath string) {
+func printWriteCrawledInfo(infos []CrawlInfo, filePath string) {
 	var urls []string
-	for _, info := range good {
+	for _, info := range infos {
 		info.Print()
 		urls = append(urls, info.ToUrl())
 	}
@@ -105,14 +107,17 @@ func printWriteCrawledInfo(good []CrawlInfo, filePath string) {
 	Write all Movies of current page onto channel
  */
 func (self *CrawlerManager) crawlRecursive(page *util.Page, waitGroup *sync.WaitGroup) {
-	util.PrintYellow(fmt.Sprintf("Processing: %v Collected: %v", page.Document.Url.String(), 1))
+	collected := atomic.LoadInt32(&self.collected)
 
-	/* If Next Link is Present Crawl It */
-	if link, ok := self.Crawler.NextPageLink(page); ok {
-		waitGroup.Add(1)
-		go self.crawlRecursive(util.NewPage(link), waitGroup)
+	if collected < self.required {
+		util.PrintYellow(fmt.Sprintf("Processing: %v Collected: %v", page.Document.Url.String(), collected))
+		/* If Next Link is Present Crawl It */
+		if link, ok := self.Crawler.NextPageLink(page); ok {
+			waitGroup.Add(1)
+			go self.crawlRecursive(util.NewPage(link), waitGroup)
+		}
+		/* Find Links for this Page */
+		self.Crawler.GatherLinks(page, self.infoChannel)
 	}
-	/* Find Links for this Page */
-	self.Crawler.GatherLinks(page, self.infoChannel)
 	waitGroup.Done()
 }
