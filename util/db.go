d1f7da09acc44440ab8f83fce26e508e93ec8444
Amanpreet Singh <coool.aman@gmail.com>
2017-12-13 10:30:48 +0530

    Windows gcc issue

--- util/db.go
+++ util/db.go
@@ -1,31 +1,31 @@
 package util
 
-import (
-	"bitbucket.org/liamstask/goose/lib/goose"
-	log "github.com/Sirupsen/logrus"
-	"github.com/jinzhu/gorm"
-)
-
-func NewDb(path string) *gorm.DB {
-	dbConf, _ := goose.NewDBConf(path, "development", "mysql")
-
-	if db, err := gorm.Open(dbConf.PgSchema, dbConf.Driver.OpenStr); err != nil {
-		log.WithFields(log.Fields{"Path": path, "User": "root", "Type": "mysql"}).Panic("failed to connect database")
-		return nil
-	} else {
-		/** Print SQL */
-		if IsDebugMode() {
-			db.LogMode(true)
-		}
-		return db
-	}
-}
-
-func TruncateTable(db *gorm.DB, tableName string) {
-	db.Exec("truncate table " + tableName)
-}
-
-func Migrate(db *gorm.DB, values ...interface{}) {
-	/** AutoMigrate Schema */
-	db.AutoMigrate(values)
-}
+//import (
+//	"bitbucket.org/liamstask/goose/lib/goose"
+//	log "github.com/Sirupsen/logrus"
+//	"github.com/jinzhu/gorm"
+//)
+//
+//func NewDb(path string) *gorm.DB {
+//	dbConf, _ := goose.NewDBConf(path, "development", "mysql")
+//
+//	if db, err := gorm.Open(dbConf.PgSchema, dbConf.Driver.OpenStr); err != nil {
+//		log.WithFields(log.Fields{"Path": path, "User": "root", "Type": "mysql"}).Panic("failed to connect database")
+//		return nil
+//	} else {
+//		/** Print SQL */
+//		if IsDebugMode() {
+//			db.LogMode(true)
+//		}
+//		return db
+//	}
+//}
+//
+//func TruncateTable(db *gorm.DB, tableName string) {
+//	db.Exec("truncate table " + tableName)
+//}
+//
+//func Migrate(db *gorm.DB, values ...interface{}) {
+//	/** AutoMigrate Schema */
+//	db.AutoMigrate(values)
+//}
