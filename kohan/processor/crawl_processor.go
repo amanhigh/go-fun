be049915e2b8ae7cafe77d5d5c34d87eccf41822
Amanpreet Singh <coool.aman@gmail.com>
2017-12-24 19:39:12 +0530

    Reduced default Required count to 200

--- kohan/processor/crawl_processor.go
+++ kohan/processor/crawl_processor.go
@@ -21,7 +21,7 @@ func (self *CrawlProcessor) GetNonArgedHandlers() (map[string]DirectFunc) {
 func (self *CrawlProcessor) handleImdb(flagSet *flag.FlagSet, args []string) error {
 	year := flagSet.Int("y", 2015, "Year of Movie")
 	cutoff := flagSet.Int("c", 5, "Movie Cutoff")
-	requiredCount := flagSet.Int("r", 500, "Required Count")
+	requiredCount := flagSet.Int("r", 200, "Required Count")
 	verbose := flagSet.Bool("v", false, "Verbose")
 	langCode := flagSet.String("l", "en", "Language Code [pa,en,hi]")
 	keyFile := flagSet.String("k", "/tmp/imdb.key", "IMDB Key File")
