7c9134903bca6714593942c92efb755b3e7452b7
Amanpreet Singh <amanpreet.singh@flipkart.com>
2018-02-05 15:25:09 +0530

    Fixed Param Name

--- kohan/commander/tools/mysql.go
+++ kohan/commander/tools/mysql.go
@@ -2,7 +2,7 @@ package tools
 
 import "fmt"
 
-func RunQuery(host string, database string, cmd string) string {
-	dbCmd := fmt.Sprintf(`ssh %v "echo 'use %v; %v' | sudo mysql -uroot"`, host, database, cmd)
+func RunQuery(host string, database string, query string) string {
+	dbCmd := fmt.Sprintf(`ssh %v "echo 'use %v; %v' | sudo mysql -uroot"`, host, database, query)
 	return RunCommandPrintError(dbCmd)
 }
