9412c092deed0c69534a50572025dd7173f914ac
Amanpreet Singh <amanpreet.singh@flipkart.com>
2018-02-05 16:06:12 +0530

    Added Run Remote Query

--- kohan/commander/tools/mysql.go
+++ kohan/commander/tools/mysql.go
@@ -2,7 +2,16 @@ package tools
 
 import "fmt"
 
-func RunQuery(host string, database string, query string) string {
-	dbCmd := fmt.Sprintf(`ssh %v "echo 'use %v; %v' | sudo mysql -uroot"`, host, database, query)
+func RunRootQuery(host string, database string, query string) string {
+	dbCmd := fmt.Sprintf(`ssh %v "%v | sudo mysql -uroot"`, host, getDatabaseQuery(database, query))
 	return RunCommandPrintError(dbCmd)
+}
+
+func RunQuery(host string, user string, pass string, database string, query string) string {
+	cmd := fmt.Sprintf("%v | mysql -u %v -p%v -h %v", getDatabaseQuery(database, query), user, pass, host)
+	return RunCommandPrintError(cmd)
+}
+
+func getDatabaseQuery(database string, query string) string {
+	return fmt.Sprintf("echo 'use %v; %v'", database, query)
 }
