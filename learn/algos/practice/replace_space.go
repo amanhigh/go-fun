ffaf5e4c33f34c5d9bd7a63811c0155afb7dbcca
Amanpreet Singh <coool.aman@gmail.com>
2018-04-23 01:58:04 +0530

    Added Replace Space

---
+++ learn/algos/practice/replace_space.go
@@ -0,0 +1,27 @@
+package practice
+
+func ReplaceSpace(input string) (string) {
+	spaceCount := 0
+	for _,c := range input {
+		if c == ' ' {
+			spaceCount++
+		}
+	}
+
+	out := make([]rune, len(input)+spaceCount*2)
+
+	i := 0
+	for _, c := range input {
+		if c == ' ' {
+			out[i] = '%'
+			out[i+1] = '2'
+			out[i+2] = '0'
+			i = i + 3
+		} else {
+			out[i] = rune(c)
+			i++
+		}
+	}
+
+	return string(out)
+}
