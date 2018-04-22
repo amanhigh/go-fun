c0d486a56414aed5408e4fe228d37099935ce65a
Amanpreet Singh <coool.aman@gmail.com>
2018-04-23 00:15:45 +0530

    Anagram Practice

---
+++ learn/algos/practice/anagrams.go
@@ -0,0 +1,25 @@
+package practice
+
+import (
+	"strings"
+	"fmt"
+	"unicode"
+)
+
+func AnagramGroups(words []string) map[string][]string {
+	anagramMap := map[string][]string{}
+	for _, word := range words {
+		fingerPrint := strings.Replace(fmt.Sprint(fingerPrint(word)), " ", "", -1)
+		anagramMap[fingerPrint] = append(anagramMap[fingerPrint], word)
+	}
+	return anagramMap
+}
+
+func fingerPrint(word string) (fingerPrint []int) {
+	fingerPrint=make([]int,26)
+	for _,c := range word{
+		cc:=unicode.ToLower(c)
+		fingerPrint[cc-'a']++
+	}
+	return
+}
