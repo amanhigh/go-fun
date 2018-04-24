50f70772264e2415aaaedb8b37b412b945a09bc4
Amanpreet Singh <coool.aman@gmail.com>
2018-04-24 20:32:20 +0530

    Fixed Read All Lines

--- util/fs.go
+++ util/fs.go
@@ -69,7 +69,7 @@ func ReplaceContent(path string, findRegex string, replace string) {
 
 func ReadAllLines(filePath string) []string {
 	if content, err := ioutil.ReadFile(filePath); err == nil {
-		lines := strings.Split(string(content), "\n")
+		lines := strings.Split(string(content), "\r\n")
 		return lines
 	} else {
 		log.WithFields(log.Fields{"Error": err}).Error("Error Reading File")
