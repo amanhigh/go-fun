8e45f2c2546ea6867e6aff94f0c4107d885c24f0
Amanpreet Singh <coool.aman@gmail.com>
2018-05-04 21:13:11 +0530

    Bubble Sort

---
+++ learn/algos/sort/bubble.go
@@ -0,0 +1,18 @@
+package sort
+
+/**
+O(n^2) Runtime, O(1) Space Complexity
+Each iteration Bubble Largest Element to end of array
+*/
+func BubbleSort(ints []int, n int) ([]int, int) {
+	swapCount := 0
+	for i := 0; i < n; i++ {
+		for j := i; j < n; j++ {
+			if ints[i] > ints[j] {
+				ints[i], ints[j] = ints[j], ints[i]
+				swapCount++
+			}
+		}
+	}
+	return ints, swapCount
+}
