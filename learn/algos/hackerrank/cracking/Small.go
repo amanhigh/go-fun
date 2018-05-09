a6fabd67fcd5b70be863b26e796792bedfa724f8
Amanpreet Singh <coool.aman@gmail.com>
2018-05-09 23:25:44 +0530

    Fib with Memorization

--- learn/algos/hackerrank/cracking/Small.go
+++ learn/algos/hackerrank/cracking/Small.go
@@ -8,3 +8,20 @@ func LeftRotate(input []int, rotationCount int) (rotatedArray []int) {
 	}
 	return
 }
+
+var mem []int
+
+//mem = make([]int, n+1)
+
+func Fibonacci(n int) (result int) {
+	memFib := mem[n]
+	if n == 0 || n == 1 {
+		result = n
+	} else if memFib != 0 {
+		result = memFib
+	} else {
+		result = Fibonacci(n-1) + Fibonacci(n-2)
+		mem[n] = result
+	}
+	return
+}
