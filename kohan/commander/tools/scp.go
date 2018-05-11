f7b8120833b23d2d72cfe06f97a21fd7e45293a7
Amanpreet Singh <amanpreet.singh@flipkart.com>
2018-05-11 16:32:30 +0530

    Added Log

--- kohan/commander/tools/scp.go
+++ kohan/commander/tools/scp.go
@@ -1,11 +1,16 @@
 package tools
 
-import "fmt"
+import (
+	"fmt"
 
-func Sync(remoteHost string, srcDir string, targetDir string, targetHosts []string) {
+	"github.com/amanhigh/go-fun/util"
+)
+
+func Sync(srcHost string, srcDir string, targetDir string, targetHosts []string) {
 	for _, target := range targetHosts {
+		util.PrintSkyBlue(fmt.Sprintf("Syncing: %v:%v -> %v:%v", srcHost, srcDir, target, targetDir))
 		RunCommandPrintError(fmt.Sprintf("ssh %v 'mkdir -p ~/sync'", target))
-		RunCommandPrintError(fmt.Sprintf("ssh -A -t %v 'scp -rC %v/* %v:~/sync'", remoteHost, srcDir, target))
+		RunCommandPrintError(fmt.Sprintf("ssh -A -t %v 'scp -rC %v/* %v:~/sync'", srcHost, srcDir, target))
 		RunCommandPrintError(fmt.Sprintf("ssh %v 'sudo mv ~/sync/* %v;rm -rf ~/sync'", target, targetDir))
 	}
 
