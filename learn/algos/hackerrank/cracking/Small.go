c813075d1a61b350f3032446350353dd57673d7b
Amanpreet Singh <coool.aman@gmail.com>
2018-05-15 11:00:44 +0530

    Solved Lonely Int

--- learn/algos/hackerrank/cracking/Small.go
+++ learn/algos/hackerrank/cracking/Small.go
@@ -25,3 +25,20 @@ func Fibonacci(n int) (result int) {
 	}
 	return
 }
+
+/**
+1) Any number xor'd with itself will give zero. 2) Any number xor'd with zero will give the number. 3) We are told there is an odd number of numbers in the array and they are all pairs of the same number, apart from one.
+
+So if we xor all the numbers in the array together then any which are the same will cancel out - and give zero as the result of all the xors.
+
+Then we are left with the unique number, which xor's with zero and so gives the unique number as the answer.
+
+Eg. 1,1,2 -> Answer: 2
+*/
+func FindLonely(ints []int) int {
+	lonelyInt := 0
+	for _, i := range ints {
+		lonelyInt ^= i
+	}
+	return lonelyInt
+}
