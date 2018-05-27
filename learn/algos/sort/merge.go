4f350b2f2dd7ffcd5c7f17ba4d5228bc1dbeb2af
Amanpreet Singh <coool.aman@gmail.com>
2018-05-27 15:07:52 +0530

    Added Inversion Logic to Merge Sort

--- learn/algos/sort/merge.go
+++ learn/algos/sort/merge.go
@@ -1,31 +1,33 @@
 package sort
 
-import "fmt"
-
-func MergeSort(input []int, start int, end int) {
+func MergeSort(input []int, start int, end int) (inversion int) {
 	/* End If we have Single Element Left */
 	if start < end {
 		/* Break Problem */
 		mid := (start + end) / 2
 		/* Solve Subproblems */
-		MergeSort(input, start, mid)
-		MergeSort(input, mid+1, end)
-		Merge(input, start, mid, end)
+		inversion += MergeSort(input, start, mid)
+		inversion += MergeSort(input, mid+1, end)
+		inversion += Merge(input, start, mid, end)
 	}
+	return
 }
 
-func Merge(input []int, start int, mid int, end int) {
+func Merge(input []int, start int, mid int, end int) (inversion int) {
 	result := make([]int, end-start+1)
 	i, j, k := start, mid+1, 0
 
 	/* Copy Minimum of Left & Right */
 	for i <= mid && j <= end {
-		if input[i] < input[j] {
+		/* Made Less than equal to consider inversions only if not equal */
+		if input[i] <= input[j] {
 			result[k] = input[i]
 			i++
 			k++
 		} else {
 			result[k] = input[j]
+			/* https://www.youtube.com/watch?v=k9RQh21KrH8 */
+			inversion += mid - i + 1
 			j++
 			k++
 		}
@@ -42,6 +44,7 @@ func Merge(input []int, start int, mid int, end int) {
 		k++
 	}
 
-	fmt.Println("Range:", start, mid, end, input[start:end+1], input[start:mid+1], input[mid+1:end+1], result)
+	//fmt.Println("Range:", start, mid, end, input[start:end+1], input[start:mid+1], input[mid+1:end+1], result, inversion)
 	copy(input[start:end+1], result)
+	return
 }
