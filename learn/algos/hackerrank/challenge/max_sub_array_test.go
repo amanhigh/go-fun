c4cf3288e08a2e215cad42251d29af4e1233771c
Amanpreet Singh <coool.aman@gmail.com>
2018-06-27 11:50:27 +0530

    Added Test Case

--- learn/algos/hackerrank/challenge/max_sub_array_test.go
+++ learn/algos/hackerrank/challenge/max_sub_array_test.go
@@ -6,6 +6,8 @@ import (
 
 	"bufio"
 
+	"fmt"
+
 	. "github.com/amanhigh/go-fun/learn/algos/hackerrank/challenge"
 	"github.com/amanhigh/go-fun/util"
 	"github.com/amanhigh/go-fun/util/helper"
@@ -34,12 +36,34 @@ var _ = Describe("MaxSubArray", func() {
 		for i := 0; i < n; i++ {
 			_, ints := helper.ReadCountInts(inScan)
 			expected := util.ReadInts(outScan, 2)
-			Expect(MaxSubArray(ints)).To(Equal(expected))
+			Expect(MaxSubArray(ints)).To(Equal(expected), fmt.Sprintf("Input: %v Expected: %v", ints, expected))
 		}
 	})
 
 	It("should be correct", func() {
 		inScan = util.NewStringScanner(input)
 		outScan = util.NewStringScanner(output)
+	})
+
+	It("should be correct 1", func() {
+		inScan = util.NewStringScanner(`6
+1
+1
+6
+-1 -2 -3 -4 -5 -6
+2
+1 -2
+3
+1 2 3
+1
+-10
+6
+1 -1 -1 -1 -1 5`)
+		outScan = util.NewStringScanner(`1 1
+-1 -1
+1 1
+6 6
+-10 -10
+5 6`)
 	})
 })
