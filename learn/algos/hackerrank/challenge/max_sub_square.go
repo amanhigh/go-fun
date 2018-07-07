c5c5fada73f8a2d049ba829ef32d70b4caa4eda9
Amanpreet Singh <coool.aman@gmail.com>
2018-07-07 14:20:04 +0530

    Used Kadanes Algo

--- learn/algos/hackerrank/challenge/max_sub_square.go
+++ learn/algos/hackerrank/challenge/max_sub_square.go
@@ -1,22 +1,42 @@
 package challenge
 
 import (
+	"fmt"
 	"math"
 )
 
 /**
 cooridantes = top,left,bottom,right
 */
-func MaximumSumSubSquare(input [][]int) (coordinates []int, sum int) {
-	return MaximumSumSubSquareBruteForce(input)
+func MaximumSumSubSquare(input [][]int, n int) (coordinates []int, maxSum int) {
+	return MaximumSumSubSquareSmart(input, n)
+}
+
+func MaximumSumSubSquareSmart(input [][]int, n int) (coordinates []int, maxSum int) {
+	maxSum = -math.MaxInt32
+	for jStart := 0; jStart < n; jStart++ {
+		columnSum := make([]int, n)
+		for jEnd := jStart; jEnd < n; jEnd++ {
+			/* Sum Column to previous Sum */
+			for i := 0; i < n; i++ {
+				columnSum[i] += input[i][jEnd]
+			}
+			sum := KadensAlgorithm(columnSum)[0]
+			if sum > maxSum {
+				maxSum = sum
+				coordinates = []int{0, jStart, 0, jEnd}
+			}
+			fmt.Println(jStart, jEnd, columnSum, sum)
+		}
+	}
+	return
 }
 
 /**
 Vary top-left and bottom-right for all possible combinations and sum.
 O(n^4) or O(n^2m^2) incase fo rectangle
 */
-func MaximumSumSubSquareBruteForce(input [][]int) (coordinates []int, maxSum int) {
-	n := len(input)
+func MaximumSumSubSquareBruteForce(input [][]int, n int) (coordinates []int, maxSum int) {
 	maxSum = -math.MaxInt32
 	for i := 0; i < n; i++ {
 		for j := 0; j < n; j++ {
