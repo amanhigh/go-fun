5b0c0c8234dad25c792153273a3739fac3afa4a6
Amanpreet Singh <coool.aman@gmail.com>
2018-07-07 11:48:04 +0530

    Added Read Matrix

--- util/helper/test.go
+++ util/helper/test.go
@@ -11,3 +11,11 @@ func ReadCountInts(scanner *bufio.Scanner) (n int, ints []int) {
 	ints = util.ReadInts(scanner, n)
 	return
 }
+
+func ReadMatrix(scanner *bufio.Scanner, n int) (matrix [][]int) {
+	matrix = make([][]int, n)
+	for i := 0; i < n; i++ {
+		matrix[i] = util.ReadInts(scanner, n)
+	}
+	return
+}
