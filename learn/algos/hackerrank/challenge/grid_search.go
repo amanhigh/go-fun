979513aea36f121ac8b5c744f9450fa352935ba1
Amanpreet Singh <coool.aman@gmail.com>
2018-07-12 10:18:52 +0530

    Added Grid Search

---
+++ learn/algos/hackerrank/challenge/grid_search.go
@@ -0,0 +1,12 @@
+package challenge
+
+import "fmt"
+
+/**
+https://www.hackerrank.com/challenges/the-grid-search/problem
+*/
+
+func GridSearch(grid, search [][]int) (ok bool) {
+	fmt.Println(grid, search)
+	return
+}
