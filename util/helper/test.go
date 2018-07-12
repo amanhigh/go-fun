b847bcbd7d08966b4f65730275474968f1d8d68d
Amanpreet Singh <coool.aman@gmail.com>
2018-07-12 10:18:43 +0530

    Added Read Matrix With Dimension

--- util/helper/test.go
+++ util/helper/test.go
@@ -3,6 +3,8 @@ package helper
 import (
 	"bufio"
 
+	"fmt"
+
 	"github.com/amanhigh/go-fun/util"
 )
 
@@ -17,5 +19,14 @@ func ReadMatrix(scanner *bufio.Scanner, n, m int) (matrix [][]int) {
 	for i := 0; i < n; i++ {
 		matrix[i] = util.ReadInts(scanner, m)
 	}
+	return
+}
+
+func ReadMatrixWithDimensions(scanner *bufio.Scanner) (matrix [][]int, n, m int) {
+	ints := util.ReadInts(scanner, 2)
+	fmt.Println(ints)
+	n = ints[0]
+	m = ints[1]
+	matrix = ReadMatrix(scanner, n, m)
 	return
 }
