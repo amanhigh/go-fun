fb0e1ba961f8eb88a65d7dd54e5dd641f83b877b
Amanpreet Singh <coool.aman@gmail.com>
2018-07-14 13:00:34 +0530

    Read Input

--- learn/algos/hackerrank/challenge/grid_search.go
+++ learn/algos/hackerrank/challenge/grid_search.go
@@ -6,7 +6,7 @@ import "fmt"
 https://www.hackerrank.com/challenges/the-grid-search/problem
 */
 
-func GridSearch(grid, search [][]int) (ok bool) {
+func GridSearch(grid, search []string) (ok bool) {
 	fmt.Println(grid, search)
 	return
 }
