45826d8e8f3cd3f36bac364bf57ec057c8f4e2d2
Amanpreet Singh <coool.aman@gmail.com>
2018-09-20 18:28:22 +0530

    Added Etcd based confd

--- learn/confd/run.sh
+++ learn/confd/run.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-confd -onetime -backend file -file myapp.yaml -confdir .
+
+#File
+confd -confdir . -onetime -backend file -file myapp.yaml
+
+#Etcd
+#etcdctl set /myapp/database/url db.example.com
+#etcdctl set /myapp/database/user rob
+#confd -confdir . -watch -backend etcd -node http://192.168.99.100:2379
