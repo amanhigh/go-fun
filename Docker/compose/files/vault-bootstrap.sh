c92cf41dcd220827cf546e63b8bde26b5d4f63d5
Amanpreet Singh <coool.aman@gmail.com>
2019-01-04 13:43:33 +0530

    Enabled Transit

--- Docker/compose/files/vault-bootstrap.sh
+++ Docker/compose/files/vault-bootstrap.sh
@@ -8,3 +8,14 @@ vault secrets disable secret &&
 vault secrets enable -version=1 -path=secret kv
 
 # Enable Transit
+vault secrets enable transit
+
+#Encrypt Decrypt Data
+#vault write -f transit/keys/my-key #Generate New Key
+
+#vault write transit/encrypt/my-key plaintext=$(base64 <<< "my secret data") #Encrypt
+#vault write transit/decrypt/my-key ciphertext=<CIPHER> #Decrypt to Base64
+#base64 --decode <<< <BASE_64_TEXT #Get Plain Text
+
+#vault write -f transit/keys/my-key/rotate #Rotate Key
+#vault write transit/rewrap/my-key ciphertext=<CIPHER> #Re-encrypt data using rotate key
