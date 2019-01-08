c98f4f98193de885249c3c1a0e82acd087fd717d
Amanpreet Singh <coool.aman@gmail.com>
2019-01-08 16:21:29 +0530

    Set Config

--- util/db.go
+++ util/db.go
@@ -12,6 +12,11 @@ func NewDb(path string) (db *gorm.DB) {
 	if dbConf, err = goose.NewDBConf(path, "development", "mysql"); err == nil {
 		db, err = gorm.Open(dbConf.PgSchema, dbConf.Driver.OpenStr)
 		//db.LogMode(true)
+
+		db.DB().SetMaxIdleConns(5)
+		db.DB().SetMaxOpenConns(20)
+
+		//goose.RunMigrations(dbConf,path,1)
 	}
 
 	if err != nil {
