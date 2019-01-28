328b9d39bde50bd95519945090d1e1f49c5b72af
Amanpreet Singh <coool.aman@gmail.com>
2019-01-28 16:59:05 +0530

    Added Consul Fun
    Signed-off-by: Amanpreet Singh <coool.aman@gmail.com>

---
+++ learn/frameworks/consul/consul.go
@@ -0,0 +1,30 @@
+package consul
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/consul/api"
+)
+
+func ConsulFun() {
+	// Get a new client
+	if client, err := api.NewClient(&api.Config{Address: "docker:8500"}); err == nil {
+		// Get a handle to the KV API
+		kv := client.KV()
+
+		// PUT a new KV pair
+		key := "aman/1"
+		p := api.KVPair{Key: key, Value: []byte("2000")}
+		if _, err = kv.Put(&p, nil); err == nil {
+			if pair, _, err := kv.Get(key, nil); err == nil {
+				fmt.Printf("KV: %v %s\n", pair.Key, pair.Value)
+				//_, err = kv.Delete(key, nil)
+			}
+
+		}
+		// Log Error
+		if err != nil {
+			panic(err)
+		}
+	}
+}
