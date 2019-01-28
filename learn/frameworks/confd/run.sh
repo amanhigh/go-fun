f7a6a4ee27e444f69b89253968357402bab257d8
Amanpreet Singh <coool.aman@gmail.com>
2019-01-28 16:43:10 +0530

    Added Consul Backend
    Signed-off-by: Amanpreet Singh <coool.aman@gmail.com>

--- learn/frameworks/confd/run.sh
+++ learn/frameworks/confd/run.sh
@@ -7,3 +7,7 @@ confd -confdir . -onetime -backend file -file myapp.yaml
 #etcdctl set /myapp/database/url db.example.com
 #etcdctl set /myapp/database/user rob
 #confd -confdir . -watch -backend etcd -node http://docker:2379
+
+#curl -X PUT -d 'db.example.com' http://docker:8500/v1/kv/myapp/database/url
+#curl -X PUT -d 'rob' http://docker:8500/v1/kv/myapp/database/user
+#confd -confdir . -watch -backend consul -node http://docker:8500
