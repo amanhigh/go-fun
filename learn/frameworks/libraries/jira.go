402d6cc9579d84e2df9f1580010c24c4e37ace8f
Amanpreet Singh <coool.aman@gmail.com>
2019-02-25 13:13:30 +0530

    Added Jira Fun

---
+++ learn/frameworks/libraries/jira.go
@@ -0,0 +1,48 @@
+package libraries
+
+import (
+	"fmt"
+
+	"github.com/andygrunwald/go-jira"
+)
+
+func JiraFun() {
+	var err error
+	var jiraClient *jira.Client
+	var issue *jira.Issue
+
+	//tp := jira.BasicAuthTransport{
+	//	Username: "aman",
+	//	Password: "token",
+	//}
+
+	// Pass tp.Client() for issue creation
+	if jiraClient, err = jira.NewClient(nil, "https://issues.apache.org/jira/"); err == nil {
+		if issue, _, err = jiraClient.Issue.Get("MESOS-3325", nil); err == nil {
+			fmt.Printf("%s: %+v\n", issue.Key, issue.Fields.Summary)
+			fmt.Printf("Type: %s\n", issue.Fields.Type.Name)
+			fmt.Printf("Priority: %s\n", issue.Fields.Priority.Name)
+		}
+	}
+
+	/* Create */
+	//newIssue := jira.Issue{
+	//	Fields: &jira.IssueFields{
+	//		Description: "Demo",
+	//		Type: jira.IssueType{
+	//			Name: "Task",
+	//		},
+	//		Project: jira.Project{
+	//			Key: "DEMO_PROJECT",
+	//		},
+	//		Summary: "Demo Issue",
+	//	},
+	//}
+	//if issue, _, err = jiraClient.Issue.Create(&newIssue); err == nil {
+	//	fmt.Println(issue)
+	//}
+	//fmt.Println(issue.ID)
+
+	fmt.Println("Error: ", err)
+
+}
