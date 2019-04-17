1b913c377ab0b2480436b4cfd4f91b98d32e533b
Amanpreet Singh <coool.aman@gmail.com>
2019-04-17 18:22:28 +0530

    Decode Json Once

--- perf/lua/fun-app.lua
+++ perf/lua/fun-app.lua
@@ -6,21 +6,23 @@
 -- To change this template use File | Settings | File Templates.
 --
 require "lua.lib.library"
-JSON = (loadfile "./lua/lib/JSON.lua")()
 
 local header = { ["Content-Type"] = "application/json" }
 local personCount = 1000
 
+init = function()
+    payload = file_get_contents("./request/person.json")
+    req = JSON:decode(payload);
+end
+
 request = function()
     local api = "/person"
-    local payload = file_get_contents("./request/person.json")
 
     --local age = math.random(1,personCount)
-    --local req = JSON:decode(payload);
     --req.name="Aman-" .. age
     --req.age=age
     --payload=JSON:encode(req)
 
-    --print(api .. payload)
+    print(api .. payload)
     return wrk.format("POST", api, header, payload)
 end
