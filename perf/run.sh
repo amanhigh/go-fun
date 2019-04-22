3e39c53603c19c5663fe3054466a7148df15af1d
Amanpreet Singh <coool.aman@gmail.com>
2019-04-22 13:34:01 +0530

    Matched Thread and Concurrency.

--- perf/run.sh
+++ perf/run.sh
@@ -3,6 +3,11 @@ echo "Usage: perf.sh <script> <host:port> <concurrency> <timeout (minutes)>"
 script="$1.lua"
 host=${2:-localhost\:8080}
 con=${3:-10}
-min=${4:-5}
-echo -en "\n\033[1;32m Running Pref(${script}) on $host for $min Minute with $con Concurrency \033[0m \n"
-wrk -t2 -c$con -d${min}m --latency -s ./lua/${script} http://$host
+if [ $con == 1 ];then
+       th=1;
+    else
+        th=2;
+fi
+min=${4:-1}
+echo -en "\n\033[1;32m Running Pref(${script}) on $host for $min Minute with $con Concurrency ($th Threads) \033[0m \n"
+wrk -t$th -c$con -d${min}m --latency -s ./lua/${script} http://$host
