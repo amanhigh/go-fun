7df9b0d1e781d92812974be7b5f50b5a3a53a5a3
Amanpreet Singh <coool.aman@gmail.com>
2019-04-27 12:33:14 +0530

    Added Parallel Kitchen

--- learn/frameworks/simpy/food-delivery/entities/restaurant.py
+++ learn/frameworks/simpy/food-delivery/entities/restaurant.py
@@ -4,18 +4,23 @@ import simpy
 
 
 class Restaurant:
-    def __init__(self, env, id):
+    def __init__(self, env, id,kitchencount):
         self.id = id
         self.env = env
         self.name = "Food Point %d" % id
         self.orderStore = simpy.FilterStore(env)
+        self.kitchen = simpy.Resource(env, kitchencount)
 
     def prepare_food(self, order):
         logging.info("%s: Order: %d received at %s" % (self.name, order.id, self.env.now))
-        yield self.env.timeout(order.dish.prep_time())
-        yield self.orderStore.put(order)
+        with self.kitchen.request() as req:
+            yield req
 
-        logging.info("%s: Order: %d prepared at %s" % (self.name, order.id, self.env.now))
+            logging.info("%s: Order: %d prep-started at %s" % (self.name, order.id, self.env.now))
+            yield self.env.timeout(order.dish.prep_time())
+            yield self.orderStore.put(order)
+
+            logging.info("%s: Order: %d cooked at %s" % (self.name, order.id, self.env.now))
 
     def handover_food(self, order):
         yield self.orderStore.get(lambda o: o.id == order.id)
