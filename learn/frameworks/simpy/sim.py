19ddca7243afd21fbd1a65e97a5e0c1d6b1735ae
Amanpreet Singh <coool.aman@gmail.com>
2019-04-28 01:18:47 +0530

    Added Producer Consumer

--- learn/frameworks/simpy/sim.py
+++ learn/frameworks/simpy/sim.py
@@ -52,6 +52,21 @@ def driver(env, car):
     # Interrupt Car Process
     car.action.interrupt()
 
+# Producer Consumer using Store
+def producer(env, store):
+    for i in range(100):
+        yield env.timeout(2)
+        yield store.put('spam %s' % i)
+        print('Produced spam at', env.now)
+
+
+def consumer(name, env, store):
+    while True:
+        yield env.timeout(1)
+        print(name, 'requesting spam at', env.now)
+        item = yield store.get()
+        print(name, 'got', item, 'at', env.now)
+
 
 env = simpy.Environment()
 
@@ -63,4 +78,9 @@ for i in range(10):
 
 # Driver Interrupts last Car
 env.process(driver(env, car))
+
+# store = simpy.Store(env, capacity=2)
+# prod = env.process(producer(env, store))
+# consumers = [env.process(consumer(i, env, store)) for i in range(2)]
+
 env.run(until=50)
