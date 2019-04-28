8bef569a391111ebca5517f9f1bd7322d8ab77b8
Amanpreet Singh <coool.aman@gmail.com>
2019-04-28 22:36:01 +0530

    Moved Http Client

--- apps/common/clients/http_client_instances.go
+++ apps/common/clients/http_client_instances.go
@@ -6,6 +6,24 @@ import (
 	"github.com/amanhigh/go-fun/apps/models/config"
 )
 
+const (
+	DIAL_TIMEOUT     = 2 * time.Second
+	REQUEST_TIMEOUT  = 10 * time.Second
+	IDLE_TIMEOUT     = time.Minute
+	IDLE_CONNECTIONS = 64
+)
+
+var (
+	DefaultHttpClientConfig = config.HttpClientConfig{
+		DialTimeout:            DIAL_TIMEOUT,
+		RequestTimeout:         REQUEST_TIMEOUT,
+		IdleConnectionTimeout:  IDLE_TIMEOUT,
+		IdleConnectionsPerHost: IDLE_CONNECTIONS,
+		KeepAlive:              true,
+		Compression:            false,
+	}
+)
+
 var TestHttpClient = NewHttpClient(config.HttpClientConfig{
 	DialTimeout:            time.Millisecond * 200,
 	RequestTimeout:         time.Second,
