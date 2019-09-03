5497c568f06f727d31ab86cf25ce58cce8bbd5fe
Amanpreet Singh <coool.aman@gmail.com>
2019-09-03 12:28:41 +0530

    Moved commands to script

---
+++ Docker/compose/files/scripts/mongo.js
@@ -0,0 +1,27 @@
+ //Create Admin:
+ use; admin;
+ db.createUser(
+      {
+        user: "admin",
+            pwd: "changeme",
+            roles: [ { role: "root", db: "admin" } ]
+       }
+  );
+
+ //Replication Status
+ rs.status();
+
+ //Initiate Replicas
+ // rs.initiate({
+ //     _id: "shard1",
+ //     version: 1,
+ //     members: [
+ //         { _id: 0, host: "mongo1:27017" },
+ //         { _id: 1, host: "mongo2:27017" },
+ //         { _id: 2, host: "mongo3:27017" }
+ //     ]
+ // });
+
+ //Member Management
+ // rs.add( { host: "mongo1:27017"} ); //Add Member (only on primary as admin)
+ // rs.remove("mongo1:27017"); // Remove Member (only on primary as admin)
