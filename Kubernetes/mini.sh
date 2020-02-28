4cdbe23d1e7b8aaa7506771cc24a3eed6fd75a68
Amanpreet Singh <coool.aman@gmail.com>
2020-02-28 17:40:22 +0530

    Added Swagger Proxy
    Signed-off-by: Amanpreet Singh <coool.aman@gmail.com>

--- Kubernetes/mini.sh
+++ Kubernetes/mini.sh
@@ -1,7 +1,11 @@
 minikube delete;
-minikube start --extra-config="apiserver.service-account-api-audiences=api" \
+minikube start \
+--extra-config="apiserver.enable-swagger-ui=true" \
+--extra-config="apiserver.service-account-api-audiences=api" \
 --extra-config="apiserver.service-account-issuer=api" \
 --extra-config="apiserver.service-account-key-file=/var/lib/minikube/certs/sa.pub" \
 --extra-config="apiserver.service-account-signing-key-file=/var/lib/minikube/certs/sa.key";
 
-minikube dashboard;
+minikube dashboard &
+kubectl proxy --port=8091 &
+echo -en "\033[1;32m Swagger: http://localhost:8091/swagger-ui \033[0m \n"
