04b6e58d3428209b40ef14ebfc4d883295df1af9
Amanpreet Singh <coool.aman@gmail.com>
2020-03-03 12:44:59 +0530

    Added Marshal/Unmarshal
    Signed-off-by: Amanpreet Singh <coool.aman@gmail.com>

--- learn/frameworks/security/jwt.go
+++ learn/frameworks/security/jwt.go
@@ -1,10 +1,12 @@
 package security
 
 import (
+	"crypto"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
+	"errors"
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"time"
@@ -15,17 +17,34 @@ func JwtFun() {
 	privateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
 	publicKey := privateKey.Public()
 
-	//Marshal to PEM Format
-	if pubkey_bytes, err := x509.MarshalPKIXPublicKey(publicKey); err == nil {
-		pubkey_pem := pem.EncodeToMemory(
-			&pem.Block{
-				Type:  "RSA PUBLIC KEY",
-				Bytes: pubkey_bytes,
-			},
-		)
-		fmt.Println("Public Pem", string(pubkey_pem))
-	}
+	//Marshal to Private Key To PEM Format
+	privatekeyPem := marshalPrivateKey(privateKey)
+	fmt.Println("Private Pem", string(privatekeyPem))
 
+	//Unmarshal Private Key Pem
+	privateKey, _ = unmarshalRSAPrivateKey(string(privatekeyPem))
+
+	//Marshal to Public Key PEM Format
+	pubkeyPem := marshalPublicKey(publicKey)
+	fmt.Println("Public Pem", string(pubkeyPem))
+
+	//Unmarshal Public Key Pem
+	publicKey, _ = unmarshalRSAPublicKey(string(pubkeyPem))
+
+	//Generate Token String
+	tokenString, err := generateToken(privateKey)
+	fmt.Println("JwtToken", tokenString, err)
+
+	//Parse Token
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (i interface{}, err error) {
+		return publicKey, nil
+	})
+
+	claims := token.Claims.(jwt.MapClaims)
+	fmt.Println("ParsedToken", claims["purpose"], claims, claims.Valid(), err)
+}
+
+func generateToken(privateKey *rsa.PrivateKey) (string, error) {
 	// Create a new token object, specifying signing method and the claims
 	// you would like it to contain.
 	token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
@@ -38,12 +57,61 @@ func JwtFun() {
 
 	// Sign and get the complete encoded token as a string using the secret
 	tokenString, err := token.SignedString(privateKey)
-	fmt.Println("JwtToken", tokenString, err)
+	return tokenString, err
+}
 
-	token, err = jwt.Parse(tokenString, func(token *jwt.Token) (i interface{}, err error) {
-		return publicKey, nil
-	})
+func marshalPublicKey(publicKey crypto.PublicKey) []byte {
+	if pubkeyBytes, err := x509.MarshalPKIXPublicKey(publicKey); err == nil {
+		pubkeyPem := pem.EncodeToMemory(
+			&pem.Block{
+				Type:  "RSA PUBLIC KEY",
+				Bytes: pubkeyBytes,
+			},
+		)
+		return pubkeyPem
+	}
+	return nil
+}
 
-	claims := token.Claims.(jwt.MapClaims)
-	fmt.Println("ParsedToken", claims["purpose"], claims, claims.Valid(), err)
+func marshalPrivateKey(privateKey *rsa.PrivateKey) []byte {
+	privatekeyBytes := x509.MarshalPKCS1PrivateKey(privateKey)
+	privatekeyPem := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: privatekeyBytes,
+		},
+	)
+	return privatekeyPem
+}
+
+func unmarshalRSAPublicKey(pemPubKey string) (key *rsa.PublicKey, err error) {
+	var block *pem.Block
+	var pub interface{}
+	if block, _ = pem.Decode([]byte(pemPubKey)); block != nil {
+		if pub, err = x509.ParsePKIXPublicKey(block.Bytes); err == nil {
+			switch key := pub.(type) {
+			case *rsa.PublicKey:
+				return key, nil
+			default:
+				err = errors.New("unknown type of public key")
+			}
+		}
+	}
+	return
+}
+
+func unmarshalRSAPrivateKey(pemPrivateKey string) (key *rsa.PrivateKey, err error) {
+	var block *pem.Block
+	var private interface{}
+	if block, _ = pem.Decode([]byte(pemPrivateKey)); block != nil {
+		if private, err = x509.ParsePKCS1PrivateKey(block.Bytes); err == nil {
+			switch key := private.(type) {
+			case *rsa.PrivateKey:
+				return key, nil
+			default:
+				err = errors.New("unknown type of private key")
+			}
+		}
+	}
+	return
 }
