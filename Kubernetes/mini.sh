4954f9ed4c0446eb646091015d47bb85f586c65d
Amanpreet Singh <coool.aman@gmail.com>
2020-03-03 12:51:43 +0530

    Added Helpful Commands
    Signed-off-by: Amanpreet Singh <coool.aman@gmail.com>

--- Kubernetes/mini.sh
+++ Kubernetes/mini.sh
@@ -9,3 +9,12 @@ minikube start \
 minikube dashboard &
 kubectl proxy --port=8091 &
 echo -en "\033[1;32m Swagger: http://localhost:8091/swagger-ui \033[0m \n"
+
+
+# Helpful Commands
+# Docker Imageto Minikube: eval $(minikube docker-env); docker build -t fun-app .
+# Port Forward (Local Port 9090 to Container Port 8080) - kubectl port-forward `kubectl get pods -o name | grep fun-app | head  -1` 9090:8080
+# Logs - kubectl logs `kubectl get pods -o name | grep fun-app | head  -1` -f
+# Login - kubectl -it exec `kubectl get pods -o name | grep fun-app | head  -1` bash
+#
+#
