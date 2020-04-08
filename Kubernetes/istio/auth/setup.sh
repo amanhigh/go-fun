7d15d34bbf444d425b382f5c8f4c4189726cb4a6
Amanpreet Singh <coool.aman@gmail.com>
2020-04-08 17:42:41 +0530

    Added Delete Command
    Signed-off-by: Amanpreet Singh <coool.aman@gmail.com>

--- Kubernetes/istio/auth/setup.sh
+++ Kubernetes/istio/auth/setup.sh
@@ -1,3 +1,6 @@
+#kubectl delete ns foo bar legacy
+
+echo -en "\033[1;32m Setting Up Foo, Bar with Proxy \033[0m \n"
 kubectl create ns foo
 kubectl apply -f <(istioctl kube-inject -f httpbin.yaml) -n foo
 kubectl apply -f <(istioctl kube-inject -f sleep.yaml) -n foo
