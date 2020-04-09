ecb272ac7a59c21837aaf5b1fd8c6a6aa68245b9
Amanpreet Singh <coool.aman@gmail.com>
2020-04-09 21:46:57 +0530

    Added Primary Cluster Extra Config on Default Profile

--- Kubernetes/istio/istio.sh
+++ Kubernetes/istio/istio.sh
@@ -1,7 +1,19 @@
+export MAIN_CLUSTER_NAME=minikube
+export REMOTE_CLUSTER_NAME=secondary
+
+export MAIN_CLUSTER_NETWORK=minikube-network
+export REMOTE_CLUSTER_NETWORK=secondary-network
+
 echo -en "\033[1;32m Configuring Istio: Primary \033[0m \n"
 minikube profile minikube
-istioctl manifest apply -f istio-primary.yaml
+istioctl manifest apply --set profile=default
+istioctl manifest apply -f istio-primary.yaml --set values.global.mtls.enabled=true
 kubectl label namespace default istio-injection=enabled
+
+echo -en "\033[1;32m Creating Certs \033[0m \n"
+kubectl create secret generic cacerts -n istio-system \
+  --from-file=./certs/ca-cert.pem --from-file=./certs/ca-key.pem \
+  --from-file=./certs/root-cert.pem --from-file=./certs/cert-chain.pem
 
 #istioctl manifest apply --set profile=demo
 #-- Dasboards
