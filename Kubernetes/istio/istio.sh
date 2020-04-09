c6e3a08870d784fa4a68011b8ede70ed106713c1
Amanpreet Singh <coool.aman@gmail.com>
2020-04-09 17:08:40 +0530

    Primary Cluster Setup

--- Kubernetes/istio/istio.sh
+++ Kubernetes/istio/istio.sh
@@ -1,8 +1,8 @@
-istioctl manifest apply --set profile=demo
+echo -en "\033[1;32m Configuring Istio: Primary \033[0m \n"
+minikube profile minikube
+istioctl manifest apply -f istio-primary.yaml
 kubectl label namespace default istio-injection=enabled
 
-sleep 10
-
-
+#istioctl manifest apply --set profile=demo
 #-- Dasboards
 #istioctl dashboard kiali
