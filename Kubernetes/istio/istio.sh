c5a6dd1d17ebe778e0c50bd4cc84bb6e6fe28909
Amanpreet Singh <coool.aman@gmail.com>
2020-04-09 14:27:41 +0530

    Commented Kiali Dashboard
    Signed-off-by: Amanpreet Singh <coool.aman@gmail.com>

--- Kubernetes/istio/istio.sh
+++ Kubernetes/istio/istio.sh
@@ -2,4 +2,7 @@ istioctl manifest apply --set profile=demo
 kubectl label namespace default istio-injection=enabled
 
 sleep 10
-istioctl dashboard kiali &
+
+
+#-- Dasboards
+#istioctl dashboard kiali
