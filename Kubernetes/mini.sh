c896fce939068d0db29b6b36ed5b707925187ede
Amanpreet Singh <coool.aman@gmail.com>
2020-04-09 13:58:23 +0530

    Made Proxy Foreground
    Signed-off-by: Amanpreet Singh <coool.aman@gmail.com>

--- Kubernetes/mini.sh
+++ Kubernetes/mini.sh
@@ -7,13 +7,12 @@ minikube start \
 --extra-config="apiserver.service-account-key-file=/var/lib/minikube/certs/sa.pub" \
 --extra-config="apiserver.service-account-signing-key-file=/var/lib/minikube/certs/sa.key";
 
-minikube dashboard &
-kubectl proxy --port=8091 &
-
 minikube ssh 'sudo cat /var/lib/minikube/certs/sa.pub'
+minikube dashboard --url=true &
 
 echo -en "\033[1;32m Dashboard: http://localhost:8091/api/v1/namespaces/kubernetes-dashboard/services/http:kubernetes-dashboard:/proxy/# \033[0m \n"
 echo -en "\033[1;32m Swagger: http://localhost:8091/swagger-ui \033[0m \n"
+kubectl proxy --port=8091
 
 
 
@@ -22,5 +21,5 @@ echo -en "\033[1;32m Swagger: http://localhost:8091/swagger-ui \033[0m \n"
 # Port Forward (Local Port 9090 to Container Port 8080) - kubectl port-forward `kubectl get pods -o name | grep fun-app | head  -1` 9090:8080
 # Logs - kubectl logs `kubectl get pods -o name | grep fun-app | head  -1` -f
 # Login - kubectl -it exec `kubectl get pods -o name | grep fun-app | head  -1` bash
-#
+# Delete All - kubectl delete all --all
 #
