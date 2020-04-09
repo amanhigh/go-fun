1e741ebad2d587fb0c8dca38fb2b3b2f0345ff0b
Amanpreet Singh <coool.aman@gmail.com>
2020-04-09 15:34:58 +0530

    Used Inject Label

--- Kubernetes/istio/auth/setup.sh
+++ Kubernetes/istio/auth/setup.sh
@@ -2,11 +2,16 @@
 
 echo -en "\033[1;32m Setting Up Foo, Bar with Proxy \033[0m \n"
 kubectl create ns foo
-kubectl apply -f <(istioctl kube-inject -f httpbin.yaml) -n foo
-kubectl apply -f <(istioctl kube-inject -f sleep.yaml) -n foo
+kubectl label namespace foo istio-injection=enabled
+kubectl apply -f httpbin.yaml -n foo
+kubectl apply -f sleep.yaml -n foo
+
 kubectl create ns bar
-kubectl apply -f <(istioctl kube-inject -f httpbin.yaml) -n bar
-kubectl apply -f <(istioctl kube-inject -f sleep.yaml) -n bar
+kubectl label namespace bar istio-injection=enabled
+kubectl apply -f httpbin.yaml -n bar
+kubectl apply -f sleep.yaml -n bar
+
+echo -en "\033[1;32m Setting Up Legacy Without Proxy \033[0m \n"
 kubectl create ns legacy
 kubectl apply -f httpbin.yaml -n legacy
 kubectl apply -f sleep.yaml -n legacy
