26a4ccb4e83c82e2f2d46f0ca6940845ca532d03
Amanpreet Singh <coool.aman@gmail.com>
2020-04-09 17:14:04 +0530

    Added Primary Token

--- Kubernetes/istio/auth/multi_auth.sh
+++ Kubernetes/istio/auth/multi_auth.sh
@@ -1,3 +1,8 @@
+minikube profile primary
+#Auth Headers
+echo -en "\033[1;32m Auth Headers: SPIFFE (primary.foo.sleep to primary.foo.httpbin) \033[0m \n"
+kubectl exec $(kubectl get pod -l app=sleep -n foo -o jsonpath={.items..metadata.name}) -c sleep -n foo -- curl http://httpbin.foo:8000/headers -s #| grep X-Forwarded-Client-Cert
+
 minikube profile secondary
 #Auth Headers
 echo -en "\033[1;32m Auth Headers: SPIFFE (secondary.bar.sleep to secondary.bar.httpbin) \033[0m \n"
