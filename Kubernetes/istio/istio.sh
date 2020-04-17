040091a3c2a4f8dc04afa732a3b09a7426bf6a1a
Amanpreet Singh <coool.aman@gmail.com>
2020-04-17 15:24:56 +0530

    Removed Variables

--- Kubernetes/istio/istio.sh
+++ Kubernetes/istio/istio.sh
@@ -1,9 +1,3 @@
-export MAIN_CLUSTER_NAME=minikube
-export REMOTE_CLUSTER_NAME=secondary
-
-export MAIN_CLUSTER_NETWORK=minikube-network
-export REMOTE_CLUSTER_NETWORK=secondary-network
-
 echo -en "\033[1;32m Configuring Istio: Primary \033[0m \n"
 minikube profile minikube
 istioctl manifest apply -f istio-primary.yaml
