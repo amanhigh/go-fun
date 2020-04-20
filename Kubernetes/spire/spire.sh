394873543e0bad7ea8b49b8233efac51e11a0adf
Amanpreet Singh <coool.aman@gmail.com>
2020-04-20 18:32:39 +0530

    Added Test Client

--- Kubernetes/spire/spire.sh
+++ Kubernetes/spire/spire.sh
@@ -3,6 +3,7 @@ kubectl -f ./spire-server.yaml apply
 echo -en "\033[1;32m Setting Up Spire Agent \033[0m \n"
 kubectl -f ./spire-agent.yaml apply
 
+sleep 10
 echo -en "\033[1;32m Mapping Agent Spiffe Id \033[0m \n"
 kubectl exec -n spire spire-server-0 -- \
     /opt/spire/bin/spire-server entry create \
@@ -15,11 +16,15 @@ kubectl exec -n spire spire-server-0 -- \
 echo -en "\033[1;32m Mapping Workload Spiffe Id \033[0m \n"
 kubectl exec -n spire spire-server-0 -- \
     /opt/spire/bin/spire-server entry create \
-    -spiffeID spiffe://example.org/ns/default/sa/default \
+    -spiffeID spiffe://example.org/ns/foo/sa/my-sleep \
     -parentID spiffe://example.org/ns/spire/sa/spire-agent \
-    -selector k8s:ns:default \
-    -selector k8s:sa:default
+    -selector k8s:ns:foo \
+    -selector k8s:sa:sleep
 
 echo -en "\033[1;32m Registered Entries \033[0m \n"
 kubectl exec -n spire spire-server-0 -- \
     /opt/spire/bin/spire-server entry show
+
+echo -en "\033[1;32m Setting Up Test Client \033[0m \n"
+kubectl create ns foo
+kubectl apply -f sleep.yaml -n foo
