01bbb9d4b3e0777834c7ea5bf2829dc64b17d8ee
Amanpreet Singh <coool.aman@gmail.com>
2020-04-22 15:30:32 +0530

    Decoded JWT

--- Kubernetes/spire/auth.sh
+++ Kubernetes/spire/auth.sh
@@ -1,16 +1,22 @@
-echo -en "\033[1;32m Generating Tokens (x509) \033[0m \n"
+JWT_PATH=/tmp
+echo -en "\033[1;32m Generating Tokens (x509 SVID) \033[0m \n"
 kubectl exec $(kubectl get pod -l app=sleep -n foo -o jsonpath={.items..metadata.name}) \
--c sleep -n foo -- /opt/spire/spire-agent api fetch -socketPath /run/spire/sockets/agent.sock
+  -c sleep -n foo -- /opt/spire/spire-agent api fetch -socketPath /run/spire/sockets/agent.sock
 
-echo -en "\033[1;32m Generating Tokens (JWT) \033[0m \n"
+echo -en "\033[1;32m Generating Tokens (JWT SVID) \033[0m \n"
 kubectl exec $(kubectl get pod -l app=sleep -n foo -o jsonpath={.items..metadata.name}) \
--c sleep -n foo -- /opt/spire/spire-agent api fetch jwt -audience spiffe://example.org/ns/foo/sa/my-httpbin -socketPath /run/spire/sockets/agent.sock > ./jwt; cat ./jwt;
+  -c sleep -n foo -- sh -c "/opt/spire/spire-agent api fetch jwt -audience spiffe://example.org/ns/foo/sa/my-httpbin -socketPath /run/spire/sockets/agent.sock > $JWT_PATH/jwt;
+cat $JWT_PATH/jwt;"
+
+echo -en "\033[1;32m Decoding JWT Token \033[0m \n"
+JWT_TOKEN=$(kubectl exec $(kubectl get pod -l app=sleep -n foo -o jsonpath={.items..metadata.name}) \
+  -c sleep -n foo -- head -2 $JWT_PATH/jwt | tail -1)
+jq -R 'split(".") | .[1] | @base64d | fromjson' <<< "$JWT_TOKEN"
 
 echo -en "\033[1;33m Validating Token (Invalid Audience) \033[0m \n"
 kubectl exec $(kubectl get pod -l app=sleep -n foo -o jsonpath={.items..metadata.name}) \
--c sleep -n foo -- /opt/spire/spire-agent api validate jwt -audience my-httpbin -socketPath /run/spire/sockets/agent.sock -svid `head -2 jwt | tail -1`
+  -c sleep -n foo -- /opt/spire/spire-agent api validate jwt -audience my-httpbin -socketPath /run/spire/sockets/agent.sock -svid $JWT_TOKEN
 
 echo -en "\033[1;32m Validating Token (Valid Audience) \033[0m \n"
 kubectl exec $(kubectl get pod -l app=sleep -n foo -o jsonpath={.items..metadata.name}) \
--c sleep -n foo -- /opt/spire/spire-agent api validate jwt -audience spiffe://example.org/ns/foo/sa/my-httpbin -socketPath /run/spire/sockets/agent.sock -svid `head -2 jwt | tail -1`
-
+  -c sleep -n foo -- /opt/spire/spire-agent api validate jwt -audience spiffe://example.org/ns/foo/sa/my-httpbin -socketPath /run/spire/sockets/agent.sock -svid $JWT_TOKEN
