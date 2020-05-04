5e2431960ece454ce0159da62ab73bbb57a8b12b
Amanpreet Singh <coool.aman@gmail.com>
2020-05-04 18:46:26 +0530

    Added Back Data Binary

--- Docker/compose/files/scripts/opa.sh
+++ Docker/compose/files/scripts/opa.sh
@@ -1,5 +1,5 @@
 echo -en "\033[1;34m Apply Demo Policy \033[0m \n"
-curl -s -X PUT -d @../opa/example.rego docker:8181/v1/policies/example > /dev/null
+curl -s -X PUT --data-binary @../opa/example.rego docker:8181/v1/policies/example > /dev/null
 sleep 2
 
 echo -en "\033[1;32m Alice Checks Her Own Salary \033[0m \n"
@@ -15,7 +15,7 @@ echo -en "\033[1;33m HR Trying to access Alice Salary (Without Policy)\033[0m \n
 curl --user david:password docker:5000/finance/salary/alice
 
 echo -en "\033[1;34m Apply HR Policy \033[0m \n"
-curl -s -X PUT -d @../opa/hr.rego docker:8181/v1/policies/example-hr > /dev/null
+curl -s -X PUT --data-binary @../opa/hr.rego docker:8181/v1/policies/example-hr > /dev/null
 sleep 2
 
 echo -en "\033[1;32m HR Able to Check all Salaries \033[0m \n"
