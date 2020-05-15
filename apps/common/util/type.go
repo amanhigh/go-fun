8ef2ac6617188a77fc0e4099c075346581f93593
Amanpreet Singh <coool.aman@gmail.com>
2020-05-15 13:42:48 +0530

    Added Reverse Array

--- apps/common/util/type.go
+++ apps/common/util/type.go
@@ -3,6 +3,7 @@ package util
 import (
 	"errors"
 	"fmt"
+	"reflect"
 	"strconv"
 )
 
@@ -18,4 +19,12 @@ func ParseInt(value string) (i int, err error) {
 		err = errors.New(fmt.Sprintf("%v is not a Valid Integer", value))
 	}
 	return
+}
+
+func ReverseArray(input interface{}) {
+	n := reflect.ValueOf(input).Len()
+	swap := reflect.Swapper(input)
+	for i, j := 0, n-1; i < j; i, j = i+1, j-1 {
+		swap(i, j)
+	}
 }
