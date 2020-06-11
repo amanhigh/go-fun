c7e5220609da27d96f90c637d2ac0de3dad96dd7
Amanpreet Singh <coool.aman@gmail.com>
2020-06-11 13:14:01 +0530

    Linked Policy via Role

--- learn/frameworks/casbin/rbac_test.go
+++ learn/frameworks/casbin/rbac_test.go
@@ -1,6 +1,7 @@
 package casbin
 
 import (
+	"fmt"
 	"github.com/casbin/casbin/v2"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -12,26 +13,43 @@ var _ = Describe("Rbac", func() {
 	)
 
 	It("should build", func() {
+		fmt.Println(">>ERROR<<", err)
+
 		Expect(e).To(Not(BeNil()))
 		Expect(err).To(BeNil())
 	})
 
-	Context("IsAuthorized", func() {
-		It("should be allowed", func() {
-			ok, err := e.Enforce("alice", "data1", "read")
+	Context("Alice", func() {
+		It("should be allowed to read image", func() {
+			ok, err := e.Enforce("alice", "image", "read")
 			Expect(ok).To(BeTrue())
+			Expect(err).To(BeNil())
+		})
+
+		It("should not be allowed to write image", func() {
+			ok, err := e.Enforce("alice", "image", "write")
+			Expect(ok).To(BeFalse())
+			Expect(err).To(BeNil())
+		})
+
+		It("should give admin for alice", func() {
+			roles, err := e.GetRolesForUser("alice")
+			Expect(roles).To(Equal([]string{"image-reader"}))
 			Expect(err).To(BeNil())
 		})
 	})
 
-	Context("Roles", func() {
-		Context("Get", func() {
-			It("should give admin for alice", func() {
-				roles, err := e.GetRolesForUser("alice")
-				Expect(roles).To(Equal([]string{"admin"}))
-				Expect(err).To(BeNil())
-			})
+	Context("Bob", func() {
+		It("should be able to write image", func() {
+			ok, err := e.Enforce("bob", "image", "write")
+			Expect(ok).To(BeTrue())
+			Expect(err).To(BeNil())
 		})
 
+		It("should give admin for alice", func() {
+			roles, err := e.GetRolesForUser("bob")
+			Expect(roles).To(Equal([]string{"image-writer"}))
+			Expect(err).To(BeNil())
+		})
 	})
 })
