08ab42e78b9c7cefdc3f233e2cdb56f7f8e9ad20
Amanpreet Singh <coool.aman@gmail.com>
2020-07-10 13:23:06 +0530

    Added HTML Template

--- learn/frameworks/libraries/go-generate.go
+++ learn/frameworks/libraries/go-generate.go
@@ -11,9 +11,25 @@ type Metadata struct {
 	Type        string
 }
 
+type entry struct {
+	Name string
+	Done bool
+}
+
+type ToDo struct {
+	User string
+	List []entry
+}
+
 func GenerateFun() {
+	printText()
+	printHtml()
+}
+
+/* Text Template */
+func printText() {
 	tmpl := template.New("jsonTemplate")
-	if tmpl, err := tmpl.Parse(templateString()); err == nil {
+	if tmpl, err := tmpl.Parse(textTemplate()); err == nil {
 		_ = tmpl.Execute(os.Stdout, Metadata{
 			PackageName: "com.test.gen",
 			Type:        "string",
@@ -22,7 +38,7 @@ func GenerateFun() {
 	}
 }
 
-func templateString() string {
+func textTemplate() string {
 	return `//This is Aman's Generated File
 //Request you not to mess with it :)
 
@@ -47,4 +63,49 @@ func (obj {{ .Type }}) WriteTo(writer io.Writer) (int64, error) {
 	You Missed Supplying Type Variable
 {{end}}.
 `
+}
+
+/* HTML Template */
+func printHtml() {
+	tmpl := template.New("htmlTemplate")
+	if tmpl, err := tmpl.Parse(htmlTemplate()); err == nil {
+		_ = tmpl.Execute(os.Stdout, ToDo{
+			User: "Amanpreet Singh",
+			List: []entry{{
+				Name: "Prepare Slides",
+				Done: true,
+			}, {
+				Name: "Give Demo",
+				Done: false,
+			}},
+		})
+	}
+}
+
+func htmlTemplate() string {
+	return `<!DOCTYPE html>
+<html>
+  <head>
+    <title>Go To-Do list</title>
+  </head>
+  <body>
+    <p>
+      To-Do list for user: {{ .User }} 
+    </p>
+    <table>
+      	<tr>
+          <td>Task</td>
+          <td>Done</td>
+    	</tr>
+      	{{ with .List }}
+			{{ range . }}
+      			<tr>
+              		<td>{{ .Name }}</td>
+              		<td>{{ if .Done }}Yes{{ else }}No{{ end }}</td>
+      			</tr>
+			{{ end }} 
+      	{{ end }}
+    </table>
+  </body>
+</html>`
 }
