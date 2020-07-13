7718fed1d428997b6ba0cfeafa046a4dc7587f7e
Amanpreet Singh <coool.aman@gmail.com>
2020-07-13 18:49:11 +0530

    Added Interesting Assertions

--- learn/frameworks/json/json_convey_test.go
+++ learn/frameworks/json/json_convey_test.go
@@ -14,8 +14,10 @@ func TestJsonEncode(t *testing.T) {
 		personJson = fmt.Sprintf(`{"name":"%s","Age":%d,"MobileNumber":%d}`, name, age, number)
 		person     = person{name, age, number}
 	)
+
 	Convey("Json", t, func() {
 		Convey("encode", func() {
+			age = 55
 			So(encodePerson(person), ShouldEqual, personJson)
 		})
 
@@ -25,4 +27,14 @@ func TestJsonEncode(t *testing.T) {
 
 	})
 
+	//Assertions - https://github.com/smartystreets/goconvey/blob/master/examples/assertion_examples_test.go
+	Convey("Interesting Assertions", t, func() {
+		So(1, ShouldAlmostEqual, 1.000000000000001)
+		So([]int{1, 2, 3}, ShouldContain, 2)
+		So(map[int]int{1: 1, 2: 2, 3: 3}, ShouldContainKey, 2)
+		So(1, ShouldBeIn, []int{1, 2, 3})
+		So(func() {}, ShouldNotPanic)
+		So(1, ShouldNotHaveSameTypeAs, "1")
+		So(bytes.NewBufferString(""), ShouldImplement, (*io.Reader)(nil))
+	})
 }
