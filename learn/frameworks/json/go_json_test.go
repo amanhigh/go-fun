8943643ade05d9394b9ed491c9b822c0c27ab1f2
Amanpreet Singh <coool.aman@gmail.com>
2020-07-14 14:42:24 +0530

    Added Basic Go Test

---
+++ learn/frameworks/json/go_json_test.go
@@ -0,0 +1,19 @@
+package json
+
+import "testing"
+
+var per = person{"Zoye", 44, 8983333}
+
+func TestGoEncode(t *testing.T) {
+	_, err := encodePerson(per)
+	if err != nil {
+		t.Errorf("Encode Failied for Person %v", per)
+	}
+}
+
+func BenchmarkEncode(b *testing.B) {
+
+	for n := 0; n < b.N; n++ {
+		encodePerson(per)
+	}
+}
