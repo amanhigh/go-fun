1a96fb5e030d33a6a0e93a94d768aef9a7fc5fcb
Amanpreet Singh <coool.aman@gmail.com>
2020-09-10 12:02:25 +0530

    Installed Addon as per 1.7 Spec

--- Kubernetes/istio/istio.sh
+++ Kubernetes/istio/istio.sh
@@ -11,10 +11,11 @@ kubectl create secret generic cacerts -n istio-system \
 
 echo -en "\033[1;32m Create Kialia Admin Login \033[0m \n"
 kubectl apply -f ./components/kiali.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.7/samples/addons/kiali.yaml
 
 #istioctl manifest apply --set profile=demo
 #-- Dasboards
-#istioctl dashboard kiali
+istioctl dashboard kiali
 
 echo -en "\033[1;32m Gateway Filter Chain Check CMD \033[0m \n"
 echo 'istioctl proxy-config listeners -n istio-system $(kubectl get pod -l app=istio-ingressgateway -n istio-system -o jsonpath={.items..metadata.name}) --port 80 -o json | jq ".[0].filterChains[0].filters"'
