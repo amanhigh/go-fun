60603771782dbb2607ace2f87dcad65172c2012d
Amanpreet Singh <coool.aman@gmail.com>
2021-08-27 14:13:16 +0530

    Added Admin Handler

---
+++ apps/components/fun-app/handlers/AdminHandler.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"github.com/amanhigh/go-fun/apps/common/util"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type AdminHandler struct {
+	Shutdown *util.GracefullShutdown `inject:""`
+}
+
+func (self *AdminHandler) Stop(c *gin.Context) {
+	self.Shutdown.Close()
+	//TODO:Add check to prevent accidental stop
+	c.JSON(http.StatusOK, "Stop Started")
+}
