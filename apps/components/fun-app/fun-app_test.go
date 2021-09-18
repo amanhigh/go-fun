82bf5ea554e563b6837a26eb5a46f377a7c59427
Amanpreet Singh <coool.aman@gmail.com>
2021-09-18 09:56:50 +0530

    Moved Coverage Test to Fun App Dir.

--- apps/components/fun-app/fun-app_test.go
+++ apps/components/fun-app/fun-app_test.go
@@ -6,7 +6,7 @@ import (
 
 // TestMain - test to drive external testing coverage
 /**
-go test app_test.go fun-app.go -coverprofile=coverage.out
+go test fun-app_test.go fun-app.go -coverprofile=coverage.out
 curl http://localhost:8080/admin/stop
 go tool cover -func=coverage.out
 */
