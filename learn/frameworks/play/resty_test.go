7bcd3df5487b6768929f04b0165f95b8862ecaee
Amanpreet Singh <coool.aman@gmail.com>
2021-09-20 23:52:29 +0530

    Retry and Custom Transport

--- learn/frameworks/play/resty_test.go
+++ learn/frameworks/play/resty_test.go
@@ -5,7 +5,9 @@ import (
 	"github.com/go-resty/resty/v2"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"net"
 	"net/http"
+	"time"
 )
 
 type BinAnyResponse struct {
@@ -26,6 +28,29 @@ var _ = Describe("Resty", func() {
 		err = nil
 		resp = nil
 
+		//Custom Transport
+		transport := http.Transport{
+			DisableCompression: false,
+			DisableKeepAlives:  false,
+			DialContext: (&net.Dialer{
+				Timeout: time.Second, // Connect Timeout
+			}).DialContext,
+			IdleConnTimeout:     time.Minute, //Idle Timeout Before Closing Keepalive Connection
+			MaxIdleConnsPerHost: 10,
+		}
+		client.SetTransport(&transport)
+
+		//client.SetDebug(true)
+		//client.EnableTrace()
+		client.SetHeader("Content-Type", "application/json")
+		client.SetHeaderVerbatim("Foo", "bar")
+		client.SetTimeout(2 * time.Second)
+		client.SetHostURL("https://httpbin.org")
+
+		//Try 2 times at interval of one second, max time 5 Seconds
+		client.SetRetryCount(5).
+			SetRetryWaitTime(300 * time.Millisecond).
+			SetRetryMaxWaitTime(3 * time.Second)
 	})
 
 	It("should build", func() {
@@ -33,7 +58,7 @@ var _ = Describe("Resty", func() {
 	})
 
 	It("should do get", func() {
-		resp, err = client.R().Get("https://httpbin.org/status/200")
+		resp, err = client.R().SetQueryParam("foo", "bar").Get("/status/200")
 		Expect(err).To(BeNil())
 		Expect(resp.StatusCode()).To(Equal(http.StatusOK))
 	})
@@ -56,12 +81,18 @@ var _ = Describe("Resty", func() {
 		})
 
 		It("should build custom Request", func() {
-			resp, err = client.R().SetHeader(headerKey, headerValue).SetBody(person).SetResult(&binResponse).Post("https://httpbin.org/anything")
+			resp, err = client.R().
+				SetHeader(headerKey, headerValue).
+				SetBody(person).
+				SetResult(&binResponse).
+				Post("/anything")
+
 			Expect(err).To(BeNil())
 			Expect(resp.StatusCode()).To(Equal(http.StatusOK))
 			Expect(binResponse.Method).To(Equal(http.MethodPost))
 			Expect(len(binResponse.Headers)).To(BeNumerically(">", 2))
 			Expect(binResponse.Headers).To(HaveKeyWithValue(headerKey, headerValue))
+			Expect(binResponse.Headers).To(HaveKeyWithValue("Foo", "bar"), "Having Global Headers")
 		})
 	})
 
