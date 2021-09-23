ca2a3167502b87cc261e00cf42af435341cd91bb
Amanpreet Singh <coool.aman@gmail.com>
2021-09-23 17:05:15 +0530

    Added Docs

--- apps/common/util/db_resolver.go
+++ apps/common/util/db_resolver.go
@@ -15,28 +15,44 @@ incase of error switches to fallback (master).
 At given intervals try to revert config back to slave
 based on Pings. It needs Ping Table and a Ping dbresolver for it
 to work.
+
+
+Usage:
+policy := util.NewFallBackPolicy(db, time.Second * 2,"verticals")
+	db.Use(dbresolver.Register(dbresolver.Config{
+		Replicas: []gorm.Dialector{
+			slave,
+			master,
+		},
+		Policy: policy,
+	}).Register(dbresolver.Config{
+		Replicas: []gorm.Dialector{
+			slave,
+		},
+	},"ping"))
+
 */
 type FallBackPolicy struct {
 	//Current Pool which should be Used
 	currentPool int
-	//DB to do ping checks
+	//DB to do ping checks with ping resolver configured
 	db *gorm.DB
 	//Channel to Report Errors
 	errChan chan error
 	//Interval retry to revert to original Config
 	ticker *time.Ticker
-	//Model for Table to get count as a Ping
+	//TableName to get count as a PingCheck on DB
 	pingTable string
 }
 
 const (
-	PING          = "ping"
-	POOL_PRIMARY  = 0
-	POOL_FALLBACK = 1
+	PING          = "ping" //DBResolver Which Represents Primary for Reads
+	POOL_PRIMARY  = 0      //PoolIndex Which Represents Primary
+	POOL_FALLBACK = 1      //PoolIndex Which Represents Fallback
 )
 
 /**
-DB with read,write and ping resolver configured.
+DB with read,write and ping (primary for reads) resolver configured.
 RetryInterval at which restore to primary would be tried.
 PingTable Name used to ping db with count query to check connectivity.
 */
@@ -56,12 +72,11 @@ Resolve Function Implementation for dbResolver.
 func (self *FallBackPolicy) Resolve(connPools []gorm.ConnPool) gorm.ConnPool {
 	x := self.GetPool()
 	log.WithFields(log.Fields{"Pool Count": len(connPools), "Current Pool": x}).Trace("Pool Info")
-	pool := connPools[x]
-	return pool
+	return connPools[x]
 }
 
 /**
-Report any DB Errors, can handle nils
+Report any DB Errors which will be used to fallback if applicable
 */
 func (self *FallBackPolicy) ReportError(err error) {
 	self.errChan <- err
@@ -75,17 +90,17 @@ successful Ping
 func (self *FallBackPolicy) GetPool() (poolIndex int) {
 	select {
 	case err, ok := <-self.errChan:
-		//If Channel is Not Closed Update Pool
 		_, isNetErr := err.(net.Error)
-		if ok && err != nil && isNetErr {
+		//Process if errorChannel Open we are on Primary Pool and there is a Network Error
+		if ok && self.currentPool == POOL_PRIMARY && isNetErr {
 			log.WithFields(log.Fields{"Error": err}).Error("Falling Back to Master for Reads")
-			//Update New Value in Cache
 			self.currentPool = POOL_FALLBACK
 		}
 		//Serve Updated Pool
 		poolIndex = self.currentPool
+
 	case <-self.ticker.C:
-		//If we have switched to Fallback ping primary
+		//At each Retry Interval, If we have switched to Fallback
 		if self.currentPool == POOL_FALLBACK {
 			//Try to Ping Slave if it is up
 			if err := self.Ping(); err == nil {
@@ -103,6 +118,11 @@ func (self *FallBackPolicy) GetPool() (poolIndex int) {
 	return
 }
 
+/**
+Triest to do a Count * on configured Ping Table to check connectivity.
+Uses Ping Resolver as Read Resolver is switched by our Fallback Policy and is point
+to master.
+*/
 func (self *FallBackPolicy) Ping() (err error) {
 	c := int64(0)
 	err = self.db.Clauses(dbresolver.Use(PING)).Table(self.pingTable).Count(&c).Error
