6c750b4814acbdbda441a7b5f27b5dbf3cb9ada0
Amanpreet Singh <coool.aman@gmail.com>
2021-09-23 13:35:50 +0530

    Added Ping Support (Not Working)

--- apps/common/util/db_resolver.go
+++ apps/common/util/db_resolver.go
@@ -1,10 +1,12 @@
 package util
 
 import (
+	"errors"
 	"fmt"
 	log "github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 	"gorm.io/plugin/dbresolver"
+	"time"
 )
 
 /**
@@ -21,13 +23,19 @@ type FallBackPolicy struct {
 	db *gorm.DB
 	//Channel to Report Errors
 	errChan chan error
+	//Interval retry to revert to original Config
+	ticker *time.Ticker
+	//Model for Table to get count as a Ping
+	pingTable string
 }
 
-func NewFallBackPolicy(Db *gorm.DB) *FallBackPolicy {
+func NewFallBackPolicy(Db *gorm.DB, retryInterval time.Duration, pingTable string) *FallBackPolicy {
 	return &FallBackPolicy{
 		currentPool: 0,
 		errChan:     make(chan error, 5),
 		db:          Db,
+		ticker:      time.NewTicker(retryInterval),
+		pingTable:   pingTable,
 	}
 }
 
@@ -48,6 +56,11 @@ func (self *FallBackPolicy) ReportError(err error) {
 	self.errChan <- err
 }
 
+/**
+Returns Current Pool that should be getting Traffic.
+On Error switches to fallback and reverts post
+successful Ping
+*/
 func (self *FallBackPolicy) GetPool() (poolIndex int) {
 	select {
 	case err, ok := <-self.errChan:
@@ -60,6 +73,19 @@ func (self *FallBackPolicy) GetPool() (poolIndex int) {
 		}
 		//Serve Updated Pool
 		poolIndex = self.currentPool
+	case t := <-self.ticker.C:
+		fmt.Println("Ticker", t)
+		//If we have switched to Fallback ping primary
+		if self.currentPool == 1 {
+			//Try to Ping Slave if it is up
+			if err := self.Ping(dbresolver.Read); err == nil {
+				self.currentPool = 0
+				log.Info("Slave Up reverting config for Reads.")
+			} else {
+				log.WithFields(log.Fields{"Error": err}).Warning("Pinged Slave still not up. Reads continue on master")
+			}
+			poolIndex = self.currentPool
+		}
 	default:
 		//Serve Current Pool
 		poolIndex = self.currentPool
@@ -68,7 +94,9 @@ func (self *FallBackPolicy) GetPool() (poolIndex int) {
 }
 
 func (self *FallBackPolicy) Ping(resolver dbresolver.Operation) (err error) {
-	err = self.db.Clauses(resolver).Raw("show tables").Error
+	err = errors.New("Not Implemented")
+	//c:=int64(0)
+	//err = self.db.Clauses(resolver).Table("verticals").Count(&c).Error
 	fmt.Println(resolver, err)
 	return
 }
