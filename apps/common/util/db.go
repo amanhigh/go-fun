c57c6c48f4fab8856ebabfa9b6601ed50e9d73b0
Amanpreet Singh <coool.aman@gmail.com>
2021-09-30 13:18:50 +0530

    Added Create Mysql Connection

--- apps/common/util/db.go
+++ apps/common/util/db.go
@@ -1,7 +1,10 @@
 package util
 
 import (
+	"database/sql"
+	"fmt"
 	"github.com/amanhigh/go-fun/apps/models/config"
+	_ "github.com/go-sql-driver/mysql"
 	log "github.com/sirupsen/logrus"
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlite"
@@ -28,5 +31,11 @@ func CreateDbConnection(config config.Db) (db *gorm.DB, err error) {
 func CreateTestDb() (db *gorm.DB, err error) {
 	//Use Log Level 4 for Debug, 3 for Warnings, 2 for Errors
 	db, err = gorm.Open(sqlite.Open("/tmp/gorm.db"), &gorm.Config{Logger: logger.Default.LogMode(logger.Error)})
+	return
+}
+
+func CreateMysqlConnection(username, password, host, dbName string, port int) (db *sql.DB, err error) {
+	url := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", username, password, host, port, dbName)
+	db, err = sql.Open("mysql", url)
 	return
 }
