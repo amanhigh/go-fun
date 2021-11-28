323a5eedcded82e9c0a15fcdb7866be1f3fe93de
Amanpreet Singh <coool.aman@gmail.com>
2021-11-28 23:28:43 +0530

    Constant Based Ginkgo Labels

--- components/fun-app/it/it_suite_test.go
+++ components/fun-app/it/it_suite_test.go
@@ -2,6 +2,7 @@ package it_test
 
 import (
 	"github.com/amanhigh/go-fun/components/fun-app/common"
+	"github.com/amanhigh/go-fun/models"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"time"
@@ -11,7 +12,7 @@ import (
 
 func TestIt(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "FunApp Suite")
+	RunSpecs(t, "FunApp Suite", Label(models.GINKGO_INEGRATION_TEST))
 }
 
 var _ = BeforeSuite(func() {
--- models/constant.go
+++ models/constant.go
@@ -1,3 +1,7 @@
 package models
 
 const XRequestID = "X-Request-ID"
+
+//Ginkgo
+const GINKGO_INEGRATION_TEST = "it"
+const GINKGO_SETUP = "setup"
