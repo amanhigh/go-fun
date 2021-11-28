1070d23d93117df9ed4f3c92487ea79de06d5c52
Amanpreet Singh <coool.aman@gmail.com>
2021-11-28 23:21:01 +0530

    Add App Start before Test Run

--- components/fun-app/it/it_suite_test.go
+++ components/fun-app/it/it_suite_test.go
@@ -1,8 +1,10 @@
 package it_test
 
 import (
+	"github.com/amanhigh/go-fun/components/fun-app/common"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"time"
 
 	"testing"
 )
@@ -11,3 +13,9 @@ func TestIt(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "FunApp Suite")
 }
+
+var _ = BeforeSuite(func() {
+	go common.RunFunApp()
+	//TODO:Avoid Sleep, Better Way ?
+	time.Sleep(time.Second)
+})
