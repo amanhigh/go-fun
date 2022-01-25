34136a400dfe58c90f7c8fcd4ce06e1030990ca6
Amanpreet Singh <coool.aman@gmail.com>
2022-01-25 22:14:43 +0530

    Moved File Server as Test

---
+++ components/learn/frameworks/play-fast/file_server_test.go
@@ -0,0 +1,34 @@
+package play_fast_test
+
+import (
+	"fmt"
+	"github.com/go-resty/resty/v2"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"net/http"
+	"strconv"
+)
+
+var _ = Describe("File Server", func() {
+	var (
+		// create file server handler
+		dir  = "/tmp"
+		fs   = http.FileServer(http.Dir(dir))
+		port = 8092
+		url  = "http://localhost:" + strconv.Itoa(port)
+
+		response *resty.Response
+		err      error
+	)
+	BeforeEach(func() {
+		go http.ListenAndServe(fmt.Sprintf(":%v", port), fs)
+	})
+
+	It("should run", func() {
+		response, err = resty.New().R().Get(url)
+		Expect(err).To(BeNil())
+		Expect(response.StatusCode()).To(Equal(http.StatusOK))
+		Expect(response.String()).To(Not(BeNil()))
+	})
+
+})
