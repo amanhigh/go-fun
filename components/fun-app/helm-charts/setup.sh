fb780baea1f42af0928dbc9f481839896e5f9bb4
Amanpreet Singh <coool.aman@gmail.com>
2022-05-20 22:06:20 +0530

    Added FunApp URL

--- components/fun-app/helm-charts/setup.sh
+++ components/fun-app/helm-charts/setup.sh
@@ -11,3 +11,4 @@ echo -en "\033[1;32m Setup FunApp \033[0m \n"
 kubectl apply -n fun-app -f .
 
 echo -en "\033[1;32m MysqlAdmin: http://localhost:8091/api/v1/namespaces/fun-app/services/fun-mysqladmin-phpmyadmin:80/proxy/index.php?server=fun-mysql \033[0m \n"
+echo -en "\033[1;32m FunApp: http://localhost:8091/api/v1/namespaces/fun-app/services/fun-app:9000/proxy/metrics \033[0m \n"
