5962bf5a4254ed1b1411e8656db13ed530497fe0
Amanpreet Singh <coool.aman@gmail.com>
2022-05-20 15:37:41 +0530

    Added Basic Mysql and App Setup with Cleanup.

--- components/fun-app/helm-charts/clean.sh
+++ components/fun-app/helm-charts/clean.sh
@@ -1,3 +1,5 @@
 helm delete -n fun-app fun-mysql
+kubectl delete -n fun-app -f .
+
 
 kubectl delete ns fun-app
--- components/fun-app/helm-charts/setup.sh
+++ components/fun-app/helm-charts/setup.sh
@@ -1,4 +1,13 @@
-
-helm install --create-namespace --replace --set auth.rootPassword=root --set auth.username=aman --set auth.password=aman --wait -n fun-app fun-mysql bitnami/mysql
-
+kubectl create ns fun-app
 kubectl label namespace fun-app istio-injection=enabled
+
+echo -en "\033[1;32m Setup Mysql \033[0m \n"
+#TODO: Master Slave Setup
+helm install --set auth.rootPassword=root --set auth.username=aman --set auth.password=aman --wait -n fun-app fun-mysql bitnami/mysql
+helm install --wait -n fun-app fun-mysqladmin bitnami/phpmyadmin
+
+
+echo -en "\033[1;32m Setup FunApp \033[0m \n"
+kubectl apply -n fun-app -f .
+
+echo -en "\033[1;32m MysqlAdmin: http://localhost:8091/api/v1/namespaces/fun-app/services/fun-mysqladmin-phpmyadmin:80/proxy/index.php?server=fun-mysql \033[0m \n"
