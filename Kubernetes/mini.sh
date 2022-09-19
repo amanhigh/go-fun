327455aba1651ddde06ee6663f84adb1bed113ca
Amanpreet Singh <coool.aman@gmail.com>
2022-09-19 23:11:55 +0530

    Added K9s Help

--- Kubernetes/mini.sh
+++ Kubernetes/mini.sh
@@ -3,7 +3,8 @@ PORT=8091
 minikube -p minikube delete;
 minikube  -p minikube start \
 --memory=3096 --cpus=2 \
---host-only-cidr='24.1.1.100/24' \
+# TODO: Fix on WSL
+#--host-only-cidr='24.1.1.100/24' \
 --cache-images=true \
 --extra-config="apiserver.enable-swagger-ui=true" \
 --extra-config="apiserver.service-account-api-audiences=api" \
@@ -17,12 +18,17 @@ minikube -p minikube dashboard --url=true &
 echo -en "\033[1;33m Run 'minikube tunnel’ for Emulating ELB\033[0m \n"
 echo -en "\033[1;32m Dashboard: http://localhost:$PORT/api/v1/namespaces/kubernetes-dashboard/services/http:kubernetes-dashboard:/proxy/# \033[0m \n"
 echo -en "\033[1;32m Swagger: http://localhost:$PORT/swagger-ui \033[0m \n"
-echo -en "\033[1;33m Context: `kubectl config current-context`\033[0m \n"
+echo -en "\033[1;33m Context: `kubectl config current-context; k9s --context minikube`\033[0m \n"
 kubectl proxy --port=$PORT
 
 
+## k9s
+# k9s --readonly , -n <namespace>, -l <loglevel>
+# k9s :pu (pulse), :dp (deployments), :po (pods), :ns (namespace),:rb (Role Bindings) ,:a (aliases)
+# Switch Namespace :po <namespace> to see pods of that namespace
 
 # Helpful Commands
+# Docker Start: sudo service docker start
 # Docker Image to Minikube: eval $(minikube docker-env); docker build -t fun-app .
 # Port Forward (Local Port 9090 to Container Port 8080) - kubectl port-forward `kubectl get pods -o name | grep fun-app | head  -1` 9090:8080
 # Logs - kubectl logs `kubectl get pods -o name | grep fun-app | head  -1` -f
