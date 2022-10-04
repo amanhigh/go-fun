26c54d1bed6883ee7bcd88952257479a9bedecbd
Amanpreet Singh <coool.aman@gmail.com>
2022-10-04 19:25:02 +0530

    Fixed Name changes due to helm dependency.

--- components/fun-app/helm-charts/clean.sh
+++ components/fun-app/helm-charts/clean.sh
@@ -1,6 +1,4 @@
-helm delete -n fun-app fun-mysqladmin
-helm delete -n fun-app fun-mysql
-helm delete -n fun-app fun-redis
+# helm delete -n fun-app fun-mysql
 helm delete -n fun-app fun-app
 
 kubectl delete ns fun-app
--- components/fun-app/helm-charts/setup.sh
+++ components/fun-app/helm-charts/setup.sh
@@ -4,15 +4,12 @@ kubectl label namespace fun-app istio-injection=enabled
 
 echo -en "\033[1;32m Setup Mysql \033[0m \n"
 #TODO: Master Slave Setup
-helm install --set auth.rootPassword=root --set auth.database=compute --set auth.username=aman --set auth.password=aman -n fun-app fun-mysql bitnami/mysql > /dev/null
+# helm install --set auth.rootPassword=root --set auth.database=compute --set auth.username=aman --set auth.password=aman -n fun-app fun-mysql bitnami/mysql > /dev/null
 echo -en "\033[1;33m K9S Shell to master, 'mysql -u root -p compute' (Password: root) \033[0m \n"
-
-helm install -n fun-app fun-mysqladmin bitnami/phpmyadmin > /dev/null
-echo -en "\033[1;33m MysqlAdmin: http://localhost:8091/api/v1/namespaces/fun-app/services/fun-mysqladmin-phpmyadmin:80/proxy/index.php?server=fun-mysql \033[0m \n"
+echo -en "\033[1;33m MysqlAdmin: http://localhost:8091/api/v1/namespaces/fun-app/services/fun-app-phpmyadmin:80/proxy/index.php?server=fun-app-mysql \033[0m \n"
 
 echo -en "\033[1;32m Setup Redis \033[0m \n"
-helm install -n fun-app --set auth.enabled=false --set auth.password="" --set replica.replicaCount=1 fun-redis bitnami/redis > /dev/null
-echo -en "\033[1;33m K9S Shell to master, 'redis-cli' OR 'redis-cli -h fun-redis-master-0'/fun-redis-replicas \033[0m \n"
+echo -en "\033[1;33m K9S Shell to master, 'redis-cli' OR 'redis-cli -h fun-app-redis-master-0'/fun-app-redis-replicas \033[0m \n"
 
 echo -en "\033[1;32m Setup FunApp \033[0m \n"
 helm install -n fun-app fun-app . --set rateLimit.perMin=150
@@ -33,5 +30,11 @@ echo -en "\033[1;33m FunApp: http://localhost:8091/api/v1/namespaces/fun-app/ser
 # helm delete -n <Namespace> <Chart Name>
 
 # helm list -n <Namespace>
+
+# helm repo list
+# helm dependency list
+# helm dependency update
+# helm dependency build
+
 
 
