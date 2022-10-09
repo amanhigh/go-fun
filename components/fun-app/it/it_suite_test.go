13feecb7a0d21f25570449737cbed5ba911e4c80
Amanpreet Singh <coool.aman@gmail.com>
2022-10-09 21:23:18 +0530

    Added Fun Test as Devspace Command

--- components/fun-app/it/it_suite_test.go
+++ components/fun-app/it/it_suite_test.go
@@ -1,11 +1,13 @@
 package it_test
 
 import (
+	"os"
+	"time"
+
 	"github.com/amanhigh/go-fun/components/fun-app/common"
 	"github.com/amanhigh/go-fun/models"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"time"
 
 	"testing"
 )
@@ -16,7 +18,10 @@ func TestIt(t *testing.T) {
 }
 
 var _ = BeforeSuite(func() {
+	// Override Port to Avoid Collision with Default APp
+	os.Setenv("PORT", "8085")
 	go common.RunFunApp()
+
 	//TODO:Avoid Sleep, Better Way ?
 	time.Sleep(time.Second)
 })
