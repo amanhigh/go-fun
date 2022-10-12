7b443c4f07d8ce0fab17619847f802f4b8950f56
Amanpreet Singh <coool.aman@gmail.com>
2022-10-12 16:48:41 +0530

    Moved Redis to K8

--- Kubernetes/services/service.sh
+++ Kubernetes/services/service.sh
@@ -1,5 +1,4 @@
-
-answers=`gum choose RESET MYSQL MONGO --limit 5`
+answers=`gum choose RESET MYSQL MONGO REDIS --limit 5`
 for SVC in $answers
 do
     echo -en "\033[1;32m \n $SVC \033[0m \n"
@@ -20,8 +19,17 @@ do
         echo -en "\033[1;33m mongosh -u root -p root --host localhost  < /etc/files/scripts/mongo.js \033[0m \n"
         ;;
 
+    REDIS)
+        #TODO: Move commander generic helm
+        helm install redis bitnami/redis -f redis.yml > /dev/null
+        dman run rn5 1 'redis-cli -c incr mycounter'
+        echo -en "\033[1;33m redis-cli -c incr mycounter \033[0m \n"
+        echo -en "\033[1;33m redis-cli -c set mypasswd lol \033[0m \n"
+        echo -en "\033[1;33m redis-cli -c get mypasswd \033[0m \n"
+        ;;
+
     *)
-        echo -en "\033[1;34m Mongo \033[0m \n"
+        echo -en "\033[1;34m Service Not Supported: $SVC \033[0m \n"
         ;;
     esac
 done
