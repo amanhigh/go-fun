acdbaabc6061cdaa32ed4340022f424bde403d03
Amanpreet Singh <coool.aman@gmail.com>
2022-10-12 14:31:30 +0530

    Added Kubernetes Files and mounted them to minikube

--- Kubernetes/mini.sh
+++ Kubernetes/mini.sh
@@ -3,7 +3,8 @@ PORT=8091
 minikube -p minikube delete;
 
 echo -en "\033[1;32m Creating Minikube Cluster \033[0m \n"
-minikube  -p minikube start --memory=3096 --cpus=3 --cache-images=true --host-only-cidr='24.1.1.100/24';
+FILE_PATH=`readlink -f ./services/files`
+minikube  -p minikube start --memory=3096 --cpus=3 --cache-images=true --mount-string="$FILE_PATH:/etc/files" --mount --host-only-cidr='24.1.1.100/24';
 # TODO: Fix on WSL
 # --extra-config="apiserver.enable-swagger-ui=true";
 # --extra-config="apiserver.service-account-api-audiences=api" \
