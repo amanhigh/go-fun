794d112adee51dbb376b23a33d40b09c6267a606
Amanpreet Singh <coool.aman@gmail.com>
2022-10-20 00:31:44 +0530

    Added FunApp Grafana Dashboard

--- components/fun-app/charts/setup.sh
+++ components/fun-app/charts/setup.sh
@@ -20,6 +20,7 @@ echo -en "\033[1;32m Vegeta Attack \033[0m \n"
 kubectl run vegeta -n fun-app --image="peterevans/vegeta" -- sh -c "sleep 10000"
 echo -en "\033[1;33m echo 'GET http://app:8080/person/all' | vegeta attack | vegeta report (dev- app:8080, image- fun-app:9000) \033[0m \n"
 echo -en "\033[1;33m Prometheus: http://localhost:9090/graph?g0.expr=rate(fun_app_person_count%5B5m%5D)&g0.tab=0&g0.stacked=0&g0.show_exemplars=0&g0.range_input=5m&g1.expr=fun_app_person_create_time_bucket&g1.tab=0&g1.stacked=1&g1.show_exemplars=1&g1.range_input=1h&g2.expr=rate(fun_app_person_create_time_count%5B5m%5D)&g2.tab=0&g2.stacked=0&g2.show_exemplars=0&g2.range_input=1h \033[0m \n"
+echo -en "\033[1;33m Grafana Import: /fun-app/it/dashboard.json \033[0m \n"
 
 ### Helpful Commands
 # helm init fun-app - Bootstrap Charts
