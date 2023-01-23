0db876d6b261914804ab92a056c8c504856efa35
Amanpreet Singh <coool.aman@gmail.com>
2023-01-23 23:04:45 +0530

    Improved Log Cmd for K8

--- Kubernetes/mini.sh
+++ Kubernetes/mini.sh
@@ -64,7 +64,7 @@ kubectl proxy --port=$PORT;
 
 ## Kubectl
 # Port Forward (Local Port 9090 to Container Port 8080) - kubectl port-forward `kubectl get pods -o name | grep fun-app | head  -1` 9090:8080
-# Logs - kubectl logs `kubectl get pods -o name | grep fun-app | head  -1` -f
+# Logs - kubectl logs `kubectl get pods -o name | grep fun-app | head  -1` --since=1m -f
 # Login - kubectl -it exec `kubectl get pods -o name | grep fun-app | head  -1` bash
 # Delete All - kubectl delete all --all
 
