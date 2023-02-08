db7bdda563b43454fae4f780fc00ebdbeee00df4
Amanpreet Singh <coool.aman@gmail.com>
2023-02-08 12:35:02 +0530

    Added Minikube as Option

--- Kubernetes/mini.sh
+++ Kubernetes/mini.sh
@@ -1,24 +1,9 @@
 PORT=8091
-answers=`gum choose INGRESS ISTIO --limit 5`
+answers=`gum choose MINIKUBE INGRESS ISTIO --limit 5`
 
 #Use minikube config set vm-driver virtualbox/docker
 minikube -p minikube delete;
 
-echo -en "\033[1;32m Creating Minikube Cluster \033[0m \n"
-FILE_PATH=`readlink -f ./services/files`
-#Additional Flags: --kubernetes-version v1.23.0
-minikube  -p minikube start --memory=3096 --cpus=3 --cache-images=true --mount-string="$FILE_PATH:/etc/files" --mount --host-only-cidr='24.1.1.100/24';
-# TODO: Fix on WSL
-# --extra-config="apiserver.enable-swagger-ui=true";
-# --extra-config="apiserver.service-account-api-audiences=api" \
-# --extra-config="apiserver.service-account-issuer=api" \
-# --extra-config="apiserver.service-account-key-file=/var/lib/minikube/certs/sa.pub" \
-# --extra-config="apiserver.service-account-signing-key-file=/var/lib/minikube/certs/sa.key";
-# minikube -p minikube ssh 'sudo cat /var/lib/minikube/certs/sa.pub'
-
-echo -en "\033[1;32m Minikube Dashboard & Addons \033[0m \n";
-minikube -p minikube dashboard --url=true &
-minikube addons enable metrics-server;
 for SVC in $answers
 do
     echo -en "\033[1;32m \n $SVC \033[0m \n"
@@ -31,6 +16,26 @@ do
 
     ISTIO)
         ./istio/istio.sh;
+        ;;
+
+    MINIKUBE)
+        echo -en "\033[1;32m Creating Minikube Cluster \033[0m \n"
+        FILE_PATH=`readlink -f ./services/files`
+        
+        #Additional Flags: --kubernetes-version v1.23.0
+        minikube  -p minikube start --memory=3096 --cpus=3 --cache-images=true --mount-string="$FILE_PATH:/etc/files" --mount --host-only-cidr='24.1.1.100/24';
+        
+        # TODO: Fix on WSL
+        # --extra-config="apiserver.enable-swagger-ui=true";
+        # --extra-config="apiserver.service-account-api-audiences=api" \
+        # --extra-config="apiserver.service-account-issuer=api" \
+        # --extra-config="apiserver.service-account-key-file=/var/lib/minikube/certs/sa.pub" \
+        # --extra-config="apiserver.service-account-signing-key-file=/var/lib/minikube/certs/sa.key";
+        # minikube -p minikube ssh 'sudo cat /var/lib/minikube/certs/sa.pub'
+
+        echo -en "\033[1;32m Minikube Dashboard & Addons \033[0m \n";
+        minikube -p minikube dashboard --url=true &
+        minikube addons enable metrics-server;
         ;;
 
     *)
@@ -71,3 +76,7 @@ kubectl proxy --port=$PORT;
 ## Minkikube
 # Tunnel (Emulate Load Balancer) - minikube tunnel
 # List Emulated Services URL's - minikube service list
+
+# https://minikube.sigs.k8s.io/docs/handbook/pushing/
+# Load Image from Host - minikube image load amanfdk/controller
+# Minikube Docker ENV Connect - eval $(minikube docker-env)
