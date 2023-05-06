57c40534c261ef2db79b23b263cd19d49b3feb89
Amanpreet Singh <coool.aman@gmail.com>
2023-05-06 18:45:36 +0530

    Added Restore and Verify

--- components/learn/tools/duplicity/duplicity.zsh
+++ components/learn/tools/duplicity/duplicity.zsh
@@ -8,25 +8,48 @@
 #PublicKey: gpg --delete-key <email>
 ###### Importing: 
 # gpg --import <key_file>
+# Trust Key (Enter 5 for ultimiate) , q to exit: : gpg --edit-key <key_id> trust
+####### Backup Management
+# Trigger FullBackup if older than 5 Months: duplicity --full-if-older-than 5M /source/path file:///destination/path
+# Force Full: duplicity full  /source/path file:///destination/path
+# Force Incremental: duplicity incremental  /source/path file:///destination/path
 
 
 
 # Duplicity Tutorial: https://www.youtube.com/watch?v=G8M3GnAkufw
-#
+# Help Page: https://manpages.ubuntu.com/manpages/bionic/man1/duplicity.1.html
 
-KEY_ID=DAEBA62B1F563C062EBB33BA464D9D0E1EEBE051 #Get From List Key
+KEY_ID=DAEBA62B1F563C062EBB33BA464D9D0E1EEBE051 #Get From List Key (Passphrase: amanps)
+BACKUP_FILE=file://Backup
 
-echo "\033[1;32m Import Keys \033[0m \n";
+echo "\033[1;32m Import Public Keys \033[0m \n";
 gpg --import public.pgp
 
 echo "\033[1;32m Full Backup \033[0m \n";
-duplicity full ../jq file://Backup --encrypt-key=$KEY_ID
+duplicity --full-if-older-than 10s ../jq $BACKUP_FILE --encrypt-key=$KEY_ID
 
 echo "\033[1;34m Modifying Source \033[0m \n";
 echo "Test" > ../jq/test.txt
 
+
 echo "\033[1;32m Incremental Backup \033[0m \n";
-duplicity incremental ../jq file://Backup --encrypt-key=$KEY_ID
+duplicity ../jq $BACKUP_FILE --encrypt-key=$KEY_ID
+
+echo "\033[1;32m Incremental Backup (No Change) \033[0m \n";
+duplicity ../jq $BACKUP_FILE --encrypt-key=$KEY_ID
 
 echo "\033[1;34m Removing Modifications \033[0m \n";
 rm ../jq/test.txt
+
+echo "\033[1;32m List Files \033[0m \n";
+duplicity list-current-files --encrypt-key=$KEY_ID $BACKUP_FILE
+
+echo "\033[1;32m Remove Older Backups \033[0m \n";
+duplicity remove-older-than 10s $BACKUP_FILE --force
+ 
+#echo "\033[1;33m Restore Backup \033[0m \n";
+#gpg --import private.pgp #Import Private Key
+#duplicity restore --encrypt-key=$KEY_ID $BACKUP_FILE Restore/
+#
+#echo "\033[1;33m Verifying Backup \033[0m \n";
+#duplicity verify $BACKUP_FILE Restore --compare-data --encrypt-key=$KEY_ID
