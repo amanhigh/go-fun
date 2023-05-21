81f66afa575b9c45424da6d8389a58269a8e6780
Amanpreet Singh <coool.aman@gmail.com>
2023-05-21 18:40:41 +0530

    Removed File after Processing.

--- components/learn/tools/scripts/whisper.zsh
+++ components/learn/tools/scripts/whisper.zsh
@@ -6,26 +6,34 @@ fi
 
 whisper_dir=$1
 journal_dir=$2
+echo "\033[1;33m Whisper Started: `date` \033[0m \n";
 
 for file in "$whisper_dir"/*.m4a; do
-    filename=$(basename "$file")
-    # Strip the .m4a extension
-    filename_without_ext="${filename%.*}"
+    if [ -f "$file" ]; then
+        filename=$(basename "$file")
+        # Strip the .m4a extension
+        filename_without_ext="${filename%.*}"
 
-    # Extract date and time
-    date=$(echo "$filename_without_ext" | cut -d "_" -f 1-3)
-    time=$(echo "$filename_without_ext" | cut -d "_" -f 4-6)
-    
-    #Journal Detection
-    journal_name="$date.md"
-    journal_path="$journal_dir/$journal_name";
-    whisper_output="$whisper_dir/$filename_without_ext.txt"
+        # Extract date and time
+        date=$(echo "$filename_without_ext" | cut -d "_" -f 1-3)
+        time=$(echo "$filename_without_ext" | cut -d "_" -f 4-6)
+        
+        #Journal Detection
+        journal_name="$date.md"
+        journal_path="$journal_dir/$journal_name";
+        whisper_output="$whisper_dir/$filename_without_ext.txt"
 
-    #Convert Note
-    echo "\033[1;32m JournalName: $journal_name Time: $time  \033[0m \n";
-    whisper "$file" --output_format txt -o $whisper_dir --language en >/dev/null 2>/dev/null;
-    
-    #Append Note
-    echo "\n- VoiceNote: $time" >> $journal_path;
-    cat $whisper_output >> $journal_path
+        #Convert Note
+        echo "\033[1;32m JournalName: $journal_name Time: $time  \033[0m \n";
+        whisper "$file" --output_format txt -o $whisper_dir --language en >/dev/null 2>/dev/null;
+        
+        #Append Note
+        echo "\n- VoiceNote: $time" >> $journal_path;
+        cat $whisper_output >> $journal_path
+
+        #Remove Converted File
+        rm $file $whisper_output
+    fi
 done
+
+echo "\033[1;33m Whisper Finished: `date` \033[0m \n";
