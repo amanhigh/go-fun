346b575b15be92ffcac1b4afb55ea9a22c9f696f
Amanpreet Singh <coool.aman@gmail.com>
2023-05-22 13:06:40 +0530

    Generated beta1 CRD Version

--- components/operator/main.go
+++ components/operator/main.go
@@ -34,6 +34,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	cachev1alpha1 "github.com/amanhigh/go-fun/components/operator/api/v1alpha1"
+	cachev1beta1 "github.com/amanhigh/go-fun/components/operator/api/v1beta1"
 	"github.com/amanhigh/go-fun/components/operator/controllers"
 	//+kubebuilder:scaffold:imports
 )
@@ -47,6 +48,7 @@ func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
 	utilruntime.Must(cachev1alpha1.AddToScheme(scheme))
+	utilruntime.Must(cachev1beta1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
