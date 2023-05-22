b33eb36910592f18a914f220590c2cdb22514326
Amanpreet Singh <coool.aman@gmail.com>
2023-05-22 13:39:40 +0530

    Generated Webhooks for betav1

--- components/operator/main.go
+++ components/operator/main.go
@@ -105,6 +105,10 @@ func main() {
 		setupLog.Error(err, "unable to create webhook", "webhook", "Memcached")
 		os.Exit(1)
 	}
+	if err = (&cachev1beta1.Memcached{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "Memcached")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
