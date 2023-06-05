40d59ee7cdd1fbebadab4436c12cf671d10cb386
Amanpreet Singh <coool.aman@gmail.com>
2023-06-05 09:36:34 +0000

    Added Slave Status

--- Kubernetes/kube-play/play.zsh
+++ Kubernetes/kube-play/play.zsh
@@ -58,8 +58,8 @@ case $REPLY in
         kubectl exec -it $(kubectl get pods -l app=mysql -o jsonpath='{.items[0].metadata.name}') -c mysql -- /bin/sh -c 'mysql -h 127.0.0.1 -u root -proot -e "show databases;"'
         echo "\033[1;33m Master \033[0m \n";
         kubectl exec -it $(kubectl get pods -l app=mysql -o jsonpath='{.items[0].metadata.name}') -c mysql -- /bin/sh -c 'mysql -h 127.0.0.1 -u root -proot -e "SHOW MASTER STATUS\G;"'
-        # echo "\033[1;33m Slave Status (Seconds_Behind_Master,Last_IO_Error) \033[0m \n";
-        # kubectl exec -it $(kubectl get pods -l app=mysql-slave -o jsonpath='{.items[0].metadata.name}') -c mysql -- /bin/sh -c 'mysql -h 127.0.0.1 -u root -proot -e "SHOW SLAVE STATUS\G;"'
+        echo "\033[1;33m Slave Status (Seconds_Behind_Master,Last_IO_Error) \033[0m \n";
+        kubectl exec -it $(kubectl get pods -l app=mysql-slave -o jsonpath='{.items[0].metadata.name}') -c mysql -- /bin/sh -c 'mysql -h 127.0.0.1 -u root -proot -e "SHOW SLAVE STATUS\G;"'
         ;;
     stateful.yml)
         echo "\033[1;32m Deploying Nginx with Stateful Set \033[0m \n";
