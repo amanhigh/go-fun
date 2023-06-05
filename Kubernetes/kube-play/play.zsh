20acb357bac758362c9cb02ecc7735a674927224
Amanpreet Singh <coool.aman@gmail.com>
2023-06-05 05:37:01 +0000

    Added Slave Status Check

--- Kubernetes/kube-play/play.zsh
+++ Kubernetes/kube-play/play.zsh
@@ -32,10 +32,12 @@ case $REPLY in
         echo "\033[1;33m Show Services \033[0m \n";
         kubectl get service -l app=mysql
         #TODO Run in Sidecar
-        echo "\033[1;33m Show Databases \033[0m \n";
-        kubectl exec -it $(kubectl get pods -l app=mysql -o jsonpath='{.items[0].metadata.name}') -c mysql -- /bin/sh -c 'mysql -h 127.0.0.1 -u root -proot -e "show databases;"'
         echo "\033[1;33m Mysql Health Check from Sidecar \033[0m \n";
         kubectl exec -it $(kubectl get pods -l app=mysql -o jsonpath='{.items[0].metadata.name}') -c sidecar -- sh -c 'if pgrep mysqld >/dev/null 2>&1; then echo "MySQL process is running"; else echo "MySQL process is not running"; fi'
+        echo "\033[1;33m Show Databases \033[0m \n";
+        kubectl exec -it $(kubectl get pods -l app=mysql -o jsonpath='{.items[0].metadata.name}') -c mysql -- /bin/sh -c 'mysql -h 127.0.0.1 -u root -proot -e "show databases;"'
+        echo "\033[1;33m Master Status \033[0m \n";
+        kubectl exec -it $(kubectl get pods -l app=mysql -o jsonpath='{.items[0].metadata.name}') -c mysql -- /bin/sh -c 'mysql -h 127.0.0.1 -u root -proot -e "SHOW MASTER STATUS\G;"'
         echo "\033[1;33m Kill DB from Sidecar \033[0m \n";
         kubectl exec -it $(kubectl get pods -l app=mysql -o jsonpath='{.items[0].metadata.name}') -c sidecar -- sh -c 'pkill mysqld'
         ;;
@@ -46,8 +48,8 @@ case $REPLY in
         kubectl get service -l app=mysql-slave
         echo "\033[1;33m Show Databases \033[0m \n";
         kubectl exec -it $(kubectl get pods -l app=mysql-slave -o jsonpath='{.items[0].metadata.name}') -c mysql -- /bin/sh -c 'mysql -h 127.0.0.1 -u root -proot -e "show databases;"'
-        # echo "\033[1;33m Mysql Health Check from Sidecar \033[0m \n";
-        # kubectl exec -it $(kubectl get pods -l app=mysql -o jsonpath='{.items[0].metadata.name}') -c sidecar -- sh -c 'if pgrep mysqld >/dev/null 2>&1; then echo "MySQL process is running"; else echo "MySQL process is not running"; fi'
+        echo "\033[1;33m Slave Status (Seconds_Behind_Master) \033[0m \n";
+        kubectl exec -it $(kubectl get pods -l app=mysql-slave -o jsonpath='{.items[0].metadata.name}') -c mysql -- /bin/sh -c 'mysql -h 127.0.0.1 -u root -proot -e "SHOW SLAVE STATUS\G;"'
         ;;
     stateful.yml)
         echo "\033[1;32m Deploying Nginx with Stateful Set \033[0m \n";
