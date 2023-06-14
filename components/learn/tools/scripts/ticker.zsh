73205637cad19c0e0c660dc6b6b1d749596943f4
Amanpreet Singh <coool.aman@gmail.com>
2023-06-14 17:50:34 +0000

    Added Script for Ticker Tagging Photos

---
+++ components/learn/tools/scripts/ticker.zsh
@@ -0,0 +1,28 @@
+# Check if the first argument is provided
+if [ -z $1 ]; then
+  echo "Usage: $0 <directory_path>"
+  echo "Example: $0 /path/to/your/directory"
+  exit 1
+fi
+
+# Get the directory path from the first argument
+directory_path=$1
+
+# Check if the directory exists
+if [ ! -d $directory_path ]; then
+  echo "The specified directory does not exist."
+  exit 1
+fi
+
+# Store clipboard content in a variable
+clipboard_content=$(xclip -selection clipboard -o)
+
+# Find files created in the last hour in the specified directory with Name SNAG
+files=$(find "$directory_path" -maxdepth 1 -type f -cmin -25 -name '*SNAG*')
+
+# Iterate through the files and rename them
+for file in $files; do
+  # Replace 'SNAG' with clipboard content (Ticker and Timeframe) in the file name
+  new_name=$(echo $file | sed "s/SNAG/$clipboard_content/g")
+  echo mv "$file" "$new_name"
+done
