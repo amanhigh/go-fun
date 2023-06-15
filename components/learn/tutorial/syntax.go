b3e78804311a2612f175f30b54a4a21739437c17
Amanpreet Singh <coool.aman@gmail.com>
2023-06-15 06:40:15 +0000

    Added Base Golang Syntax

---
+++ components/learn/tutorial/syntax.go
@@ -0,0 +1,62 @@
+package tutorial
+
+import (
+	"fmt"
+	"time"
+)
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func Syntax() {
+	/* Vars */
+	var arr = [3]int{1, 2, 3}
+	var mapv = map[string]int{}
+	fmt.Println("Array", "Map", arr, mapv)
+
+	/* Struct */
+	fmt.Println(Person{
+		Name: "Aman",
+		Age:  27,
+	})
+
+	/* Conditionals */
+	x := 75
+	if x > 50 {
+		fmt.Println("x is greater than 50")
+	} else if x < 50 {
+		fmt.Println("x is less than 50")
+	} else {
+		fmt.Println("x is equal to 50")
+	}
+
+	/* Loop */
+	count := 2
+	for i := 0; i < count; i++ {
+		fmt.Println("ILoop", i)
+	}
+
+	numbers := []int{2, 4}
+
+	for index, value := range numbers {
+		fmt.Printf("RangeLoop: Index: %d, Value: %d\n", index, value)
+	}
+
+	/* Channels */
+	c := make(chan int)
+	// Start a goroutine for the printNumbers function
+	go printNumbers(c)
+	// Receive values from the channel and print them
+	for i := 0; i < 2; i++ {
+		fmt.Println("Channel", <-c)
+	}
+}
+
+func printNumbers(c chan int) {
+	for i := 1; i <= 5; i++ {
+		time.Sleep(1 * time.Second)
+		c <- i
+	}
+}
