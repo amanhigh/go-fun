b52193fe8e45438ac724c24477bdc9548e4aec40
Amanpreet Singh <coool.aman@gmail.com>
2023-07-02 15:33:18 +0000

    Modified Ticker Script

--- components/learn/tools/scripts/ticker.zsh
+++ components/learn/tools/scripts/ticker.zsh
@@ -1,23 +1,20 @@
 # Check if the first argument is provided
-if [ -z $1 ]; then
-  echo "Usage: $0 <directory_path>"
-  echo "Example: $0 /path/to/your/directory"
+if [ -z $2 ]; then
+  echo "Usage: $0 <directory_path> <Ticker Info>"
+  echo "Example: $0 /path/to/your/directory POWERGRID.mwd.trend.taken"
   exit 1
 fi
 
 # Get the directory path from the first argument
 directory_path=$1
+#TODO Check Clipboard Regex "POLYCAB.WDH.CTREND"
+clipboard_content=$2
 
 # Check if the directory exists
 if [ ! -d $directory_path ]; then
   echo "The specified directory does not exist."
   exit 1
 fi
-
-# Store clipboard content in a variable
-clipboard_content=$(xclip -selection clipboard -o)
-
-#TODO Check Clipboard Regex "POLYCAB.WDH.CTREND"
 
 # Find files created in the last hour in the specified directory with Name SNAG
 files=$(find "$directory_path" -maxdepth 1 -type f -cmin -25 -name '*SNAG*')
@@ -29,6 +26,3 @@ for file in $files; do
   echo "\033[1;32m Processing: $new_name \033[0m \n";
   mv "$file" "$new_name"
 done
-
-# Mark Processed to avoid reuse
-echo "Processed: $clipboard_content" | xclip -sel clip
