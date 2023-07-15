66f17e93a9a75d9b1322f21b37574b65e12f6808
Amanpreet Singh <coool.aman@gmail.com>
2023-07-15 02:52:15 +0000

    Refactored Prepare

--- components/learn/tools/scripts/arch/prepare.sh
+++ components/learn/tools/scripts/arch/prepare.sh
@@ -6,27 +6,21 @@
 
 ################## Basics #####################
 # Ctrl+d to exit anywhere
+# loadkeys dvorak
 
-# Set Keyboard
-loadkeys dvorak
-
-echo "\033[1;34m UEFI Verify Value: 64 \033[0m \n";
+echo -en "\033[1;34m UEFI Verify Value: 64 \033[0m \n";
 cat /sys/firmware/efi/fw_platform_size
 
-
-# Wifi - iwctl
+## Wifi - iwctl ##
 # device list
 # station wlan0 scan
 # station wlan0 connect <sid> -P <password>
 # station wlan0 show
 
-# Network Check
+## Network Check ##
 # ip addr show (Check inet value)
-
-# TODO: DHCP Setup
-
-#Internet Check
 ping archlinux.org
+# TODO: DHCP Setup
 
 # Time Check
 timedatectl
@@ -41,6 +35,53 @@ timedatectl
 # p - Print Current Layout
 # t - Set Type (EF: UEFI, 8E: LVM)
 
+# TODO: LVM Setup
+# TODO: Encryption
+
+
+## Format ##
+# Input Partition Names
+fdisk -l
+read -p "Enter Disk Name (/dev/sda): " disk
+read -p "Formatting $disk. Confirm ?: " confirm
+
+boot=${disk}1
+root=${disk}2
+
+# Check if the disk value is not 'N'
+if [ "$disk" == 'Y' ]; then
+  # Format EFI using FAT32
+  mkfs.fat -F32 $boot -n BOOT
+  mkfs.btrfs $root -L ROOT
+  #swapon /dev/sda2
+else
+  echo 'Skipping Disk Formatting'
+fi
+
+echo -en "\033[1;33m Creating Sub Partitions \033[0m \n";
+mount $root /mnt
+# https://archive.kernel.org/oldwiki/btrfs.wiki.kernel.org/index.php/SysadminGuide.html#Managing_Snapshots
+btrfs sub cr /mnt/@
+btrfs sub cr /mnt/@home
+btrfs sub cr /mnt/@log
+btrfs sub cr /mnt/@snapshots
+
+echo -en "\033[1;33m Mounting Drives \033[0m \n";
+# Mount ROOT Subvolume @
+umount /mnt
+mount -o subvol=@ $root /mnt
+
+# Create directory for each partitions and subvolumes:
+mkdir -p /mnt/{etc,boot/efi,home,var/log}
+
+mount -o subvol=@home $root /mnt/home
+mount -o subvol=@log $root /mnt/var/log
+mount $boot /mnt/boot/efi
+
+echo -en "\033[1;33m Generate Fstab \033[0m \n";
+genfstab -U /mnt >> /mnt/etc/fstab
+
+################## Useful Command #####################
 ## Move/Resize Partition ##
 # Clone: partclone.btrfs -c -d -s /dev/sda2 -o cloned.img
 # Restore: partclone.btrfs -r -s cloned.img -o /dev/sdb1
@@ -49,41 +90,23 @@ timedatectl
 # mount /dev/sdb1 /mnt (Target Mount)
 # btrfs filesystem resize max /mnt (Fix Size)
 # arch-chroot /mnt (New Disk: Verify Size and Files)
-# Redo all fstab and grub Steps
+# Refresh fstab, grub-install --recheck and grub-mkconfig -o /boot/grub/grub.cfg
 
-## btrfs ##
-# btrfs check /dev/sdb1 (Check Disk for Errors)
-
-# TODO: LVM Setup
-# TODO: Encryption
-
-# Enable Swap (If Required):  swapon /dev/sda2
-
-## Format ##
-
-# Input Partition Names
-read -p "Enter EFI Partition Name: " efi
-read -p "Enter Primary Partition Name: " primary
-read -p "Enter Home Partition Name: " home
-
-# Format EFI using FAT32
-mkfs.fat -F32 $efi -n BOOT
-
-# Format Primary and Home using btrfs
-mkfs.btrfs $primary -L ROOT
-mkfs.btrfs $home -L HOME
+## Ram Filesystem ##
+# tmpfs - Stays in Swap
+# mount -t ramfs -o size=2g ram_bkp /backup
 
 ## Mounts ##
 # findmnt or mount - Show all Mounts
 # umount /mnt (-a All) (-R Recursive)
 
-echo "\033[1;32m Mounting Drives \033[0m \n";
-mount $primary /mnt
-mkdir -p /mnt/boot/efi
-mkdir -p /mnt/home
-mount $efi /mnt/boot/efi
-mount $home /mnt/home
+#### BTRFS ####
+# btrfs check /dev/sdb1 (Check Disk for Errors)
+## Subvolumes ##
+# btrfs sub li /mnt
+# btrfs sub cr /mnt/mysub
+# btrfs sub del /mnt/mysub
+## Snapshot ##
+# btrfs sub snap /mnt/mysub /mnt/mysub-backup
+# mount -o subvol=mysub-backup /dev/sda2 /mnt/restore/
 
-echo "\033[1;33m Generate FsTab \033[0m \n";
-mkdir -p /mnt/etc
-genfstab -U /mnt >> /mnt/etc/fstab
