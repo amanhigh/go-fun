6345f48abeb1cf607ba9e5aeb8cecbb5545da0f7
Amanpreet Singh <amanpreet.singh@flipkart.com>
2023-11-07 12:58:43 +0530

    Added Swagger Test

--- components/fun-app/it/person_test.go
+++ components/fun-app/it/person_test.go
@@ -76,4 +76,12 @@ var _ = Describe("Person Integration Test", func() {
 		Expect(err).To(BeNil())
 		Expect(resp.StatusCode()).To(Equal(200))
 	})
+
+	It("should serve swagger", func() {
+		resp, err := TestHttpClient.R().
+			Get(serviceUrl + "/swagger/index.html")
+
+		Expect(err).To(BeNil())
+		Expect(resp.StatusCode()).To(Equal(200))
+	})
 })
