2cf904da8b68d351a3584015070acb4f052a032d
Amanpreet Singh <amanpreet.singh@flipkart.com>
2023-11-10 00:42:28 +0530

    Added Name Validator

---
+++ components/fun-app/common/validator.go
@@ -0,0 +1,37 @@
+package common
+
+//Custom Name Validator for alphanum and _,-
+import (
+	"regexp"
+
+	"github.com/go-playground/validator/v10"
+	log "github.com/sirupsen/logrus"
+)
+
+const NAME_REGEX = `[0-9a-z-]+`
+
+var nameMatcher, _ = regexp.Compile(NAME_REGEX)
+var matcherMap = map[string]*regexp.Regexp{
+	"person": nameMatcher,
+}
+
+/*
+	Validate Person Name
+
+Checks if Name is AlphaNum.
+Allowed Chars -,_
+*/
+func NameValidator(fl validator.FieldLevel) (check bool) {
+	/* Extract Entity Name */
+	entityName := fl.Param()
+
+	//Finding Corresponding Matcher
+	if matcher, ok := matcherMap[entityName]; ok {
+		name := fl.Field().String()
+		//Apply Regex
+		check = matcher.MatchString(name)
+	} else {
+		log.WithFields(log.Fields{"MatcherName": entityName}).Error("NameValidator Error")
+	}
+	return
+}
