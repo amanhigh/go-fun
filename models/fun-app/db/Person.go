43d06cfb0775831eedc7670c4cde0bdf17978ef0
Amanpreet Singh <amanpreet.singh@flipkart.com>
2023-11-11 23:58:44 +0530

    Added UUID as Id

--- models/fun-app/db/Person.go
+++ models/fun-app/db/Person.go
@@ -1,13 +1,20 @@
 package db
 
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 type Person struct {
 	// Validations - https://gin-gonic.com/docs/examples/binding-and-validation/
-	Id   int64  `gorm:"primaryKey"`
-	Name string `gorm:"not null" binding:"required,min=1,max=25,name=person"`
-	Age  int    `gorm:"not null" binding:"required,min=1,max=150"`
+	Id      string `gorm:"primaryKey"`
+	Name    string `gorm:"not null" binding:"required,min=1,max=25,name=person"`
+	Age     int    `gorm:"not null" binding:"required,min=1,max=150"`
+	Gender  string `gorm:"not null" binding:"required,eq=MALE|eq=FEMALE" enums:"MALE,FEMALE"`
+	Version int64  `gorm:"not null" json:"-" binding:"-"`
+}
 
-	Gender string `gorm:"not null" binding:"required,eq=MALE|eq=FEMALE" enums:"MALE,FEMALE"`
-
-	//TODO: Implement Versioning
-	Version int64 `gorm:"not null" json:"-" binding:"-"`
+func (p *Person) BeforeCreate(tx *gorm.DB) (err error) {
+	p.Id = uuid.NewString()[:8]
+	return
 }
