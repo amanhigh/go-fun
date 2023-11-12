05ec3feba4964791d7e822d7ff8cc23f75f33f58
Amanpreet Singh <amanpreet.singh@flipkart.com>
2023-11-13 03:01:38 +0530

    Moved to Cover Script in Integration Test

--- components/fun-app/it/it_suite_test.go
+++ components/fun-app/it/it_suite_test.go
@@ -1,15 +1,27 @@
 package it_test
 
 import (
-	"os"
 	"time"
 
-	"github.com/amanhigh/go-fun/components/fun-app/common"
+	"github.com/amanhigh/go-fun/common/clients"
+	"github.com/amanhigh/go-fun/common/tools"
+	"github.com/amanhigh/go-fun/common/util"
 	"github.com/amanhigh/go-fun/models"
+	"github.com/fatih/color"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
 	"testing"
+)
+
+var (
+	err        error
+	cancelFunc util.CancelFunc
+)
+
+const (
+	COVER_CMD  = "./cover.zsh run"
+	HEALTH_URL = "http://localhost:8085/metrics"
 )
 
 func TestIt(t *testing.T) {
@@ -18,10 +30,30 @@ func TestIt(t *testing.T) {
 }
 
 var _ = BeforeSuite(func() {
-	// Override Port to Avoid Collision with Default APp
-	os.Setenv("PORT", "8085")
-	go common.RunFunApp()
+	cancelFunc, err = tools.RunBackgroundCommand(COVER_CMD)
+	Expect(err).ShouldNot(HaveOccurred())
 
-	//TODO:Avoid Sleep, Better Way ?
-	time.Sleep(time.Second)
+	//Health Check every 1 second until Healthy or 5 Second Timeout
+	for {
+		select {
+		case <-time.After(5 * time.Second):
+			Fail("Unable to Start Funapp")
+		case <-time.NewTicker(time.Second).C:
+			color.HiBlue("FunApp: Health Check")
+			if err = doHealthCheck(); err == nil {
+				return
+			}
+		}
+	}
 })
+
+var _ = AfterSuite(func() {
+	//Kill Live Command
+	err := cancelFunc()
+	Expect(err).ShouldNot(HaveOccurred())
+})
+
+func doHealthCheck() (err error) {
+	_, err = clients.TestHttpClient.R().Get(HEALTH_URL)
+	return
+}
