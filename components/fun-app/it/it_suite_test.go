b5b12354e4d1a6024f3c78e1c0c3c36fcef82938
Amanpreet Singh <amanpreet.singh@flipkart.com>
2023-11-13 16:31:16 +0530

    Run FunApp If Not Running

--- components/fun-app/it/it_suite_test.go
+++ components/fun-app/it/it_suite_test.go
@@ -1,11 +1,11 @@
 package it_test
 
 import (
+	"os"
 	"time"
 
 	"github.com/amanhigh/go-fun/common/clients"
-	"github.com/amanhigh/go-fun/common/tools"
-	"github.com/amanhigh/go-fun/common/util"
+	"github.com/amanhigh/go-fun/components/fun-app/common"
 	"github.com/amanhigh/go-fun/models"
 	"github.com/fatih/color"
 	. "github.com/onsi/ginkgo/v2"
@@ -20,9 +20,8 @@ const (
 )
 
 var (
-	err        error
-	cancelFunc util.CancelFunc
-	client     = clients.NewFunAppClient(BASE_URL)
+	err    error
+	client = clients.NewFunAppClient(BASE_URL)
 )
 
 func TestIt(t *testing.T) {
@@ -31,19 +30,24 @@ func TestIt(t *testing.T) {
 }
 
 var _ = BeforeSuite(func() {
-	cancelFunc, err = tools.RunBackgroundCommand(COVER_CMD)
-	Expect(err).ShouldNot(HaveOccurred())
+	//Run FunApp If not already running
+	if err = client.AdminService.HealthCheck(); err == nil {
+		color.HiGreen("FunApp: Running Already")
+	} else {
+		os.Setenv("PORT", "8085")
+		go common.RunFunApp()
 
-	//Health Check every 1 second until Healthy or 5 Second Timeout
-	timeout := time.After(10 * time.Second)
-	for {
-		select {
-		case <-timeout:
-			Fail("Unable to Start Funapp")
-		case <-time.NewTicker(time.Second).C:
-			color.HiBlue("FunApp: Health Check")
-			if err = client.AdminService.HealthCheck(); err == nil {
-				return
+		//Health Check every 1 second until Healthy or 5 Second Timeout
+		timeout := time.After(10 * time.Second)
+		for {
+			select {
+			case <-timeout:
+				Fail("Unable to Start Funapp")
+			case <-time.NewTicker(time.Second).C:
+				color.HiBlue("FunApp: Health Check")
+				if err = client.AdminService.HealthCheck(); err == nil {
+					return
+				}
 			}
 		}
 	}
