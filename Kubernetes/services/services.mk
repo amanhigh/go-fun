734c39a05858ca4d94ea61758eb341b4f434173a
Amanpreet Singh <coool.aman@gmail.com>
2023-12-08 22:27:47 +0530

    Migrated Remaining Services

--- Kubernetes/services/services.mk
+++ Kubernetes/services/services.mk
@@ -4,6 +4,8 @@ CMD=install
 ANS_FILE=/tmp/k8-svc.txt
 
 #TODO: Add Locust
+# Debug: find . | entr -s "helm template elasticsearch bitnami/elasticsearch -f elasticsearch.yml > debug.txt;./service.zsh -di"
+# sudo kubefwd svc | awk '{ if($2 ~ /Port-Forward/) {print $0" URL: http://"$4"/"} else {print}}'
 
 ### Basic
 help: ## Show this help
@@ -30,12 +32,45 @@ httpbin: ## Httpbin
 	@printf "\033[1;33m http://httpbin.docker/anything \033[0m \n"
 	@printf "\033[1;33m curl http://httpbin:8810/headers \033[0m \n"
 
-### Utility
-# FIXME: Cron needs sample jobs
-cron: ## Cron
+loader: ## Stress Testing
+	-helm $(CMD) vegeta onechart/onechart -f vegeta.yml > /dev/null
+	@printf "\033[1;33m Check Logs for Output \033[0m \n"
+	@printf "\033[1;33m echo 'GET http://nginx' | vegeta attack | vegeta report \033[0m \n"
+
+app: ## Fun Application
+	-helm $(CMD) app onechart/onechart -f app.yml > /dev/null
+	@printf "\033[1;33m http://app.docker/app/metrics\n http://app.docker/app/person/all \033[0m \n"
+	@printf "\033[1;33m echo 'GET http://app:9001/person/all' | vegeta attack | vegeta report \033[0m \n"
+    
+proxy: ## Proxy Servers
+	@printf "\033[1;32m Nginx \033[0m \n"
+	-helm $(CMD) nginx bitnami/nginx -f nginx.yml > /dev/null
+	@printf "\033[1;33m http://nginx.docker/ \033[0m \n"
+	@printf "\033[1;33m Refer Server Blocks for More Help.. \033[0m \n"
+
+	@printf "\033[1;32m Resty \033[0m \n"
+	-helm $(CMD) resty onechart/onechart -f resty.yml > /dev/null
+	@printf "\033[1;33m http://resty.docker/ \033[0m \n"
+	@printf "\033[1;33m http://resty.docker/example \033[0m \n"
+	@printf "\033[1;33m http://resty.docker/ndtv \033[0m \n"
+
+	@printf "\033[1;32m Squid \033[0m \n"
+	-helm $(CMD) squid onechart/onechart -f squid.yml > /dev/null
+	@printf "\033[1;33m ALLOW: curl -x localhost:3128 www.google.com \033[0m \n"
+	@printf "\033[1;33m DENY: curl -x localhost:3128 www.fb.com \033[0m \n"
+
+	@printf "\033[1;32m TinyProxy \033[0m \n"
+	-helm $(CMD) tinyproxy stakater/application -f tinyproxy.yml > /dev/null
+	@printf "\033[1;33m curl -x localhost:8888 tinyproxy.stats \033[0m \n"
+
+cron: ## Cron Server
 	-helm $(CMD) cron onechart/onechart -f rundeck.yml > /dev/null
 	@printf "\033[1;33m http://cron.docker \033[0m \n"
 	@printf "\033[1;33m http://cron.docker/health \033[0m \n"
+
+portainer: ## Portainer
+	-helm $(CMD) portainer portainer/portainer -f portainer.yml
+	@printf "\033[1;33m http://portainer.docker/ \033[0m \n"
 
 ### Security
 opa: ## Open Policy Agent
@@ -50,6 +85,20 @@ vault: ## Hashicorp Vault
 	-helm $(CMD) vault hashicorp/vault -f vault.yml > /dev/null
 	@printf "\033[1;33m vault status \033[0m \n"
 	@printf "\033[1;33m /demo/vault.sh \033[0m \n"
+
+sonar: ## Sonar
+	-helm $(CMD) sonar bitnami/sonarqube -f sonar.yml > /dev/null
+	@printf "\033[1;33m http://sonar.docker/ \033[0m \n"
+	@printf "\033[1;33m Login: aman/aman (Need 5GB Mem) \033[0m \n"
+
+webshell: ## Web Shell
+	-helm $(CMD) sshwifty onechart/onechart -f sshwifty.yml  > /dev/null
+	-helm $(CMD) webssh onechart/onechart -f webssh.yml > /dev/null
+	#FIXME: Fix Config for Web SSH
+	-helm $(CMD) webssh2 onechart/onechart -f webssh2.yml > /dev/null
+	@printf "\033[1;33m Sshwifty: http://sshwifty.docker/ \033[0m \n"
+	@printf "\033[1;33m Webssh: http://webssh.docker/ \033[0m \n"
+	@printf "\033[1;33m Webssh: http://webssh2.docker/ \033[0m \n"
 
 ### Databases
  mysql: ## MySQL
@@ -97,3 +146,38 @@ etcd: ## Etcd
 zookeeper: ## Zookeeper
 	-helm $(CMD) zookeeper bitnami/zookeeper -f zookeeper.yml > /dev/null
 	@printf "\033[1;33m /demo/demo.sh \033[0m \n"
+
+### Telemetry
+elk: ## ElasticSearch Kibana Logstash
+	# helm $(CMD) logstash bitnami/logstash -f logstash.yml > /dev/null
+	-helm $(CMD) elasticsearch bitnami/elasticsearch -f elasticsearch.yml > /dev/null
+	-helm $(CMD) kibana bitnami/kibana -f kibana.yml > /dev/null
+	@printf "\033[1;33m ElasticSearch: http://elastic.docker/_cluster/health?pretty \033[0m \n"
+	@printf "\033[1;33m Kibana: http://kibana.docker \033[0m \n"
+
+monitor: ## Prometheus, Grafana and Jaeger
+	-helm $(CMD) prometheus prometheus-community/prometheus -f prometheus.yml > /dev/null
+	@printf "\033[1;33m Prometheus Server: http://prometheus.docker/ \033[0m \n"
+	@printf "\033[1;33m Prometheus Query: http://prometheus.docker/api/v1/query \033[0m \n"
+
+	-helm $(CMD) grafana grafana/grafana -f grafana.yml > /dev/null
+	@printf "\033[1;33m http://grafana.docker/login (aman/aman) \033[0m \n"
+	@printf "\033[1;33m Datasource: http://grafana.docker/datasources/new \033[0m \n"
+	@printf "\033[1;33m Add Datasource Prometheus: http://prometheus-server \033[0m \n"
+
+	-helm $(CMD) jaeger jaegertracing/jaeger -f jaeger.yml > /dev/null
+	@printf "\033[1;33m http://jaeger.docker/ \033[0m \n"
+
+### Istio
+istio: ## Istio Service Mesh
+	-helm $(CMD) istio-base istio/base -n istio-system --create-namespace > /dev/null
+	-helm $(CMD) istiod istio/istiod -n istio-system -f istio.yml > /dev/null
+	@printf "\033[1;32m Enabled Istio for Default Namespace \033[0m \n"
+	# kubectl label namespace default istio-injection-
+	kubectl label namespace default istio-injection=enabled --overwrite
+
+kiali: ## Kiali Dashboard
+	-helm $(CMD) kiali-operator kiali/kiali-operator -f kiali.yml > /dev/null
+	#Create Kiali CRD
+	kubectl apply -f ./files/istio/kiali-crd.yml
+	@printf "\033[1;33m Kiali: http://kiali.docker/kiali \033[0m \n"
