d21a4dbbe5d5ba10c9b1e494555f5f643ae7284e
Amanpreet Singh <coool.aman@gmail.com>
2023-12-13 19:23:35 +0530

    Moved Seed Script

--- components/learn/tools/mysql/seed.sql
+++ components/learn/tools/mysql/seed.sql
@@ -1,10 +1,23 @@
+-- Active: 1702221591394@@docker@5432@play
+
 -- Create Sample Student Table
 
-CREATE TABLE
-    `student` (
-        `id` int(11) NOT NULL AUTO_INCREMENT,
-        `name` varchar(255) NOT NULL,
-        `age` int(11) NOT NULL,
-        `gender` varchar(10) NOT NULL,
-        PRIMARY KEY (`id`)
-    )
+CREATE TABLE students (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        age INTEGER NOT NULL,
+        gender VARCHAR(255) NOT NULL
+    );
+
+-- Insert 5 Sample Records
+
+INSERT INTO students (name, age, gender)
+VALUES
+('John', 20, 'Male'),
+('Jane', 21, 'Female'),
+('Bob', 22, 'Male'),
+('Sarah', 23, 'Female'),
+('Mike', 24, 'Male');
+
+-- Create Index on Name
+CREATE INDEX idx_name ON students (name);
