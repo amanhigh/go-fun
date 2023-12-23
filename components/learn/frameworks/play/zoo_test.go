51df2eeb79d7dac0dc387f9ce58a6bb7824f5ad1
Amanpreet Singh <coool.aman@gmail.com>
2023-12-23 14:43:53 +0530

    Fixed basic CRUD ZK Tests

--- components/learn/frameworks/play/zoo_test.go
+++ components/learn/frameworks/play/zoo_test.go
@@ -10,15 +10,15 @@ import (
 )
 
 // TODO:Complete Migration from zoo.go
-var _ = Describe("Zookeeper", Label(models.GINKGO_SETUP), func() {
+var _ = Describe("Zookeeper", Ordered, Label(models.GINKGO_SETUP), func() {
 	var (
 		connection *zk.Conn
 		err        error
 		//dman set zookeeper
 		zkHost = "docker"
 	)
-	BeforeEach(func() {
-		connection, _, err = zk.Connect([]string{zkHost}, time.Second) //*10)
+	BeforeAll(func() {
+		connection, _, err = zk.Connect([]string{zkHost}, time.Second)
 		Expect(err).To(BeNil())
 	})
 
@@ -26,35 +26,28 @@ var _ = Describe("Zookeeper", Label(models.GINKGO_SETUP), func() {
 		Expect(connection).To(Not(BeNil()))
 	})
 
-	// Context("Write", func() {
-	// 	var (
-	// 		testPath  = "/testPath"
-	// 		testValue = "Test Value"
-	// 		readValue []byte
-	// 	)
-	// 	BeforeEach(func() {
-	// 		_, err = connection.Create(testPath, []byte(testValue), 0, zk.WorldACL(zk.PermAll))
-	// 		Expect(err).To(BeNil())
-	// 	})
+	Context("Write", func() {
+		var (
+			testPath  = "/testPath"
+			testValue = "Test Value"
+			readValue []byte
+		)
+		BeforeEach(func() {
+			_, err = connection.Create(testPath, []byte(testValue), 0, zk.WorldACL(zk.PermAll))
+			Expect(err).To(BeNil())
+		})
 
-	// 	It("should have Read Value", func() {
-	// 		readValue, _, err = connection.Get(testPath)
-	// 		Expect(string(readValue)).To(Equal(testValue))
+		AfterEach(func() {
+			err = connection.Delete(testPath, -1)
+			Expect(err).To(BeNil())
+		})
 
-	// 	})
+		It("should have Read Value", func() {
+			readValue, _, err = connection.Get(testPath)
+			Expect(string(readValue)).To(Equal(testValue))
 
-	// 	Context("Delete Value", func() {
-	// 		BeforeEach(func() {
-	// 			err = connection.Delete(testPath, -1)
-	// 			Expect(err).To(BeNil())
-	// 		})
-
-	// 		It("should have No Value", func() {
-	// 			readValue, _, err = connection.Get(testPath)
-	// 			Expect(string(readValue)).To(Equal(""))
-	// 		})
-	// 	})
-	// })
+		})
+	})
 
 })
 
