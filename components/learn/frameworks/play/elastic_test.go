728103607609e33879354c9cd252c96e539d24fe
Amanpreet Singh <coool.aman@gmail.com>
2023-12-29 21:43:52 +0530

    Inserted Records.

--- components/learn/frameworks/play/elastic_test.go
+++ components/learn/frameworks/play/elastic_test.go
@@ -1,14 +1,23 @@
 package play_test
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"time"
+
 	"github.com/amanhigh/go-fun/models"
+	"github.com/amanhigh/go-fun/models/learn/frameworks"
 	es "github.com/elastic/go-elasticsearch"
+	"github.com/elastic/go-elasticsearch/esapi"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/teris-io/shortid"
 )
 
 /*
  Guide - https://www.elastic.co/guide/en/elasticsearch/client/go-api/current/getting-started-go.html
+ Store Example - https://github.com/elastic/go-elasticsearch/blob/main/_examples/xkcdsearch/store.go
  Index - http://docker:9200/learn/_search?pretty
 */
 
@@ -17,7 +26,7 @@ var _ = Describe("Elastic", Label(models.GINKGO_SETUP), func() {
 		elasticClient *es.Client
 		endpoint      = "http://docker:9200"
 		err           error
-		// ctx           = context.Background()
+		ctx           = context.Background()
 	)
 
 	BeforeEach(func() {
@@ -36,10 +45,6 @@ var _ = Describe("Elastic", Label(models.GINKGO_SETUP), func() {
 		var (
 			indexName = "learn"
 			// typeName  = "learnType"
-			// docs      = []frameworks.DocumentRequest{
-			// 	{Title: "Aman", Content: "Preet"},
-			// 	{Title: "John", Content: "Doe"},
-			// }
 		// query = "aman"
 		// skip  = 1
 		// top   = 2
@@ -48,29 +53,41 @@ var _ = Describe("Elastic", Label(models.GINKGO_SETUP), func() {
 			//Create Index
 			_, err = elasticClient.Indices.Create(indexName)
 			Expect(err).To(BeNil())
-
-			// Insert documents in bulk
-			// bulk := elasticClient.Bulk().Index(indexName).Type(typeName)
-			// for _, d := range docs {
-			// 	doc := frameworks.Document{
-			// 		ID:        shortid.MustGenerate(),
-			// 		Title:     d.Title,
-			// 		CreatedAt: time.Now().UTC(),
-			// 		Content:   d.Content,
-			// 	}
-			// 	bulk.Add(elastic.NewBulkIndexRequest().Id(doc.ID).Doc(doc))
-			// }
-			// _, err = bulk.Do(ctx)
-			// Expect(err).To(BeNil())
-		})
-
-		AfterEach(func() {
-			//Delete Records
 		})
 
 		It("should exist", func() {
 			_, err = elasticClient.Indices.Exists([]string{indexName})
 			Expect(err).To(BeNil())
+		})
+
+		Context("Document", func() {
+			var (
+				docs = []frameworks.Document{
+					{ID: shortid.MustGenerate(), Title: "Aman", Content: "Preet", CreatedAt: time.Now().UTC()},
+					{ID: shortid.MustGenerate(), Title: "John", Content: "Doe", CreatedAt: time.Now().UTC()},
+				}
+			)
+
+			BeforeEach(func() {
+				// Insert documents in bulk
+				for _, doc := range docs {
+					payload, _ := json.Marshal(doc)
+
+					_, err = esapi.CreateRequest{
+						Index:      indexName,
+						DocumentID: doc.ID,
+						Body:       bytes.NewReader(payload),
+					}.Do(ctx, elasticClient)
+					Expect(err).To(BeNil())
+				}
+			})
+
+			It("should exist", func() {
+				// exists, err := esapi.Exists(indexName, docs[0].ID)
+				// Expect(err).To(BeNil())
+				// Expect(exists).To(BeTrue())
+			})
+
 		})
 	})
 })
