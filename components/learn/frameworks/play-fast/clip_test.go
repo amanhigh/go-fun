9b18f843b0bf504880257d649a5870fdff4ab44f
Amanpreet Singh <coool.aman@gmail.com>
2024-03-09 13:06:56 +0530

    Added Watch Test for Clip

--- components/learn/frameworks/play-fast/clip_test.go
+++ components/learn/frameworks/play-fast/clip_test.go
@@ -1,6 +1,8 @@
 package play_fast_test
 
 import (
+	"context"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"golang.design/x/clipboard"
@@ -10,6 +12,7 @@ var _ = FDescribe("Clipboard", func() {
 	var (
 		err      error
 		testData = "CopyThis!!"
+		ctx      = context.Background()
 	)
 
 	BeforeEach(func() {
@@ -21,9 +24,7 @@ var _ = FDescribe("Clipboard", func() {
 	})
 
 	Context("Paste", func() {
-		var (
-			ch <-chan struct{}
-		)
+		var ch <-chan struct{}
 		BeforeEach(func() {
 			ch = clipboard.Write(clipboard.FmtText, []byte(testData))
 		})
@@ -35,8 +36,24 @@ var _ = FDescribe("Clipboard", func() {
 
 		It("should signal overwrite", func() {
 			// Overwrite Clipboard
-			clipboard.Write(clipboard.FmtText, []byte(""))
+			clipboard.Write(clipboard.FmtText, []byte("Overwrite"))
 			Eventually(ch, 1).Should(Receive())
+		})
+	})
+
+	Context("Watch", func() {
+		var (
+			ch        <-chan []byte
+			watchText = "I am Watching!!"
+		)
+
+		BeforeEach(func() {
+			ch = clipboard.Watch(ctx, clipboard.FmtText)
+		})
+
+		It("should work", func() {
+			clipboard.Write(clipboard.FmtText, []byte(watchText))
+			Eventually(ch, "2s").Should(Receive(Equal([]byte(watchText))))
 		})
 	})
 })
