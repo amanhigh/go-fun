4f364ab36aedf3d672011b06dcd326209bcd6e49
Amanpreet Singh <coool.aman@gmail.com>
2024-03-16 02:21:46 +0530

    Added Zap Logger

--- components/learn/frameworks/play-fast/log_test.go
+++ components/learn/frameworks/play-fast/log_test.go
@@ -8,6 +8,9 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/sirupsen/logrus"
 	"github.com/sirupsen/logrus/hooks/test"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zaptest/observer"
 )
 
 // https://www.sentinelone.com/blog/log-formatting-best-practices-readable/
@@ -25,28 +28,31 @@ var _ = FDescribe("Logging", func() {
 		var (
 			logger *logrus.Logger
 			err    error
+			rus    = "logrus"
 		)
 
 		It("should build", func() {
 			logger = logrus.New()
-			logger.SetFormatter(&logrus.TextFormatter{
-				FullTimestamp: true,
-				ForceColors:   true,
-			})
 			Expect(logger).To(Not(BeNil()))
 		})
 
 		Context("StdOut", func() {
 			BeforeEach(func() {
 				logger.Out = os.Stdout
+				// Formatter
+				logger.SetFormatter(&logrus.TextFormatter{
+					FullTimestamp: true,
+					ForceColors:   true,
+				})
 			})
 
 			It("should write log", func() {
-				logger.WithField("Logger", "logrus").Info(msgStdout)
+				logger.WithField("Logger", rus).Info(msgStdout)
 			})
 
 			It("should print fields", func() {
 				fields := logrus.Fields{
+					"Logger": rus,
 					"Field1": field1,
 					"Field2": field2,
 				}
@@ -96,4 +102,91 @@ var _ = FDescribe("Logging", func() {
 			Expect(hook.LastEntry()).To(BeNil())
 		})
 	})
+
+	Context("Zap", func() {
+		var (
+			logger *zap.Logger
+			err    error
+			zapStr = "zap"
+		)
+
+		It("should build", func() {
+			logger, err = zap.NewProduction()
+			Expect(err).To(BeNil())
+			Expect(logger).To(Not(BeNil()))
+		})
+
+		Context("StdOut", func() {
+			BeforeEach(func() {
+				// Formatter
+				config := zap.NewDevelopmentConfig()
+				config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+				config.EncoderConfig.ConsoleSeparator = " | "
+
+				logger, err = config.Build()
+				Expect(err).To(BeNil())
+			})
+
+			It("should write log", func() {
+				logger.Info(msgStdout, zap.String("Logger", zapStr))
+			})
+
+			It("should print fields", func() {
+				fields := zap.Fields(
+					zap.String("Logger", zapStr),
+					zap.String("Field1", field1),
+					zap.String("Field2", field2),
+				)
+
+				logger.WithOptions(fields).Info(msgStdout)
+			})
+		})
+
+		Context("File", func() {
+			BeforeEach(func() {
+				file, err = util.OpenOrCreateFile(log_file)
+				Expect(err).To(BeNil())
+				logger = zap.New(zapcore.NewCore(
+					zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+					zapcore.AddSync(file),
+					zap.InfoLevel,
+				))
+			})
+
+			AfterEach(func() {
+				err = os.Remove(log_file)
+				Expect(err).To(BeNil())
+			})
+
+			It("should write log", func() {
+				logger.Info(msgFile)
+				lines := util.ReadAllLines(log_file)
+				Expect(len(lines)).To(Equal(1))
+				Expect(lines[0]).To(ContainSubstring(msgFile))
+			})
+
+			It("should write json log", func() {
+				logger.Info(msgFile)
+				lines := util.ReadAllLines(log_file)
+				Expect(len(lines)).To(Equal(1))
+				Expect(lines[0]).To(ContainSubstring(msgFile))
+				Expect(lines[0]).To(ContainSubstring(`"level":"info"`))
+			})
+		})
+
+		It("should have test logger", func() {
+			core, recorded := observer.New(zap.InfoLevel)
+			logger := zap.New(core)
+
+			logger.Info(msgFile)
+
+			Expect(len(recorded.All())).To(Equal(1))
+			Expect(recorded.All()[0].Message).To(Equal(msgFile))
+			Expect(recorded.All()[0].Level).To(Equal(zap.InfoLevel))
+			recorded.TakeAll()
+
+			Expect(len(recorded.All())).To(BeZero())
+		})
+	})
+
 })
