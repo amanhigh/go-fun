ce99b996af6115e385c99ddd9566106cce06b41f
Amanpreet Singh <coool.aman@gmail.com>
2024-03-17 01:20:49 +0530

    Added Zero OTEL Hook.

--- common/telemetry/logger.go
+++ common/telemetry/logger.go
@@ -8,6 +8,14 @@ import (
 	"github.com/rs/zerolog/log"
 	"github.com/sirupsen/logrus"
 	"github.com/uptrace/opentelemetry-go-extra/otellogrus"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
+)
+
+const (
+	logSeverityKey = attribute.Key("log.severity")
+	logMessageKey  = attribute.Key("log.message")
 )
 
 /*
@@ -60,4 +68,41 @@ func InitLogger(level zerolog.Level) {
 	// HACK: Add Environment Support to Switch Dev vs Prod
 	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: "2006-01-02 15:04:05"}
 	log.Logger = zerolog.New(output).With().Timestamp().Logger()
+
+	// Add the OTEL Trace Hook
+	log.Hook(NewZeroOtelHook())
+}
+
+// ZeroOtelHook is a Zerolog hook that adds logs to the active span as events.
+type ZeroOtelHook struct {
+}
+
+// NewZeroOtelHook returns a Zerolog hook.
+func NewZeroOtelHook() zerolog.Hook {
+	return &ZeroOtelHook{}
+}
+
+// Run adds trace context to the logger context.
+func (h *ZeroOtelHook) Run(e *zerolog.Event, level zerolog.Level, msg string) {
+	ctx := e.GetCtx()
+	if ctx == nil {
+		return
+	}
+
+	span := trace.SpanFromContext(ctx)
+	if !span.IsRecording() {
+		return
+	}
+
+	// Add events to the span
+	attrs := make([]attribute.KeyValue, 0, 2)
+	attrs = append(attrs, logSeverityKey.String(level.String()))
+	attrs = append(attrs, logMessageKey.String(msg))
+
+	span.AddEvent("log", trace.WithAttributes(attrs...))
+
+	// Set status if level is error
+	if level <= zerolog.ErrorLevel {
+		span.SetStatus(codes.Error, msg)
+	}
 }
