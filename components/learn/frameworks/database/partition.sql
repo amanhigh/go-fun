7507fa0d145829687eb27e771c853ea864370d6d
Amanpreet Singh <amanpreet.singh@flipkart.com>
2024-03-25 14:52:47 +0530

    Improved Partitioning Example.

--- components/learn/frameworks/database/partition.sql
+++ components/learn/frameworks/database/partition.sql
@@ -1,41 +1,48 @@
 -- Partition by created date year and month.
-CREATE TABLE student
-(
-    `id`            bigint       NOT NULL AUTO_INCREMENT,
-    `name`          varchar(100) NOT NULL DEFAULT '0',
-    `class`         varchar(100) NOT NULL,
-    `created`       datetime     NOT NULL DEFAULT NOW(),
-    `partition_key` bigint       NOT NULL,
-    PRIMARY KEY (id, partition_key)
-) PARTITION BY LIST (partition_key ) (
-    PARTITION p202101 VALUES IN (202101),
-    PARTITION p202102 VALUES IN (202102)
+CREATE TABLE student (
+    `id` bigint NOT NULL AUTO_INCREMENT, `name` varchar(100) NOT NULL DEFAULT '0', `class` varchar(100) NOT NULL, `created` datetime NOT NULL DEFAULT NOW(), PRIMARY KEY (id, created)
+)
+PARTITION BY
+    RANGE (
+        YEAR(created) * 100 + MONTH(created)
+    ) (
+        PARTITION p202101
+        VALUES
+            LESS THAN (202102), PARTITION p202102
+        VALUES
+            LESS THAN (202103), PARTITION p202103
+        VALUES
+            LESS THAN (202104), PARTITION pLAST
+        VALUES
+            LESS THAN MAXVALUE
     );
 
-
 -- Data added goes to respective partition (seprate db file).
-insert into student
-values (DEFAULT, 'Aman', '10th', DEFAULT, 202101);
-insert into student
-values (DEFAULT, 'Preet', '11th', DEFAULT, 202101);
-insert into student
-values (DEFAULT, 'Singh', '12th', DEFAULT, 202102);
+insert into student values ( DEFAULT, 'Aman', '10th', '2021-01-01' );
 
-select *
-from student
-where partition_key = 202102;
+insert into
+    student
+values (
+        DEFAULT, 'Preet', '11th', '2021-02-01'
+    );
+
+insert into
+    student
+values (
+        DEFAULT, 'Singh', '12th', '2021-03-01'
+    );
+
+select * from student where created = '2021-01-01';
 
 -- Faster operations when partition column included.
-explain
-select *
-from student
-where partition_key = 202102;
+explain select * from student where created = '2021-01-01';
 
 -- Check Partition Row Count.
 SELECT PARTITION_NAME, TABLE_ROWS
 FROM INFORMATION_SCHEMA.PARTITIONS
-WHERE TABLE_SCHEMA = Database()
-  AND TABLE_NAME = 'student';
+WHERE
+    TABLE_SCHEMA = Database()
+    AND TABLE_NAME = 'student';
 
 -- Can Clear Partition if to be reused.
 alter table student truncate partition p202101;
@@ -43,15 +50,21 @@ alter table student truncate partition p202101;
 # alter table student drop partition p202103;
 
 -- Can Add more Partitions
-alter table student
-    add partition (
-        PARTITION p202103 VALUES IN (202103),
-        PARTITION p202104 VALUES IN (202104)
-        );
+ALTER TABLE student REORGANIZE PARTITION pLAST INTO (
+    PARTITION p202104
+    VALUES
+        LESS THAN (202105), PARTITION p202105
+    VALUES
+        LESS THAN (202106), PARTITION pLAST
+    VALUES
+        LESS THAN MAXVALUE
+);
 
 -- Data goes to newly added Partition
-insert into student
-values (DEFAULT, 'Sachin', '12th', DEFAULT, 202103);
-
+insert into
+    student
+values (
+        DEFAULT, 'Sachin', '6th', '2021-05-01'
+    );
 
 DROP TABLE student;
