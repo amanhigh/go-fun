0864af4c4a0a2772df2c046aeee34853a49add8e
Amanpreet Singh <coool.aman@gmail.com>
2024-06-22 20:30:11 +0530

    Added Auto Test

---
+++ components/kohan/command/auto_test.go
@@ -0,0 +1,38 @@
+package command
+
+import (
+	"bytes"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Auto", func() {
+	var (
+		actual = new(bytes.Buffer)
+	)
+
+	BeforeEach(func() {
+		actual.Reset()
+		RootCmd.SetOut(actual)
+		RootCmd.SetErr(actual)
+
+		logActual.Reset()
+	})
+
+	Context("Run Or Focus", func() {
+		It("should enable debug", func() {
+			RootCmd.SetArgs([]string{"auto", "run-or-focus", "ls", "-d"})
+
+			Expect(RootCmd.Execute()).Should(Succeed())
+			Expect(logActual.String()).To(ContainSubstring("Debug Mode Enabled"))
+		})
+
+		It("should error with no args", func() {
+			RootCmd.SetArgs([]string{"auto", "run-or-focus"})
+			Expect(RootCmd.Execute()).ShouldNot(Succeed())
+			Expect(actual.String()).To(ContainSubstring("accepts 1 arg"))
+		})
+	})
+
+})
