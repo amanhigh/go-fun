14ec74f704dfde9b354bc2d0ece9d4bef8d9ec07
Amanpreet Singh <coool.aman@gmail.com>
2024-06-29 13:50:21 +0530

    Added Context and Command View

--- components/kohan/tui/ui.go
+++ components/kohan/tui/ui.go
@@ -5,21 +5,32 @@ import (
 )
 
 type UIManager struct {
-	app      *tview.Application
-	mainFlex *tview.Flex
-	svcList  *tview.List
+	app         *tview.Application
+	mainFlex    *tview.Flex
+	svcList     *tview.List
+	contextView *tview.TextView
+	commandView *tview.TextView
 }
 
 func NewUIManager(app *tview.Application, services []string) *UIManager {
 	return &UIManager{
-		app:      app,
-		mainFlex: tview.NewFlex(),
-		svcList:  createList("Services", services),
+		app:         app,
+		mainFlex:    tview.NewFlex(),
+		contextView: createTextView("Context"),
+		commandView: createTextView("Command"),
+		svcList:     createList("Services", services),
 	}
 }
 
 func (ui *UIManager) SetupLayout() {
-	ui.mainFlex.AddItem(ui.svcList, 0, 1, true)
+	leftPane := tview.NewFlex().SetDirection(tview.FlexRow).
+		AddItem(ui.svcList, 0, 1, true)
+	rightPane := tview.NewFlex().SetDirection(tview.FlexRow).
+		AddItem(ui.contextView, 0, 1, false).
+		AddItem(ui.commandView, 0, 1, false)
+	ui.mainFlex.AddItem(leftPane, 0, 1, true)
+	ui.mainFlex.AddItem(rightPane, 0, 1, false)
+	ui.mainFlex.SetTitle("Helm Manager").SetBorder(true)
 	ui.app.SetRoot(ui.mainFlex, true)
 }
 
@@ -30,4 +41,10 @@ func createList(title string, items []string) *tview.List {
 		list.AddItem(item, "", 0, nil)
 	}
 	return list
+}
+
+func createTextView(title string) *tview.TextView {
+	tv := tview.NewTextView()
+	tv.SetBorder(true).SetTitle(title)
+	return tv
 }
