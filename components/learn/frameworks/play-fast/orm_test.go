3d2902a2064b21738d3e62c86ea9ecf15a150005
Amanpreet Singh <coool.aman@gmail.com>
2024-07-06 03:19:27 +0530

    Added ORM Base Test

--- components/learn/frameworks/play-fast/orm_test.go
+++ components/learn/frameworks/play-fast/orm_test.go
@@ -3,10 +3,40 @@ package play_fast_test
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 
-	"github.com/amanhigh/go-fun/components/learn/frameworks/play-fast"
+	"github.com/amanhigh/go-fun/common/util"
+	"github.com/amanhigh/go-fun/models/learn/frameworks"
 )
 
-var _ = Describe("Orm", func() {
+var _ = FDescribe("Orm", func() {
+	var db *gorm.DB
+
+	BeforeEach(func() {
+		db, _ = util.CreateTestDb(logger.Info)
+	})
+
+	It("should connect", func() {
+		// Verify Connection
+		Expect(db).To(Not(BeNil()))
+	})
+
+	Context("Create Table", func() {
+		BeforeEach(func() {
+			db.AutoMigrate(&frameworks.Product{}, &frameworks.AuditLog{})
+		})
+
+		AfterEach(func() {
+			db.Migrator().DropTable(&frameworks.Product{})
+			db.Migrator().DropTable(&frameworks.AuditLog{})
+		})
+
+		It("should create Tables", func() {
+			// Verify Tables Created
+			Expect(db.Migrator().HasTable(&frameworks.Product{})).To(BeTrue())
+			Expect(db.Migrator().HasTable(&frameworks.AuditLog{})).To(BeTrue())
+		})
+	})
 
 })
