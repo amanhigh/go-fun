a941de083d1bbc20e608ca0325dce9073e8bce68
Amanpreet Singh <coool.aman@gmail.com>
2024-07-06 03:06:32 +0530

    Added ORM Test

---
+++ components/learn/frameworks/play-fast/orm_test.go
@@ -0,0 +1,12 @@
+package play_fast_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/amanhigh/go-fun/components/learn/frameworks/play-fast"
+)
+
+var _ = Describe("Orm", func() {
+
+})
