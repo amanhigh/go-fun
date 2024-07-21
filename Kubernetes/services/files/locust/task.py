2baea5ddc80534cc72064192ffa6a7fb65f068c5
Amanpreet Singh <coool.aman@gmail.com>
2024-07-21 13:28:15 +0530

    App Metric Locust working

--- Kubernetes/services/files/locust/task.py
+++ Kubernetes/services/files/locust/task.py
@@ -1,20 +1,10 @@
 from locust import HttpUser, task, between
 
-class MyUser(HttpUser):
+# https://docs.locust.io/en/stable/writing-a-locustfile.html
+
+class MetricsUser(HttpUser):
     wait_time = between(1, 5)
 
     @task
-    def visit_homepage(self):
-        self.client.get("/")
-
-    @task(2)
-    def visit_about_page(self):
-        self.client.get("/about")
-
-    @task(3)
-    def api_request(self):
-        self.client.get("/api/data")
-
-    def on_start(self):
-        # Log in at the start of each simulated user session
-        self.client.post("/login", json={"username": "testuser", "password": "testpass"})
+    def get_metrics(self):
+        self.client.get("/metrics")
