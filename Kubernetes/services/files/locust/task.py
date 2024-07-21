776214ec6f6060efff0521228e3dc571ab1486bb
Amanpreet Singh <coool.aman@gmail.com>
2024-07-21 13:32:30 +0530

    List Task

--- Kubernetes/services/files/locust/task.py
+++ Kubernetes/services/files/locust/task.py
@@ -1,10 +1,20 @@
 from locust import HttpUser, task, between
+import random
 
-# https://docs.locust.io/en/stable/writing-a-locustfile.html
+API_VERSION = "v1"
 
 class MetricsUser(HttpUser):
     wait_time = between(1, 5)
 
-    @task
+    @task(1)
     def get_metrics(self):
         self.client.get("/metrics")
+
+    @task(2)
+    def list_persons(self):
+        limit = random.randint(2, 5)
+        params = {
+            "offset": 0,
+            "limit": limit
+        }
+        self.client.get(f"/{API_VERSION}/person", params=params)
