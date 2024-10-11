202ad51a566c96a9c26e49c34bb1ef7ef79459bf
Amanpreet Singh <coool.aman@gmail.com>
2024-10-11 10:29:00 +0530

    Merged Http Util

--- common/clients/http_client.go
+++ common/clients/http_client.go
@@ -1,11 +1,14 @@
 package clients
 
 import (
+	"net"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
 	"time"
 
+	"github.com/amanhigh/go-fun/models/config"
+	"github.com/dubonzi/otelresty"
 	"github.com/go-resty/resty/v2"
 	"github.com/rs/zerolog/log"
 )
@@ -24,8 +27,6 @@ var DefaultHttpClient = resty.New().SetTimeout(REQUEST_TIMEOUT).SetTransport(&ht
 	DisableCompression: true,
 })
 
-var TestHttpClient = resty.New()
-
 func NewHttpClientWithCookies(cookieUrl string, cookies []*http.Cookie, client *resty.Client) *resty.Client {
 	cookieJar, _ := cookiejar.New(nil)
 	if u, err := url.Parse(cookieUrl); err == nil {
@@ -34,4 +35,30 @@ func NewHttpClientWithCookies(cookieUrl string, cookies []*http.Cookie, client *
 		log.Error().Err(err).Msg("Error Setting Cookies in HttpClient")
 	}
 	return client.SetCookieJar(cookieJar)
+}
+
+func NewRestyClient(baseUrl string, httpConfig config.HttpClientConfig) (client *resty.Client) {
+	//Init Client
+	client = resty.New().SetBaseURL(baseUrl)
+
+	//Default Header
+	// client.SetHeader("Content-Type", "application/json")
+
+	//Tracing
+	otelresty.TraceClient(client, otelresty.WithTracerName("resty-sdk"))
+
+	//Configure Http Config
+	client.SetTimeout(httpConfig.RequestTimeout)
+
+	transport := http.Transport{
+		DisableCompression: !httpConfig.Compression,
+		DisableKeepAlives:  !httpConfig.KeepAlive,
+		DialContext: (&net.Dialer{
+			Timeout: httpConfig.DialTimeout, // Connect Timeout
+		}).DialContext,
+		IdleConnTimeout:     httpConfig.IdleConnectionTimeout, // Idle Timeout Before Closing Keepalive Connection
+		MaxIdleConnsPerHost: httpConfig.IdleConnectionsPerHost,
+	}
+	client.SetTransport(&transport)
+	return
 }
