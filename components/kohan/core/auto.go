046f6d4743fab4264ac6e29a7a7cecef07c69367
Amanpreet Singh <coool.aman@gmail.com>
2025-01-20 23:18:09 +0530

    x11 libs

--- components/kohan/core/auto.go
+++ components/kohan/core/auto.go
@@ -35,6 +35,7 @@ Support:
 -`
 )
 
+// TODO: Extract AutoManager
 func OpenTicker(ticker string) (err error) {
 	// Focus on the window named "TradingView"
 	log.Debug().Str("Ticker", ticker).Msg("OpenTicker")
