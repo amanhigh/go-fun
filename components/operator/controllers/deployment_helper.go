0fc108046f3f4a69f1385c7074a0e03dd2a78345
Amanpreet Singh <coool.aman@gmail.com>
2025-01-24 17:25:54 +0530

    Methods

--- components/operator/controllers/deployment_helper.go
+++ components/operator/controllers/deployment_helper.go
@@ -1,29 +1,170 @@
 package controllers
 
 import (
-    "context"
-    cachev1beta1 "github.com/amanhigh/go-fun/components/operator/api/v1beta1"
-    appsv1 "k8s.io/api/apps/v1"
-    corev1 "k8s.io/api/core/v1"
-    ctrl "sigs.k8s.io/controller-runtime"
+	"context"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	cachev1beta1 "github.com/amanhigh/go-fun/components/operator/api/v1beta1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
-/* DeploymentHelper handles all deployment related operations
+/*
+	DeploymentHelper handles all deployment related operations
+
 for the Memcached controller including creation, updates and
-security configuration. */
+security configuration.
+*/
 type DeploymentHelper interface {
-    CreateNewDeployment(ctx context.Context, memcached *cachev1beta1.Memcached) (ctrl.Result, error)
-    GenerateDeploymentSpec(memcached *cachev1beta1.Memcached) (*appsv1.DeploymentSpec, error)
-    GeneratePodSpec(memcached *cachev1beta1.Memcached, image string) (*corev1.PodSpec, error)
-    GenerateSecurityContext() *corev1.SecurityContext
+	CreateNewDeployment(ctx context.Context, memcached *cachev1beta1.Memcached) (ctrl.Result, error)
+	GenerateDeploymentSpec(memcached *cachev1beta1.Memcached) (*appsv1.DeploymentSpec, error)
+	GeneratePodSpec(memcached *cachev1beta1.Memcached, image string) (*corev1.PodSpec, error)
+	GenerateSecurityContext() *corev1.SecurityContext
 }
 
 type deploymentHelperImpl struct {
-    controller *MemcachedReconciler
+	controller *MemcachedReconciler
 }
 
 func NewDeploymentHelper(controller *MemcachedReconciler) DeploymentHelper {
-    return &deploymentHelperImpl{
-        controller: controller,
-    }
+	return &deploymentHelperImpl{
+		controller: controller,
+	}
+}
+
+func (d *deploymentHelperImpl) CreateNewDeployment(ctx context.Context, memcached *cachev1beta1.Memcached) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
+
+	// Generate deployment spec
+	deploymentSpec, err := d.GenerateDeploymentSpec(memcached)
+	if err != nil {
+		log.Error(err, "Failed to generate deployment spec")
+		return ctrl.Result{}, err
+	}
+
+	// Create deployment object
+	dep := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      memcached.Name,
+			Namespace: memcached.Namespace,
+		},
+		Spec: *deploymentSpec,
+	}
+
+	// Set controller reference
+	if err := ctrl.SetControllerReference(memcached, dep, d.controller.Scheme); err != nil {
+		log.Error(err, "Failed to set controller reference")
+		return ctrl.Result{}, err
+	}
+
+	// Create deployment
+	log.Info("Creating a new Deployment",
+		"Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+	if err := d.controller.Create(ctx, dep); err != nil {
+		log.Error(err, "Failed to create deployment")
+		return ctrl.Result{}, err
+	}
+
+	return ctrl.Result{RequeueAfter: time.Minute}, nil
+}
+
+func (d *deploymentHelperImpl) GenerateDeploymentSpec(memcached *cachev1beta1.Memcached) (*appsv1.DeploymentSpec, error) {
+	// Get image and labels
+	image, err := d.getMemcachedImage()
+	if err != nil {
+		return nil, err
+	}
+	ls := d.getLabels(memcached.Name, image)
+
+	// Generate pod spec
+	podSpec, err := d.GeneratePodSpec(memcached, image)
+	if err != nil {
+		return nil, err
+	}
+
+	replicas := memcached.Spec.Size
+	return &appsv1.DeploymentSpec{
+		Replicas: &replicas,
+		Selector: &metav1.LabelSelector{
+			MatchLabels: ls,
+		},
+		Template: corev1.PodTemplateSpec{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels: ls,
+			},
+			Spec: *podSpec,
+		},
+	}, nil
+}
+
+func (d *deploymentHelperImpl) GeneratePodSpec(memcached *cachev1beta1.Memcached, image string) (*corev1.PodSpec, error) {
+	return &corev1.PodSpec{
+		SecurityContext: &corev1.PodSecurityContext{
+			RunAsNonRoot: &[]bool{true}[0],
+			SeccompProfile: &corev1.SeccompProfile{
+				Type: corev1.SeccompProfileTypeRuntimeDefault,
+			},
+		},
+		Containers: []corev1.Container{
+			{
+				Image:           image,
+				Name:            "memcached",
+				ImagePullPolicy: corev1.PullIfNotPresent,
+				SecurityContext: d.GenerateSecurityContext(),
+				Command:         []string{"memcached", "-m=64", "modern", "-v"},
+			},
+			{
+				Image:           memcached.Spec.SidecarImage,
+				Name:            "sidecar",
+				ImagePullPolicy: corev1.PullIfNotPresent,
+				Command:         []string{"sleep", "infinity"},
+				SecurityContext: d.GenerateSecurityContext(),
+			},
+		},
+	}, nil
+}
+
+func (d *deploymentHelperImpl) GenerateSecurityContext() *corev1.SecurityContext {
+	return &corev1.SecurityContext{
+		RunAsNonRoot:             &[]bool{true}[0],
+		RunAsUser:                &[]int64{1001}[0],
+		AllowPrivilegeEscalation: &[]bool{false}[0],
+		Capabilities: &corev1.Capabilities{
+			Drop: []corev1.Capability{"ALL"},
+		},
+	}
+}
+
+// Helper methods
+func (d *deploymentHelperImpl) getMemcachedImage() (string, error) {
+	var imageEnvVar = "MEMCACHED_IMAGE"
+	image, found := os.LookupEnv(imageEnvVar)
+	if !found {
+		return "", fmt.Errorf("Unable to find %s environment variable with the image", imageEnvVar)
+	}
+	return image, nil
+}
+
+func (d *deploymentHelperImpl) getLabels(name string, image string) map[string]string {
+	imageTag := "latest"
+	if image != "" {
+		imageParts := strings.Split(image, ":")
+		if len(imageParts) > 1 {
+			imageTag = imageParts[1]
+		}
+	}
+
+	return map[string]string{
+		"app.kubernetes.io/name":       "Memcached",
+		"app.kubernetes.io/instance":   name,
+		"app.kubernetes.io/version":    imageTag,
+		"app.kubernetes.io/part-of":    "operator",
+		"app.kubernetes.io/created-by": "controller-manager",
+	}
 }
--- components/operator/controllers/reconciliation_helper.go
+++ components/operator/controllers/reconciliation_helper.go
@@ -2,9 +2,15 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
 	cachev1beta1 "github.com/amanhigh/go-fun/components/operator/api/v1beta1"
+	appsv1 "k8s.io/api/apps/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 /*
@@ -28,4 +34,107 @@ func NewReconciliationHelper(controller *MemcachedReconciler) ReconciliationHelp
 	return &reconciliationHelperImpl{
 		controller: controller,
 	}
+}
+
+func (r *reconciliationHelperImpl) FetchMemcachedInstance(ctx context.Context, req ctrl.Request) (*cachev1beta1.Memcached, error) {
+	log := log.FromContext(ctx)
+	memcached := &cachev1beta1.Memcached{}
+
+	if err := r.controller.Get(ctx, req.NamespacedName, memcached); err != nil {
+		if apierrors.IsNotFound(err) {
+			log.Info("memcached resource not found. Ignoring since object must be deleted")
+			return nil, nil
+		}
+		log.Error(err, "Failed to get memcached")
+		return nil, err
+	}
+	return memcached, nil
+}
+
+func (r *reconciliationHelperImpl) HandleFinalizers(ctx context.Context, memcached *cachev1beta1.Memcached) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
+
+	// Check if object is being deleted
+	if memcached.GetDeletionTimestamp() != nil {
+		if controllerutil.ContainsFinalizer(memcached, memcachedFinalizer) {
+			// Begin finalizer operations
+			log.Info("Performing Finalizer Operations for Memcached before delete CR")
+
+			// Update status to indicate deletion
+			if err := r.controller.statusHelper.UpdateDegradedStatus(ctx, memcached,
+				fmt.Sprintf("Performing finalizer operations for the custom resource: %s", memcached.Name)); err != nil {
+				return ctrl.Result{}, err
+			}
+
+			// Record event for deletion
+			r.controller.Recorder.Event(memcached, "Warning", "Deleting",
+				fmt.Sprintf("Custom Resource %s is being deleted from the namespace %s",
+					memcached.Name, memcached.Namespace))
+
+			// Remove finalizer
+			if ok := controllerutil.RemoveFinalizer(memcached, memcachedFinalizer); !ok {
+				log.Error(nil, "Failed to remove finalizer")
+				return ctrl.Result{Requeue: true}, nil
+			}
+
+			if err := r.controller.Update(ctx, memcached); err != nil {
+				log.Error(err, "Failed to remove finalizer")
+				return ctrl.Result{}, err
+			}
+		}
+		return ctrl.Result{}, nil
+	}
+
+	// Add finalizer if it doesn't exist
+	if !controllerutil.ContainsFinalizer(memcached, memcachedFinalizer) {
+		log.Info("Adding Finalizer for Memcached")
+		if ok := controllerutil.AddFinalizer(memcached, memcachedFinalizer); !ok {
+			log.Error(nil, "Failed to add finalizer")
+			return ctrl.Result{Requeue: true}, nil
+		}
+
+		if err := r.controller.Update(ctx, memcached); err != nil {
+			log.Error(err, "Failed to update CR to add finalizer")
+			return ctrl.Result{}, err
+		}
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func (r *reconciliationHelperImpl) ReconcileDeployment(ctx context.Context, memcached *cachev1beta1.Memcached) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
+	dep := &appsv1.Deployment{}
+
+	// Check if deployment exists
+	err := r.controller.Get(ctx, types.NamespacedName{
+		Name:      memcached.Name,
+		Namespace: memcached.Namespace,
+	}, dep)
+
+	if err != nil && apierrors.IsNotFound(err) {
+		return r.controller.deployHelper.CreateNewDeployment(ctx, memcached)
+	} else if err != nil {
+		log.Error(err, "Failed to get Deployment")
+		return ctrl.Result{}, err
+	}
+
+	// Update if size doesn't match
+	size := memcached.Spec.Size
+	if *dep.Spec.Replicas != size {
+		dep.Spec.Replicas = &size
+		if err := r.controller.Update(ctx, dep); err != nil {
+			log.Error(err, "Failed to update Deployment",
+				"Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+			return ctrl.Result{}, err
+		}
+		return ctrl.Result{Requeue: true}, nil
+	}
+
+	// Update success status
+	if err := r.controller.statusHelper.UpdateSuccessStatus(ctx, memcached, size); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	return ctrl.Result{}, nil
 }
