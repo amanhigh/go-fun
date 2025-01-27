af9e0552cc5157efaf715cbbf1d45c9ac5c92f2d
Amanpreet Singh <coool.aman@gmail.com>
2025-01-27 21:17:27 +0530

    Fix

--- models/tax/valuation.go
+++ models/tax/valuation.go
@@ -8,13 +8,13 @@ type Symbolic interface {
 
 // Broker statement trade model
 type Trade struct {
-	Symbol     string  // Stock symbol (e.g. MPC)
-	date       string  // Trade date
-	Type       string  // BUY/SELL
-	Quantity   float64 // Number of shares
-	USDPrice   float64 // Price per share in USD
-	USDValue   float64 // Value in USD
-	Commission float64 // Trade commission
+    Symbol     string  `csv:"Symbol"`      
+    date       string  `csv:"Date"`        
+    Type       string  `csv:"Type"`
+    Quantity   float64 `csv:"Quantity"`
+    USDPrice   float64 `csv:"Price"`       
+    USDValue   float64 `csv:"Value"`         
+    Commission float64 `csv:"Commission"`
 }
 
 func NewTrade(symbol, date, tradeType string, quantity, price float64) Trade {
