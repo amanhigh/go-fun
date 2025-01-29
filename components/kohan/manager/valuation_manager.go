6a0b785d0f6531d78b9c10a70015a7b7cdc7f60f
Amanpreet Singh <coool.aman@gmail.com>
2025-01-29 21:56:02 +0530

    Build

--- components/kohan/manager/valuation_manager.go
+++ components/kohan/manager/valuation_manager.go
@@ -74,64 +74,6 @@ func (v *ValuationManagerImpl) validateTrades(trades []tax.Trade) common.HttpErr
 	return nil
 }
 
-func (v *ValuationManagerImpl) processTradesAndUpdateAnalysis(trades []tax.Trade) (tax.Valuation, float64) {
-	analysis := tax.Valuation{
-		Ticker: trades[0].Symbol,
-	}
-
-	var currentPosition float64
-	var maxPosition float64
-	var firstBuyDate time.Time
-
-	for _, t := range trades {
-		if t.Type == "BUY" {
-			currentPosition += t.Quantity
-		} else {
-			currentPosition -= t.Quantity
-		}
-
-		if firstBuyDate.IsZero() && t.Type == "BUY" {
-			if parsedTime, err := t.ParseDate(); err == nil {
-				firstBuyDate = parsedTime
-				analysis.FirstPosition = tax.Position{
-					Date:     parsedTime,
-					Quantity: t.Quantity,
-					USDPrice: t.USDPrice,
-				}
-			}
-		}
-
-		if currentPosition > maxPosition {
-			maxPosition = currentPosition
-			if parsedTime, err := t.ParseDate(); err == nil {
-				analysis.PeakPosition = tax.Position{
-					Date:     parsedTime,
-					Quantity: maxPosition,
-					USDPrice: t.USDPrice,
-				}
-			}
-		}
-	}
-
-	return analysis, currentPosition
-}
-
-func (v *ValuationManagerImpl) updateYearEndPosition(ctx context.Context, analysis tax.Valuation, currentPosition float64, year int) (tax.Valuation, common.HttpError) {
-	if currentPosition > 0 {
-		yearEndDate := time.Date(year, 12, 31, 0, 0, 0, 0, time.UTC)
-		price, err := v.tickerManager.GetPrice(ctx, analysis.Ticker, yearEndDate)
-		if err != nil {
-			return analysis, common.NewHttpError("failed to get year end price", http.StatusInternalServerError)
-		}
-		analysis.YearEndPosition = tax.Position{
-			Date:     yearEndDate,
-			Quantity: currentPosition,
-			USDPrice: price,
-		}
-	}
-	return analysis, nil
-}
-
 func (v *ValuationManagerImpl) getStartingPosition(ctx context.Context, ticker string, year int) (position tax.Position, err common.HttpError) {
 	// Last year's account record
 	account, err := v.accountManager.GetRecord(ctx, ticker)
