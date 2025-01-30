c7f61b5bf741f915e894ddf741c5425e6624a4e3
Amanpreet Singh <coool.aman@gmail.com>
2025-01-30 10:12:14 +0530

    Tax Manager

---
+++ components/kohan/manager/tax_manager.go
@@ -0,0 +1,40 @@
+package manager
+
+import (
+	"context"
+
+	"github.com/amanhigh/go-fun/models/common"
+	"github.com/amanhigh/go-fun/models/tax"
+)
+
+type TaxManager interface {
+	GetTaxSummary(ctx context.Context, year int) (tax.TaxSummary, common.HttpError)
+}
+
+type TaxManagerImpl struct {
+	capitalGainManager CapitalGainManager
+}
+
+func NewTaxManager(capitalGainManager CapitalGainManager) TaxManager {
+	return &TaxManagerImpl{
+		capitalGainManager: capitalGainManager,
+	}
+}
+
+func (t *TaxManagerImpl) GetTaxSummary(ctx context.Context, year int) (summary tax.TaxSummary, err common.HttpError) {
+	// Get and process gains for the year
+	gains, err := t.capitalGainManager.GetGainsForYear(ctx, year)
+	if err != nil {
+		return summary, err
+	}
+
+	// Process gains to INR
+	inrGains, err := t.capitalGainManager.ProcessTaxGains(ctx, gains)
+	if err != nil {
+		return summary, err
+	}
+
+	// Build summary
+	summary.INRGains = inrGains
+	return summary, nil
+}
