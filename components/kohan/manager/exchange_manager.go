d852b38c23c1849c6eeefcf2536903d95fe896aa
Amanpreet Singh <coool.aman@gmail.com>
2025-05-09 00:46:46 +0530

    Closest date

--- components/kohan/manager/exchange_manager.go
+++ components/kohan/manager/exchange_manager.go
@@ -2,6 +2,7 @@ package manager
 
 import (
 	"context"
+	"errors"
 
 	"github.com/amanhigh/go-fun/models/common"
 	"github.com/amanhigh/go-fun/models/tax"
@@ -25,6 +26,7 @@ func NewExchangeManager(sbiManager SBIManager) ExchangeManager {
 
 func (e *ExchangeManagerImpl) Exchange(ctx context.Context, exchangeables []tax.Exchangeable) common.HttpError {
 	for _, exchangeable := range exchangeables {
+
 		requestedDate, dateErr := exchangeable.GetDate()
 		if dateErr != nil {
 			return dateErr
@@ -32,26 +34,19 @@ func (e *ExchangeManagerImpl) Exchange(ctx context.Context, exchangeables []tax.
 
 		rate, err := e.sbiManager.GetTTBuyRate(ctx, requestedDate)
 
-		// Return early for non-closest-date errors
-		if err != nil {
-			if _, ok := err.(tax.ClosestDateError); !ok {
-				return err
-			}
-		}
-
-		// Handle closest date scenario
-		if closestErr, ok := err.(tax.ClosestDateError); ok {
-			log.Ctx(ctx).Warn().
-				Time("RequestedDate", requestedDate).
-				Time("ClosestDate", closestErr.GetClosestDate()).
-				Msg("Using closest available exchange rate")
-
-			exchangeable.SetTTRate(rate)
-			exchangeable.SetTTDate(closestErr.GetClosestDate())
-		} else {
-			// Exact date found
+		var closestErr tax.ClosestDateError
+		if err == nil {
+			// Exact date found - No error
 			exchangeable.SetTTRate(rate)
 			exchangeable.SetTTDate(requestedDate)
+		} else if errors.As(err, &closestErr) {
+			// Handle closest date scenario specifically
+			exchangeable.SetTTRate(rate) // Rate is still returned by GetTTBuyRate even with ClosestDateError
+			exchangeable.SetTTDate(closestErr.GetClosestDate())
+			log.Warn().Float64("RateSet", rate).Time("RequestedDate", requestedDate).Time("DateSet", closestErr.GetClosestDate()).Msg("ExchangeManager: Set closest rate/date")
+		} else {
+			// Handle any other non-nil error
+			return err
 		}
 	}
 	return nil
