090490635b965942dd9c1d2cd790670dc2aff675
Amanpreet Singh <coool.aman@gmail.com>
2025-05-12 22:32:39 +0530

    Filter US

--- components/kohan/manager/financial_year_manager.go
+++ components/kohan/manager/financial_year_manager.go
@@ -11,6 +11,7 @@ import (
 //go:generate mockery --name FinancialYearManager
 type FinancialYearManager[T tax.CSVRecord] interface {
 	FilterIndia(ctx context.Context, records []T, year int) ([]T, common.HttpError)
+	FilterUS(ctx context.Context, records []T, year int) ([]T, common.HttpError)
 }
 
 type FinancialYearManagerImpl[T tax.CSVRecord] struct{}
@@ -24,6 +25,25 @@ func (f *FinancialYearManagerImpl[T]) FilterIndia(_ context.Context, records []T
 
 	fyStart := time.Date(year, 4, 1, 0, 0, 0, 0, time.UTC)
 	fyEnd := time.Date(year+1, 3, 31, 23, 59, 59, 0, time.UTC)
+
+	for _, record := range records {
+		date, err := record.GetDate()
+		if err != nil {
+			return nil, err
+		}
+		if (date.Equal(fyStart) || date.After(fyStart)) &&
+			(date.Equal(fyEnd) || date.Before(fyEnd)) {
+			filtered = append(filtered, record)
+		}
+	}
+	return filtered, nil
+}
+
+func (f *FinancialYearManagerImpl[T]) FilterUS(_ context.Context, records []T, year int) ([]T, common.HttpError) {
+	var filtered []T
+
+	fyStart := time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC)
+	fyEnd := time.Date(year, 12, 31, 23, 59, 59, 0, time.UTC)
 
 	for _, record := range records {
 		date, err := record.GetDate()
