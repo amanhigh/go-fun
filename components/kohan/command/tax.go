b5ecab7b04d15ea909731471a1c96af241c0140c
Amanpreet Singh <coool.aman@gmail.com>
2025-08-01 00:41:49 +0530

    vested command

--- components/kohan/command/tax.go
+++ components/kohan/command/tax.go
@@ -54,4 +54,37 @@ var taxCmd = &cobra.Command{
 
 func init() {
 	appsCmd.AddCommand(taxCmd)
+	taxCmd.AddCommand(vestedCmd)
+}
+
+var vestedCmd = &cobra.Command{
+	Use:   "vested",
+	Short: "Generate Vested Brokerage Report",
+	Long:  `Generate Vested Brokerage Report from DriveWealth Excel file`,
+	RunE: func(_ *cobra.Command, _ []string) error {
+		// Initialize config and injector
+		kohanConfig, err := config.NewKohanConfig()
+		if err != nil {
+			return fmt.Errorf("failed to load config: %w", err)
+		}
+
+		core.SetupKohanInjector(kohanConfig)
+		ki := core.GetKohanInterface()
+		driveWealthManager, err := ki.GetDriveWealthManager()
+		if err != nil {
+			return fmt.Errorf("failed to get drive wealth manager: %w", err)
+		}
+
+		info, err := driveWealthManager.Parse()
+		if err != nil {
+			return fmt.Errorf("failed to parse drive wealth report: %w", err)
+		}
+
+		if err := driveWealthManager.GenerateCsv(info); err != nil {
+			return fmt.Errorf("failed to generate csv: %w", err)
+		}
+
+		fmt.Println("Successfully generated Vested Brokerage Report")
+		return nil
+	},
 }
